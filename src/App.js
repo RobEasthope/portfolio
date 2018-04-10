5fa8809fa72d9f031bbaf3a6b9edf43d1da95140
Rob Easthope <hello@robeasthope.com>
2018-04-10 17:58:16 +0100

    Add more test copy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p className="App-intro">
-          Foobar
+          Foobar Foobar Foobar 
         </p>
       </div>
     );
