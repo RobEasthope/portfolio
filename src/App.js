20f444e2e01786de521ce66d75ec92dc309384e3
Rob Easthope <hello@robeasthope.com>
2018-04-12 09:58:32 +0100

    Remove remaining boilerplate markup

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import Title from './components/Title/Title';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Title>Welcome to React</Title>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">Foobar Foobar Foobar</p>
+      <div>
+        <Title>Codex</Title>
       </div>
     );
   }
