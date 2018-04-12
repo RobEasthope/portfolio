8b59d31a8b8bf83a951c0f4534a21714cf9a0217
Rob Easthope <hello@robeasthope.com>
2018-04-12 09:48:20 +0100

    Test Title component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import Title from './components/Title/Title';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <Title>Welcome to React</Title>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p className="App-intro">
-          Foobar Foobar Foobar
-        </p>
+        <p className="App-intro">Foobar Foobar Foobar</p>
       </div>
     );
   }
