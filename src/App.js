1583f3b14e53d9c627e2f8f8e988d4a847755a40
Rob Easthope <hello@robeasthope.com>
2018-04-12 15:53:11 +0100

    Update navigation

--- src/App.js
+++ src/App.js
@@ -13,10 +13,13 @@ const App = () => (
           <Link to="/">Home</Link>
         </li>
         <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
           <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/topics">Topics</Link>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
 
