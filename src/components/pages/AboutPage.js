ca3d9de25c6ca3031a49d07442dedf8b8c89aabe
Rob Easthope <hello@robeasthope.com>
2018-04-13 09:51:08 +0100

    Add Helmet component to all pages

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import Title from 'components/typography/Title';
 
 const AboutPage = () => (
   <div>
+    <Helmet>
+      <title>About</title>
+    </Helmet>
+
     <Title>About</Title>
   </div>
 );
