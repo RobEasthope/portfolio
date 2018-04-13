7532d5ad60f229e16f46dc1614ec81a80ce88d4a
Rob Easthope <hello@robeasthope.com>
2018-04-13 11:36:35 +0100

    Create Nav component

---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ContactPage = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/portfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+
+    <hr />
+  </nav>
+);
+
+export default ContactPage;
