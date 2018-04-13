8f0141c409626182413cd7758dbf69fc5d0f22e9
Rob Easthope <hello@robeasthope.com>
2018-04-13 12:51:29 +0100

    Refactor code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
+
+import Nav from 'components/navigation/Nav';
+
 import HomePage from 'components/pages/HomePage';
 import PortfolioPage from 'components/pages/PortfolioPage';
 import AboutPage from 'components/pages/AboutPage';
 import ContactPage from 'components/pages/ContactPage';
 import Error404Page from 'components/pages/Error404Page';
 
+import { store, history } from '../store';
+
 const App = () => (
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/portfolio" component={PortfolioPage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route component={Error404Page} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    {/* ConnectedRouter will use the store from Provider automatically */}
+    <ConnectedRouter history={history}>
+      <div>
+        <Nav />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/portfolio" component={PortfolioPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route component={Error404Page} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
-import { Route, Switch } from 'react-router';
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-// import App from './containers/App';
-import HomePage from 'components/pages/HomePage';
-import PortfolioPage from 'components/pages/PortfolioPage';
-import AboutPage from 'components/pages/AboutPage';
-import ContactPage from 'components/pages/ContactPage';
-import Error404Page from 'components/pages/Error404Page';
-
-import Nav from 'components/navigation/Nav';
+import App from './containers/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const history = createHistory();
-
-const middleware = routerMiddleware(history);
-
-const store = createStore(
-  combineReducers({
-    // ...reducers,
-    router: routerReducer,
-  }),
-  composeWithDevTools(applyMiddleware(middleware)),
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    {/* ConnectedRouter will use the store from Provider automatically */}
-    <ConnectedRouter history={history}>
-      <div>
-        <Nav />
-
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/portfolio" component={PortfolioPage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/contact" component={ContactPage} />
-          <Route component={Error404Page} />
-        </Switch>
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
