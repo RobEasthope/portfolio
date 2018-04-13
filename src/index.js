71e3db8187eb171cefa2a05146a509a5f4977aa8
Rob Easthope <hello@robeasthope.com>
2018-04-13 11:37:10 +0100

    Add Nav component and clean up

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom';
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
 import createHistory from 'history/createBrowserHistory';
 import { Route, Switch } from 'react-router';
-
 import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
-// import App from './containers/App';
 
+// import App from './containers/App';
 import HomePage from 'components/pages/HomePage';
 import PortfolioPage from 'components/pages/PortfolioPage';
 import AboutPage from 'components/pages/AboutPage';
 import ContactPage from 'components/pages/ContactPage';
 import Error404Page from 'components/pages/Error404Page';
+
+import Nav from 'components/navigation/Nav';
+
 import registerServiceWorker from './registerServiceWorker';
 
 const history = createHistory();
@@ -33,13 +34,17 @@ ReactDOM.render(
   <Provider store={store}>
     {/* ConnectedRouter will use the store from Provider automatically */}
     <ConnectedRouter history={history}>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/portfolio" component={PortfolioPage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route component={Error404Page} />
-      </Switch>
+      <div>
+        <Nav />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/portfolio" component={PortfolioPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route component={Error404Page} />
+        </Switch>
+      </div>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
