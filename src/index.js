14f0bd0ca945cdbd3423af8c97f2bec5aa11557d
Rob Easthope <hello@robeasthope.com>
2018-04-13 11:21:10 +0100

    Working redux routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import createHistory from 'history/createBrowserHistory';
+import { Route, Switch } from 'react-router';
+
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+// import App from './containers/App';
+
+import HomePage from 'components/pages/HomePage';
+import PortfolioPage from 'components/pages/PortfolioPage';
+import AboutPage from 'components/pages/AboutPage';
+import ContactPage from 'components/pages/ContactPage';
+import Error404Page from 'components/pages/Error404Page';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory();
+
+const middleware = routerMiddleware(history);
+
+const store = createStore(
+  combineReducers({
+    // ...reducers,
+    router: routerReducer,
+  }),
+  applyMiddleware(middleware),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    {/* ConnectedRouter will use the store from Provider automatically */}
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/portfolio" component={PortfolioPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route component={Error404Page} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
