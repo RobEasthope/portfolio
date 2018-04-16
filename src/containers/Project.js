dcf8c8de6f569d9e3f375d8244d0ce96fdf4b0e7
Rob Easthope <hello@robeasthope.com>
2018-04-16 12:39:20 +0100

    Improve readability

--- src/containers/Project.js
+++ src/containers/Project.js
@@ -47,8 +47,10 @@ export default class Project extends React.Component {
         <div data-wio-id={this.state.doc.id}>
           {/* This is how to get an image into your template */}
           {/* <img alt="cover" src={this.state.doc.data.your_image_field_id.url} /> */}
+
           {/* This is how to insert a Rich Text field as plain text */}
           <h1>{PrismicReact.RichText.asText(this.state.doc.data.title)}</h1>
+
           {/* This is how to insert a Rich Text field into your template as html */}
           {PrismicReact.RichText.render(
             this.state.doc.data.text,
