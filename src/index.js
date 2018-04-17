7ed8c8ce1b43cb0de6e50bd097a0081d7b1c493a
Rob Easthope <hello@robeasthope.com>
2018-04-17 14:41:55 +0100

    Add react-snap friendly google analytics code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
+import ReactGA from 'react-ga';
 
 import App from './App';
-
 import registerServiceWorker from './registerServiceWorker';
+
+const snap = navigator.userAgent !== 'ReactSnap';
+const production = process.env.NODE_ENV === 'production';
+if (production && snap) {
+  ReactGA.initialize('XX-XXXXXXXX-X');
+}
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
