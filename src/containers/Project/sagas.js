509c7ea9e8820d86a198b9afcf5d5c0bf2aaaf89
Rob Easthope <hello@robeasthope.com>
2018-04-18 18:40:21 +0100

    Switch to using util function and clean up code

--- src/containers/Project/sagas.js
+++ src/containers/Project/sagas.js
@@ -1,28 +1,12 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
+import fetchPrismicData from '../../utils/fetchPrismicData';
 
-import Prismic from 'prismic-javascript';
 import { getPrismicDocLoaded, getPrismicDocError } from './actions';
-
 import { GET_PRISMIC_DOC } from './constants';
-
-const fetchData = () => {
-  const prismicCall = Prismic.api('http://codex.prismic.io/api', (error, api) => {
-    const options = {};
-    api.query('', options, (err, response) => {
-      // An empty query will return all the documents
-      if (err) {
-        return err;
-      }
-      return response.documents;
-    });
-  });
-
-  return prismicCall;
-};
 
 function* getPrismicDoc() {
   try {
-    const response = yield call(fetchData);
+    const response = yield call(fetchPrismicData);
     yield put(getPrismicDocLoaded(response));
   } catch (err) {
     yield put(getPrismicDocError(err));
