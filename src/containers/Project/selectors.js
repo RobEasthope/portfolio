cf22930d687390a40e6f30d78c069059de7d24d7
Rob Easthope <hello@robeasthope.com>
2018-04-18 20:15:14 +0100

    Remove selectors

--- src/containers/Project/selectors.js
+++
@@ -1,4 +0,0 @@
-export const selectProjectContainer = state => state.containers.projectReducer;
-
-// Need to use .get, beucase reducer defaulState was created by using ImmutableJS
-export const selectPrismicDoc = state => selectProjectContainer(state).get('prismicDoc');
