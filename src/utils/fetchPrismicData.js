88062aeb7a769a1f91f3d2b80df65c136672a59b
Rob Easthope <hello@robeasthope.com>
2018-04-19 10:28:43 +0100

    Use Prismic API v2

--- src/utils/fetchPrismicData.js
+++ src/utils/fetchPrismicData.js
@@ -1,7 +1,7 @@
 import Prismic from 'prismic-javascript';
 
 const fetchPrismicData = () => {
-  const prismicCall = Prismic.api('http://codex.prismic.io/api', (error, api) => {
+  const prismicCall = Prismic.api('http://codex.prismic.io/api/v2', (error, api) => {
     const options = {};
     api.query('', options, (err, response) => {
       // An empty query will return all the documents
