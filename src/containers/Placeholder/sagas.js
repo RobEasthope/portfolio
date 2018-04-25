980d4c36f2560a0961387d516dd01953c80a130d
Rob Easthope <hello@robeasthope.com>
2018-04-25 11:24:56 +0100

    Wire up new util function

--- src/containers/Placeholder/sagas.js
+++ src/containers/Placeholder/sagas.js
@@ -1,12 +1,12 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
-import fetchPrismicData from '../../utils/fetchPrismicData';
+import fetchPrismicDoc from '../../utils/fetchPrismicDoc';
 
 import { getPrismicDocLoaded, getPrismicDocError } from './actions';
 import { GET_PRISMIC_DOC } from './constants';
 
 function* getPrismicDoc() {
   try {
-    const response = yield call(fetchPrismicData);
+    const response = yield call(fetchPrismicDoc);
     yield put(getPrismicDocLoaded(response));
   } catch (err) {
     yield put(getPrismicDocError(err));
