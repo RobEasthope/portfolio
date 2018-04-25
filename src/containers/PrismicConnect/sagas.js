dd63c8fd8b3cea88d0eb094e42eff1cdb848ca08
Rob Easthope <hello@robeasthope.com>
2018-04-25 12:45:29 +0100

    Use buildContext

--- src/containers/PrismicConnect/sagas.js
+++ src/containers/PrismicConnect/sagas.js
@@ -1,12 +1,13 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
-import fetchPrismicApiDetails from '../../utils/fetchPrismicApiDetails';
+// import fetchPrismicApiDetails from '../../utils/fetchPrismicApiDetails';
+import buildContext from '../../utils/buildContext';
 
 import { prismicApiConnectionLoaded, prismicApiConnectionError } from './actions';
 import { PRISMIC_API_CONNECTION_REQUEST } from './constants';
 
 function* connectToPrismicAPI() {
   try {
-    const response = yield call(fetchPrismicApiDetails);
+    const response = yield call(buildContext);
     yield put(prismicApiConnectionLoaded(response));
   } catch (err) {
     yield put(prismicApiConnectionError(err));
