53bc034f8e292313d845269be1c22b23d000efa7
Rob Easthope <hello@robeasthope.com>
2018-04-25 11:24:42 +0100

    Duplicate function

---
+++ src/utils/fetchPrismicDoc.js
@@ -0,0 +1,18 @@
+import Prismic from 'prismic-javascript';
+
+const fetchPrismicDoc = () => {
+  const prismicCall = Prismic.api('http://codex.prismic.io/api/v2', (error, api) => {
+    const options = {};
+    api.query('', options, (err, response) => {
+      // An empty query will return all the documents
+      if (err) {
+        return err;
+      }
+      return response.documents;
+    });
+  });
+
+  return prismicCall;
+};
+
+export default fetchPrismicDoc;
