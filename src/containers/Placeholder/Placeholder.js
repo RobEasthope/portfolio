7aa7e6e6d79c700d3705ea930f9d3b66839d2a19
Rob Easthope <hello@robeasthope.com>
2018-04-25 19:34:26 +0100

    Clean up prismic templating

--- src/containers/Placeholder/Placeholder.js
+++ src/containers/Placeholder/Placeholder.js
@@ -46,15 +46,16 @@ class Placeholder extends React.Component {
 
   render() {
     if (this.state.doc) {
+      const { doc } = this.state;
       return (
         <div data-wio-id={this.state.doc.id}>
           {/* This is how to get an image into your template */}
-          <img alt="cover" src={this.state.doc.data.placeholder_image.url} />
+          <img alt="cover" src={doc.data.placeholder_image.url} />
           {/* This is how to insert a Rich Text field as plain text */}
-          <h1>{PrismicReact.RichText.asText(this.state.doc.data.placeholder_tagline)}</h1>
+          <h1>{PrismicReact.RichText.asText(doc.data.placeholder_tagline)}</h1>
           {/* This is how to insert a Rich Text field into your template as html */}
           {PrismicReact.RichText.render(
-            this.state.doc.data.placeholder_text,
+            doc.data.placeholder_text,
             this.props.prismicCtx.linkResolver,
           )}
         </div>
