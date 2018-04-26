2ec1008aa399938bbcb80e504db30bbeab711cca
Rob Easthope <hello@robeasthope.com>
2018-04-26 12:32:56 +0100

    Create centered tagline title

---
+++ src/components/typography/CenteredTagline.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import colors from 'colors.css';
+
+const CenteredTagline = styled.h1`
+  text-align: center;
+  color: ${colors.red};
+`;
+
+export default CenteredTagline;
