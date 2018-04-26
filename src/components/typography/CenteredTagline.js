9d714ad1173ca101909d23b807d8cc5a93162a0c
Rob Easthope <hello@robeasthope.com>
2018-04-26 13:07:06 +0100

    Draft out centered tagline component

--- src/components/typography/CenteredTagline.js
+++ src/components/typography/CenteredTagline.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 import colors from 'colors.css';
+import palette from '../../styles/colour-palette';
 
 const CenteredTagline = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   text-align: center;
-  color: ${colors.red};
+  color: ${palette.waterPink};
+  margin: 0;
 `;
 
 export default CenteredTagline;
