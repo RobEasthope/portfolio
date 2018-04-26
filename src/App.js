2db97f9de1bbf30960edd46f8a6cce70354a8616
Rob Easthope <hello@robeasthope.com>
2018-04-26 11:57:53 +0100

    Replace Nav with Header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './styles/global-styles';
 
 import buildContext from './utils/prismicContext';
 
-import Nav from './components/navigation/Nav';
+import Header from './components/Header/Header';
 
 import HomePage from './components/pages/HomePage';
 import PortfolioPage from './components/pages/PortfolioPage';
@@ -51,7 +51,7 @@ class App extends React.Component {
     return (
       <Router onUpdate={fireTracking} prismicCtx={this.state.prismicCtx}>
         <div>
-          <Nav />
+          <Header />
 
           <Switch>
             <Route exact path="/" component={HomePage} />
