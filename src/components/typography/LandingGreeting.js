7068584c40ffa251b0ba53965bb6bbdd195866d1
Rob Easthope <hello@robeasthope.com>
2018-05-14 18:31:29 +0100

    Create new LandingGreeting title

---
+++ src/components/typography/LandingGreeting.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import palette from '../../styles/colour-palette';
+
+const LandingGreeting = styled.h1`
+  font-family: 'League Spartan', serif;
+  font-size: 72px;
+  line-height: 1;
+  margin: 0;
+
+  color: white;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default LandingGreeting;
