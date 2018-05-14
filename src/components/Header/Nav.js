8b6ec83df983e382821df71cb8a3232be1a92beb
Rob Easthope <hello@robeasthope.com>
2018-05-14 19:39:10 +0100

    Comment out nav link

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => (
   <nav>
     <NavLink to="/portfolio">Portfolio</NavLink>
     <NavLink to="/about">About</NavLink>
-    <NavLink to="/contact">Contact</NavLink>
+    {/* <NavLink to="/contact">Contact</NavLink> */}
   </nav>
 );
 
