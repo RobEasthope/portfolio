2c216227eec17ef979437a75f5c9c42067419a57
Rob Easthope <hello@robeasthope.com>
2018-05-14 20:29:49 +0100

    Tweak font size

--- src/components/Header/NavLink.js
+++ src/components/Header/NavLink.js
@@ -6,6 +6,7 @@ import palette from '../../styles/colour-palette';
 const NavLink = styled(Link)`
   display: inline-block;
   font-family: 'hk_grotesklight', serif;
+  font-size: 18px;
   color: ${palette.ink};
   text-decoration: none;
   margin: 0 90px;
