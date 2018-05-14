37aab86f5468a87f68e184779dad739d70955831
Rob Easthope <hello@robeasthope.com>
2018-05-14 10:38:18 +0100

    Add new Sun Shield logo

--- src/components/Header/Shield.js
+++ src/components/Header/Shield.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import shieldAsset from './Shield (white bkg).svg';
+import shieldAsset from './sun-shield.png';
 
 const ShieldLogo = styled.img`
   height: 40px;
