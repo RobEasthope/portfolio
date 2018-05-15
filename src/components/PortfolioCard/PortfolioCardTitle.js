e852189007441aa74a98dbad9a6c4c0696ae128a
Rob Easthope <hello@robeasthope.com>
2018-05-15 11:22:37 +0100

    Draft out portfolio title

---
+++ src/components/PortfolioCard/PortfolioCardTitle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const PortfolioCardTitle = styled('h2')`
+  display: inline-block;
+  font-weight: normal;
+  font-size: 1rem;
+  margin: 0;
+  padding: 10px;
+  background-color: white;
+`;
+
+export default PortfolioCardTitle;
