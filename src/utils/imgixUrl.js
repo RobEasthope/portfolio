97a73b8e8c7e1031e949b51c2b6645432515863c
Rob Easthope <hello@robeasthope.com>
2018-05-15 16:20:57 +0100

    Make imgixUrl arrow function consistently return

--- src/utils/imgixUrl.js
+++ src/utils/imgixUrl.js
@@ -2,6 +2,7 @@ const imgixUrl = (url) => {
   if (url) {
     return url.replace('https://codex.cdn.prismic.io/codex/', 'https://rob-easthope.imgix.net/');
   }
+  return '';
 };
 
 export default imgixUrl;
