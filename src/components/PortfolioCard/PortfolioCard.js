3adc4fc36fe839a897c5c04195bb22d9ad800a3f
Rob Easthope <hello@robeasthope.com>
2018-05-15 13:46:15 +0100

    Add responsive widths

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -8,7 +8,15 @@ import PortfolioCardBkg from './PortfolioCardBkg';
 import PortfolioCardTitle from './PortfolioCardTitle';
 
 const PortfolioCard = props => (
-  <Block size={1 / 4}>
+  <Block
+    size={{
+      xs: 1 / 1,
+      sm: 1 / 2,
+      md: 1 / 3,
+      lg: 1 / 4,
+      xl: 1 / 5,
+    }}
+  >
     <PortfolioCardBkg src={imgixUrl(props.bkg)} type="bg" fluid>
       <PortfolioCardTitle>{props.title}</PortfolioCardTitle>
     </PortfolioCardBkg>
