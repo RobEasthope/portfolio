b569872411b48743aa8e4e84d6b39518806a9735
Rob Easthope <hello@robeasthope.com>
2018-05-15 12:05:51 +0100

    Centre title

--- src/components/PortfolioCard/PortfolioCardTitle.js
+++ src/components/PortfolioCard/PortfolioCardTitle.js
@@ -7,6 +7,11 @@ const PortfolioCardTitle = styled('h2')`
   margin: 0;
   padding: 10px;
   background-color: white;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 export default PortfolioCardTitle;
