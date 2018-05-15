15b69480e7c3060399c4fe42524f5e568362dba1
Rob Easthope <hello@robeasthope.com>
2018-05-15 16:22:38 +0100

    Fix import order

--- src/containers/Portfolio/PortfolioCards.js
+++ src/containers/Portfolio/PortfolioCards.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PortfolioCard from '../../components/PortfolioCard/PortfolioCard';
 import PrismicReact from 'prismic-reactjs';
+import PortfolioCard from '../../components/PortfolioCard/PortfolioCard';
 
 const PortfolioCards = props =>
   props.cards.map((slice) => {
