7d5163d021a297fded276541f79dc99b39340dcf
Rob Easthope <hello@robeasthope.com>
2018-05-17 16:00:34 +0100

    Add basic case for image_gallery data

--- src/containers/Project/ProjectBodyContent.js
+++ src/containers/Project/ProjectBodyContent.js
@@ -5,10 +5,12 @@ const ProjectBodyContent = props =>
   props.content.map((slice) => {
     switch (slice.slice_type) {
       case 'general_content':
-      case 'rich_text':
         return slice.items.map(content => (
           <div>{PrismicReact.RichText.render(content.rich_text)}</div>
         ));
+
+      case 'image_gallery':
+        return slice.items.map(content => <img src={content.image.url} />);
 
       default:
         return 'No content';
