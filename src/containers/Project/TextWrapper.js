fbf06d8933a390346c29ee64b0cad4d6e6bde8f2
Rob Easthope <hello@robeasthope.com>
2018-05-21 10:47:10 +0100

    Create TextWrapper element

---
+++ src/containers/Project/TextWrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Row } from '../../components/Grid/Grid';
+
+const TextWrapper = styled(Row)`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
+export default TextWrapper;
