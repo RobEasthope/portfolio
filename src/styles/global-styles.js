61595e2605607d88a8136fd9012d29ef50757295
Rob Easthope <hello@robeasthope.com>
2018-05-31 12:41:27 +0100

    Add typographic scale

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -10,14 +10,13 @@ injectGlobal`
     box-sizing: border-box;
   }
 
-  html {
+  html, body {
     font-size: 100%;
   }
 
   body {
-    font-family: 'EB Garamond', serif;
+    font-family: "EB Garamond", Georgia, serif;
     color: black;
-    font-size: 100%;
     margin-left: 10%;
     margin-right: 10%;
   }
@@ -32,11 +31,79 @@ injectGlobal`
     }
   }
 
+  h1, h2, h3, h4, h5, h6 {
+    word-wrap: break-word;
+    text-rendering: optimizeLegibility;
+    line-height: 1.5em;
+    margin-bottom: .5em
+  }
+
+  h1 {
+    font-size: ${rem(36)};
+    font-weight: 500;
+
+    @media only screen and (min-width: 30em) {
+      font-size: ${rem(48)};
+    }
+    @media only screen and (min-width: 37.5em) {
+      font-size: ${rem(56)};
+    }
+
+    @media only screen and (min-width: 48em) {
+      font-size: ${rem(64)};
+    }
+
+    @media only screen and (min-width: 64em) {
+      font-size: ${rem(84)};
+    }
+  }
+
+  h2 {
+    font-size: ${rem(28)};
+
+    @media only screen and (min-width: 30em) {
+      font-size: ${rem(36)};
+    }
+  }
+
+  h3 {
+    font-size: ${rem(24)};
+
+    @media only screen and (min-width: 30em) {
+      font-size: ${rem(32)};
+    }
+  }
+
+  h4 {
+    font-size: ${rem(22)};
+
+    @media only screen and (min-width: 30em) {
+      font-size: ${rem(28)};
+    }
+  }
+
+  h5 {
+    font-size: ${rem(24)};
+  }
+
+  h6 {
+    font-size: ${rem(16)};
+  }
+
   p {
-    margin-top: 0;
+    font-size: ${rem(18)};
+    line-height: 1.65em
+    margin-bottom: 1.5em;
+
+    @media only screen and (min-width: 30em) {
+      font-size: ${rem(22)};
+      line-height: 1.75em;
+    }
   }
 
   img {
     max-width: 100%;
+    height: auto;
+    vertical-align: middle
   }
 `;
