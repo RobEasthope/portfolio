466f93432e8466360cbabf2ec1f1075202b76e31
Rob Easthope <hello@robeasthope.com>
2018-05-31 20:56:26 +0100

    Rename OrgName to LinkOrNoLink

--- src/components/ProjectDetail/ProjectDetail.js
+++ src/components/ProjectDetail/ProjectDetail.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import OrgName from '../OrgName/OrgName';
+import LinkOrNoLink from '../LinkOrNoLink/LinkOrNoLink';
 import ProjectDetailText from '../ProjectDetailText/ProjectDetailText';
 
 const ProjectDetail = (props) => {
   if (props.title) {
     return (
       <ProjectDetailText>
-        {props.detailTitle} - <OrgName title={props.title} url={props.url} />
+        {props.detailTitle} >> <LinkOrNoLink title={props.title} url={props.url} />
       </ProjectDetailText>
     );
   }
