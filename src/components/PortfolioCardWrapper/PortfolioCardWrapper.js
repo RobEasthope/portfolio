a4eab0c1af7f267356b87a4a645eaea5bfb45c4c
Rob Easthope <hello@robeasthope.com>
2018-05-31 15:10:43 +0100

    Tweak margins

--- src/components/PortfolioCardWrapper/PortfolioCardWrapper.js
+++ src/components/PortfolioCardWrapper/PortfolioCardWrapper.js
@@ -4,7 +4,15 @@ import Grid from 'styled-components-grid';
 import rem from '../../utils/rem';
 
 const PortfolioCardWrapper = styled(Grid.Unit)`
-  padding: ${rem(40)};
+  padding: 0 ${rem(20)};
+
+  @media (min-width: 420px) {
+    margin-bottom: 2em;
+  }
+
+  @media (min-width: 1020px) {
+    margin-bottom: 5em;
+  }
 `;
 
 export default PortfolioCardWrapper;
