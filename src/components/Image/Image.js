6a7e00f3337a8bf41387127a273ee2a9bbb274d2
Rob Easthope <hello@robeasthope.com>
2018-05-31 19:18:17 +0100

    Creat Image component with bottom margin

---
+++ src/components/Image/Image.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import ValidImage from '../ValidImage/ValidImage';
+import rem from '../../utils/rem';
+
+const Image = styled(ValidImage)`
+  margin-bottom: ${rem(16 * 1.65)};
+`;
+
+export default Image;
