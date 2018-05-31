18a253502a4e13437e1cae971f363a1f7a2343f6
Rob Easthope <hello@robeasthope.com>
2018-05-31 14:59:35 +0100

    Restore missing work

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -9,6 +9,7 @@ import prismicLinkResolver from '../../prismic-link-resolver';
 import PortfolioCardWrapper from '../PortfolioCardWrapper/PortfolioCardWrapper';
 import PortfolioCardBkg from '../PortfolioCardBkg/PortfolioCardBkg';
 import PortfolioCardTitle from '../PortfolioCardTitle/PortfolioCardTitle';
+import PortfolioCardLink from '../PortfolioCardLink/PortfolioCardLink';
 
 const PortfolioCard = props => (
   // if (props.url.uid) {
@@ -17,14 +18,12 @@ const PortfolioCard = props => (
       xs: 1 / 1,
       sm: 1 / 2,
       md: 1 / 3,
-      // lg: 1 / 4,
-      // xl: 1 / 5,
     }}
   >
-    <Link to={prismicLinkResolver(props.url)}>
+    <PortfolioCardLink to={prismicLinkResolver(props.url)}>
       <PortfolioCardBkg src={imgixUrl(props.bkg)} type="bg" fluid />
       <PortfolioCardTitle>{PrismicReact.RichText.asText(props.title)}</PortfolioCardTitle>
-    </Link>
+    </PortfolioCardLink>
   </PortfolioCardWrapper>
 );
 // }
