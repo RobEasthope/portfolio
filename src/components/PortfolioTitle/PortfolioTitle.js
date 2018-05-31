3140f097a90ac1272d460b2018c1d8e38b96238d
Rob Easthope <hello@robeasthope.com>
2018-05-31 18:25:57 +0100

    Account for em/rem differewnces

--- src/components/PortfolioTitle/PortfolioTitle.js
+++ src/components/PortfolioTitle/PortfolioTitle.js
@@ -4,7 +4,8 @@ const PortfolioTitle = styled('h1')`
   font-weight: 500;
   line-height: 1.2;
   width: 90%;
-  padding: 2em 0 1em;
+  padding: 15rem 0 12rem;
+  margin: 0;
 `;
 
 export default PortfolioTitle;
--- src/components/ProjectTitle/ProjectTitle.js
+++ src/components/ProjectTitle/ProjectTitle.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
-const ProjectTitle = styled('div')`
-  text-align: center;
-  font-size: 72px;
-  max-width: 1000px;
-
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+const ProjectTitle = styled('h1')`
+  font-weight: 500;
+  line-height: 1.2;
+  width: 90%;
+  padding: 15rem 0 12rem;
+  margin: 0;
 `;
 
 export default ProjectTitle;
