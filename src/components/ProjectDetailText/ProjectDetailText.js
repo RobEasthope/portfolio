6ec32f88ab0918a5a784553fd5d4e7c3b7e2124d
Rob Easthope <hello@robeasthope.com>
2018-05-31 21:20:31 +0100

    Tweak project details styling

--- src/components/ProjectDetailText/ProjectDetailText.js
+++ src/components/ProjectDetailText/ProjectDetailText.js
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 const ProjectDetailText = styled('div')`
   display: block;
   margin-bottom: 0.75em;
-  padding-bottom: 0.75em;
+  padding: 0.75em 0;
   border-bottom: 1px solid #000;
   font-size: 22px;
   font-size: 1.375rem;
   line-height: 1.6em;
+
+  &:first-child {
+    border-top: 1px solid #000;
+  }
 
   &:last-child {
     border-bottom: none;
