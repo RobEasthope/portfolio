88456bc86eb2cc1d902081ca0e343b041c66c497
Rob Easthope <hello@robeasthope.com>
2018-05-31 19:44:34 +0100

    Upgrade SlimWrapper

--- src/components/SlimWrapper/SlimWrapper.js
+++ src/components/SlimWrapper/SlimWrapper.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import rem from '../../utils/rem';
 
 const SlimWrapper = styled('div')`
-  max-width: 800px;
+  max-width: ${rem(800)};
   margin: 0 auto;
 `;
 
