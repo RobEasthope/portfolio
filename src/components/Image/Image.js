0bc1d73c862681d43dbe6b5c1704d1e350a38e1c
Rob Easthope <hello@robeasthope.com>
2018-05-31 19:30:52 +0100

    Refactor Image component

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,10 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
-import ValidImage from '../ValidImage/ValidImage';
+import Imgix from 'react-imgix';
 import rem from '../../utils/rem';
 
-const Image = styled(ValidImage)`
-  margin-bottom: ${rem(16 * 1.65)};
+const ImageWrapper = styled('div')`
+  margin-bottom: ${rem(18 * 1.65)};
 `;
+
+const Image = (props) => {
+  if (props.src.url) {
+    return (
+      <ImageWrapper>
+        <Imgix src={props.src.url} alt={props.src.alt} />
+      </ImageWrapper>
+    );
+  }
+  return null;
+};
+
+Image.defaultProps = {
+  src: [],
+  url: '',
+  alt: '',
+};
+
+Image.propTypes = {
+  src: PropTypes.arrayOf(PropTypes.shape),
+  url: PropTypes.string.required,
+  alt: PropTypes.string,
+};
 
 export default Image;
