c582e568defef006d1748d0c234e9e3e2bedac4a
Rob Easthope <hello@robeasthope.com>
2018-05-31 14:35:19 +0100

    Change logo color

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import LogoAsset from './saladin-logo-dark-with-diamond.svg';
+import palette from '../../styles/colour-palette';
 
 const LogoImage = styled.img`
   height: 40px;
@@ -13,6 +14,7 @@ const LogoLink = styled(Link)`
   font-weight: 600;
   font-size: 18px;
   letter-spacing: 0.05em;
+  color: ${palette.highlight};
 `;
 
 const Logo = () => (
