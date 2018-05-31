d7e38a99abfdd2393b056ce38b7cb926f1fee146
Rob Easthope <hello@robeasthope.com>
2018-05-31 11:06:43 +0100

    Play around with new nav styles

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -5,9 +5,18 @@ import palette from '../../styles/colour-palette';
 
 const NavLink = styled(Link)`
   display: inline-block;
+  font-family: 'Nunito Sans', sans-serif;
+  font-weight: 600;
+  font-size: 18px;
+  letter-spacing: 0.05em;
   color: ${palette.ink};
   text-decoration: none;
-  padding: 10px;
+  margin-left: 1.25em;
+
+  :hover {
+    color: #e75b4c;
+    transition: 0.4s ease-in-out;
+  }
 `;
 
 export default NavLink;
