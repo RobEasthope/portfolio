8a468b967cfbb49cf432b5d9e4085dc896b9004d
Rob Easthope <hello@robeasthope.com>
2018-06-01 01:20:20 +0100

    Add dot styling

--- src/components/ProjectDetailText/ProjectDetailText.js
+++ src/components/ProjectDetailText/ProjectDetailText.js
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
+import rem from '../../utils/rem';
 
 const ProjectDetailText = styled('div')`
   display: block;
-  margin-bottom: 0.75em;
   padding: 0.75em 0;
   border-bottom: 1px solid #000;
   font-size: 22px;
   font-size: 1.375rem;
   line-height: 1.6em;
+
+  span {
+    margin: 0 ${rem(6)};
+  }
 
   &:first-child {
     border-top: 1px solid #000;
