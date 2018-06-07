00e1dd029f6764502ef1ab7abd0a9c0708992d1c
Rob Easthope <hello@robeasthope.com>
2018-06-07 15:46:23 +0200

    Adjust colour palette

--- src/styles/colour-palette.js
+++ src/styles/colour-palette.js
@@ -3,7 +3,8 @@ const palette = {
   lightGrey: '#9b9b9b',
   waterPink: '#FC5E58',
   typographicPink: '#e75b4d',
-  highlight: '#e75b4d',
+  typographicRed: '#f74e3d',
+  highlight: '#f74e3d',
 };
 
 export default palette;
