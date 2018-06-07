379f8bf600afe9a751a772f46b4c8d9da39b61bc
Rob Easthope <hello@robeasthope.com>
2018-06-07 15:46:12 +0200

    Tweak letter spacing

--- src/components/ProjectMiscDetails/ProjectMiscDetails.js
+++ src/components/ProjectMiscDetails/ProjectMiscDetails.js
@@ -5,6 +5,7 @@ const ProjectMiscDetails = styled('div')`
   font-family: 'p22-underground', sans-serif;
   font-size: ${rem(18)};
   color: #9b9b9b;
+  letter-spacing: 0.05em;
   margin-bottom: 4.88em;
 
   span {
