02cc1a12ddbb019087811134ce13e7d7e4e14da1
Rob Easthope <hello@robeasthope.com>
2018-06-08 14:30:45 +0200

    Add templating for About page slices

--- src/components/AboutBodyContent/AboutBodyContent.js
+++ src/components/AboutBodyContent/AboutBodyContent.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PrismicReact from 'prismic-reactjs';
 
+import SlimWrapper from '../SlimWrapper/SlimWrapper';
+import Image from '../Image/Image';
+
 const AboutBodyContent = props =>
   props.content.map((slice) => {
     switch (slice.slice_type) {
       case 'general_content':
-      case 'rich_text':
-        return slice.items.map(content => (
-          <div>{PrismicReact.RichText.render(content.rich_text)}</div>
-        ));
+        return (
+          <SlimWrapper>
+            {slice.items.map(content => PrismicReact.RichText.render(content.rich_text))}
+          </SlimWrapper>
+        );
+
+      case 'single_image':
+        return (
+          <SlimWrapper>{slice.items.map(content => <Image src={content.image} />)}</SlimWrapper>
+        );
 
       default:
-        return 'No content';
+        return 'Untemplated slice';
     }
   });
 
