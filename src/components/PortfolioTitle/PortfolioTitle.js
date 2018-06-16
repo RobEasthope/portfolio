479468859e160d18c70d2fdfe8221788fa3551c6
Rob Easthope <hello@robeasthope.com>
2018-06-16 12:14:07 +0200

    Centre Portfolio title

--- src/components/PortfolioTitle/PortfolioTitle.js
+++ src/components/PortfolioTitle/PortfolioTitle.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const PortfolioTitle = styled('h1')`
   font-weight: 500;
   line-height: 1.2;
+  text-align: center;
   width: 90%;
   padding: 1.75em 0;
   margin: 0;
