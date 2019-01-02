801d099f1c87ba0b2d2c082164f0fa62c0a80d44
Rob Easthope <hello@robeasthope.com>
2019-01-02 16:55:16 +0000

    Remove Image sharp code

--- src/components/image.js
+++ src/components/image.js
@@ -13,20 +13,5 @@ import Img from 'gatsby-image';
  * - `StaticQuery`: https://gatsby.app/staticquery
  */
 
-const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
-);
+const Image = () => <div />;
 export default Image;
