88cb31c20946d40900a04ace0f6b333de5402968
Rob Easthope <hello@robeasthope.com>
2019-01-16 10:01:50 +0000

    Add default prismic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,60 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from prismic.io.
+     */
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        // The name of your prismic.io repository. This is required.
+        // Example: 'gatsby-source-prismic-test-site' if your prismic.io address
+        // is 'gatsby-source-prismic-test-site.prismic.io'.
+        repositoryName: "gatsby-source-prismic-test-site",
+
+        // An API access token to your prismic.io repository. This is required.
+        // You can generate an access token in the "API & Security" section of
+        // your repository settings. Setting a "Callback URL" is not necessary.
+        // The token will be listed under "Permanent access tokens".
+        accessToken:
+          "example-wou7evoh0eexuf6chooz2jai2qui9pae4tieph1sei4deiboj",
+
+        // Set a link resolver function used to process links in your content.
+        // Fields with rich text formatting or links to internal content use this
+        // function to generate the correct link URL.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different link resolver logic for each field if necessary.
+        // See: https://prismic.io/docs/javascript/query-the-api/link-resolving
+        linkResolver: ({ node, key, value }) => doc => {
+          // Your link resolver
+        },
+
+        // Set a list of links to fetch and be made available in your link
+        // resolver function.
+        // See: https://prismic.io/docs/javascript/query-the-api/fetch-linked-document-fields
+        fetchLinks: [
+          // Your list of links
+        ],
+
+        // Set an HTML serializer function used to process formatted content.
+        // Fields with rich text formatting use this function to generate the
+        // correct HTML.
+        // The document node, field key (i.e. API ID), and field value are
+        // provided to the function, as seen below. This allows you to use
+        // different HTML serializer logic for each field if necessary.
+        // See: https://prismic.io/docs/nodejs/beyond-the-api/html-serializer
+        htmlSerializer: ({ node, key, value }) => (
+          type,
+          element,
+          content,
+          children
+        ) => {
+          // Your HTML serializer
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -31,4 +85,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
