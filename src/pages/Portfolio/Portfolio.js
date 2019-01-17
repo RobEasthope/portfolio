a0c4bb143726d90e9fc0029f98625d558a041dbd
Rob Easthope <hello@robeasthope.com>
2019-01-17 17:48:13 +0000

    Cleaning

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -61,6 +61,7 @@ class Portfolio extends React.Component {
           <PortfolioTitle>
             Hello, I'm Rob, a front end developer based in London.
           </PortfolioTitle>
+
           <PortfolioWrapper>
             <Grid>
               <PortfolioCards cards={doc.data.body} />
