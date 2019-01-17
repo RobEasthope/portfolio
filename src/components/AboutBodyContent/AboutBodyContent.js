1eab383cb3892341c15ee2217b9d45d1128c6d85
Rob Easthope <hello@robeasthope.com>
2019-01-17 14:59:39 +0000

    Add randowm keys to About page content

--- src/components/AboutBodyContent/AboutBodyContent.js
+++ src/components/AboutBodyContent/AboutBodyContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PrismicReact from "prismic-reactjs";
+import shortid from "shortid";
 
 import { Row, Block } from "../Grid/Grid";
 import SlimWrapper from "../SlimWrapper/SlimWrapper";
@@ -11,28 +12,30 @@ const AboutBodyContent = props =>
     switch (slice.slice_type) {
       case "general_content":
         return (
-          <SlimWrapper>
-            {slice.items.map(content =>
-              PrismicReact.RichText.render(content.rich_text)
-            )}
+          <SlimWrapper key={shortid.generate()}>
+            {slice.items.map(content => (
+              <React.Fragment key={shortid.generate()}>
+                {PrismicReact.RichText.render(content.rich_text)}
+              </React.Fragment>
+            ))}
           </SlimWrapper>
         );
 
       case "slim_image_gallery":
         return (
-          <SlimWrapper>
+          <SlimWrapper key={shortid.generate()}>
             {slice.items.map(content => (
-              <Image src={content.image} />
+              <Image key={shortid.generate()} src={content.image} />
             ))}
           </SlimWrapper>
         );
 
       case "wide_image_gallery":
         return (
-          <WideWrapper>
+          <WideWrapper key={shortid.generate()}>
             <Row>
               {slice.items.map(content => (
-                <Block size={1 / 1}>
+                <Block key={shortid.generate()} size={1 / 1}>
                   <Image src={content.image} />
                 </Block>
               ))}
