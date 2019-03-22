c97368602253003f21bd174c883fe583a1b5aa27
Rob Easthope <hello@robeasthope.com>
2019-03-22 16:43:21 +0000

    Initial code to generate project pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage, createPageDependency } = actions;
 
-// You can delete this file if you're not using it
+  const result = await graphql(`
+    {
+      allSanityProject(filter: { slug: { current: { ne: null } } }) {
+        edges {
+          node {
+            title
+            description
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  const projects = result.data.allSanityProject.edges || [];
+  projects.forEach((edge, index) => {
+    const path = `/project/${edge.node.slug.current}`;
+
+    createPage({
+      path,
+      component: require.resolve("./src/templates/project.js"),
+      context: { slug: edge.node.slug.current }
+    });
+
+    createPageDependency({ path, nodeId: edge.node.id });
+  });
+};
