6907525c2eb08155dd79b429e12bbae0eb6aac11
Rob Easthope <hello@robeasthope.com>
2019-04-09 11:35:59 +0100

    Remove styling wrapper

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,16 +22,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
-          <main>{children}</main>
-        </div>
+        <main>{children}</main>
       </>
     )}
   />
