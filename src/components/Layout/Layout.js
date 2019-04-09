c8845ec3ec8725324d66c9b3315135f6b0042d1a
Rob Easthope <hello@robeasthope.com>
2019-04-09 16:44:29 +0100

    Fix paths and clean up code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,6 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import { ThemeProvider } from 'styled-components';
 import theme from '../../styles/theme';
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout/Layout';
-import SEO from '../components/SEO/seo';
+import SEO from '../components/Seo/Seo';
 
 const IndexPage = () => (
   <Layout>
