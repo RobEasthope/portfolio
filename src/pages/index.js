210867529d92c1f15cc74e11d442df6e5cf4273e
Rob Easthope <hello@robeasthope.com>
2019-04-09 11:38:29 +0100

    Remove page two

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
