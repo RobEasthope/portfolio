1b8ed9d232f39c6cc119e976e148826c43d20980
Rob Easthope <hello@robeasthope.com>
2019-04-10 08:56:17 +0100

    Create basic footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'rebass';
+
+const Footer = () => (
+  <footer>
+    <nav>
+      <Link href="mailto:hello@robeasthope.com">hello@robeasthope.com</Link>
+      <Link href="https://www.linkedin.com/in/rob-easthope-9b0bb436/">
+        Linkedin
+      </Link>
+    </nav>
+  </footer>
+);
+
+export default Footer;
