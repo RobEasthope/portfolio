32bdc5f45234319924d83e2a94189c7eb0f74df4
Rob Easthope <hello@robeasthope.com>
2019-04-10 10:28:34 +0100

    Revert "Add MobileMenu component and wrappers"
    This reverts commit 1593e2e1f763bb2716a5f9f921ecb081d7904dca.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,19 +5,15 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../../styles/theme';
 
 import Header from '../Header/Header';
-import MobileMenu from '../MobileMenu/MobileMenu';
 import Footer from '../Footer/Footer';
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <div id="outer-container">
-      <MobileMenu pageWrapId="page-wrap" outerContainerId="outer-container" />
-      <div id="page-wrap">
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
   </ThemeProvider>
 );
 
