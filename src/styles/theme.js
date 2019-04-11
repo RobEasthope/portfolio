9b45ceca291d23ad9407e358489f46eb439e48ae
Rob Easthope <hello@robeasthope.com>
2019-04-11 15:05:40 +0100

    Refactor theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,27 +1,32 @@
-export default {
-  fontSizes: [
-    '0', // Zero - 0
-    '.875rem', // 14px - 1
-    '1rem', // 16px, - 2
-    '1.25rem', // 20px -3
-    '1.5rem', // 24px - 4
-    '2.25rem', // 36px - 5
-    '3rem', // 48px - 6
-    '5rem', // 80px - 7
-    '6rem', // 96px - 8
-  ],
-  breakpoints: [
-    '28em', // 448px
-    '40em', // 640px
-    '52em', // 832px
-    '64em', // 1024px
-  ],
-  breakpoints: {
-    sm: breakpoints[0],
-    md: breakpoints[1],
-    lg: breakpoints[2],
-    xlg: breakpoints[3]
-  }
+/* eslint-disable prefer-destructuring */
+// Breakpoints
+const breakpoints = [
+  '28em', // 448px
+  '40em', // 640px
+  '52em', // 832px
+  '64em', // 1024px
+];
 
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
-};
+// Breakpoint aliases
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xlg = breakpoints[3];
+
+// Typescale
+const fontSizes = [
+  '0', // Zero - 0
+  '.875rem', // 14px - 1
+  '1rem', // 16px, - 2
+  '1.25rem', // 20px -3
+  '1.5rem', // 24px - 4
+  '2.25rem', // 36px - 5
+  '3rem', // 48px - 6
+  '5rem', // 80px - 7
+  '6rem', // 96px - 8
+];
+
+// Spacing
+const space = [0, 4, 8, 16, 32, 64, 128, 256, 512];
+
+export default { breakpoints, fontSizes, space };
