108f0e44e8faed9be7ceef80b0bf098d4864ec13
Rob Easthope <hello@robeasthope.com>
2019-04-11 16:21:57 +0100

    Add contianer wrapper to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/Seo/Seo';
+import Container from '../components/Grid/Container';
 
 const AboutPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>About</h1>
+    <Container>
+      <h1>About</h1>
+    </Container>
   </Layout>
 );
 
