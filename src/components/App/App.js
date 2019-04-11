dd40274f712c595a59c87818c92a8ddc03b01a2c
Rob Easthope <hello@robeasthope.com>
2019-04-11 19:22:47 +0100

    Split Layout component into App and Layout

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import theme from '../../styles/theme';
+import GlobalStyles from '../../styles/globalStyles';
+
+const App = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <>
+      <Normalize />
+      <GlobalStyles />
+      {children}
+    </>
+  </ThemeProvider>
+);
+
+App.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default App;
