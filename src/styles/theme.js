794c8f5ef9726e5581ef4a526fce1dd25b12452c
Rob Easthope <hello@robeasthope.com>
2019-04-11 13:36:36 +0100

    Switch font scale to rems

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,13 @@
 export default {
-  fontSizes: [14, 16, 20, 24, 36, 48, 80, 96],
+  fontSizes: [
+    '875rem', // 14px
+    '1rem', // 16px,
+    '1.25rem', // 20px
+    '1.5rem', // 24px
+    '2.25rem', // 36px
+    '3rem', // 48px
+    '5rem', // 80px
+    '6rem', // 96px
+  ],
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
 };
