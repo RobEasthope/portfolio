7eedbc128a4bb148ae9bf579088f86d81bcf377a
Rob Easthope <hello@robeasthope.com>
2019-04-11 11:05:51 +0100

    Add landing image and layout structure

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Image from 'gatsby-image';
 
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/Seo/Seo';
+import LandingTagline from '../components/LandingTagline/LandingTagline';
+import Flex from '../components/Grid/Flex';
+import Box from '../components/Grid/Box';
 
 export const query = graphql`
   {
     sanityLandingPage {
       tagline
+      landingImage {
+        imageAsset {
+          asset {
+            fluid {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        altText
+      }
       seoMetaData {
         title
         description
@@ -17,15 +31,26 @@ export const query = graphql`
 `;
 
 const LandingPage = ({ data }) => (
-  <Layout>
+  <React.Fragment>
     <SEO
       title={data.sanityLandingPage.seoMetaData.title}
       description={data.sanityLandingPage.seoMetaData.description}
-      keywords={[`gatsby`, `application`, `react`]}
       keywords={data.sanityLandingPage.seoMetaData.keywords}
     />
-    <h1>{data.sanityLandingPage.tagline}</h1>
-  </Layout>
+
+    <Layout>
+      <Flex>
+        <Box width={1}>
+          <Image
+            fluid={data.sanityLandingPage.landingImage.imageAsset.asset.fluid}
+          />
+          <LandingTagline as="h1">
+            {data.sanityLandingPage.tagline}
+          </LandingTagline>
+        </Box>
+      </Flex>
+    </Layout>
+  </React.Fragment>
 );
 
 export default LandingPage;
