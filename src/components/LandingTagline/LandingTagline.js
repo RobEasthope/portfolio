080ddc84479cd32d30ef610a779ee2ebda1e17d4
Rob Easthope <hello@robeasthope.com>
2019-04-11 11:03:13 +0100

    Create LandingTagline

---
+++ src/components/LandingTagline/LandingTagline.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Text } from 'rebass';
+
+const LandingTagline = styled(Text)`
+  color: black;
+  font-family: 'Gotham', sans-serif;
+  font-weight: bold;
+  margin: 15px;
+
+  position: absolute:
+  bottom: 0;
+`;
+
+export default LandingTagline;
