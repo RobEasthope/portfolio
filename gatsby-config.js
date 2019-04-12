6580719011e0230dcd8a447d0390fe0177400d20
Rob Easthope <hello@robeasthope.com>
2019-04-12 09:45:09 +0100

    Add gatsby-plugin-catch-links to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
