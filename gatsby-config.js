ea8ecaeef1fe67b2a18648c7fbaa65ac6ce6aac9
Rob Easthope <hello@robeasthope.com>
2019-04-12 09:50:47 +0100

    Add gatsby-plugin-nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,15 @@ module.exports = {
     //   }
     // },
     {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
