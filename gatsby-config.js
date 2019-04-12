c04edbe906fc24753b62e075029c5da713e2224f
Rob Easthope <hello@robeasthope.com>
2019-04-12 09:52:53 +0100

    Add gatsby-plugin-canonical-urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: 'https://www.robeasthope.com',
+      },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
