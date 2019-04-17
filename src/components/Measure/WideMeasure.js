f965f8ecdb43bf340ce1243428f42da14f862cb7
Rob Easthope <hello@robeasthope.com>
2019-04-17 08:37:58 +0100

    Add Tachyon measures

---
+++ src/components/Measure/WideMeasure.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Box as rebassBox } from 'rebass';
+
+const NarrowMeasure = styled(rebassBox)`
+  max-width: 20em;
+`;
+
+export default NarrowMeasure;
