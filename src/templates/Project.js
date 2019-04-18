5117b4c3f456e1e40f1a7ffe7ec57f3a2594169c
Rob Easthope <hello@robeasthope.com>
2019-04-18 11:16:25 +0100

    Expand graphql request

--- src/templates/Project.js
+++ src/templates/Project.js
@@ -6,12 +6,33 @@ export const query = graphql`
   query($slug: String) {
     sanityProject(slug: { current: { eq: $slug } }) {
       title
+      shortTitle
+      mainText
+      projectType
+      shortDescription
+      client {
+        name
+      }
+      agency {
+        name
+      }
+      date
+      projectUrlTitle
+      projectUrl
+      repoUrl
+      techUsed {
+        name
+      }
+      seoMetaData {
+        keywords
+      }
     }
   }
 `;
 
 const Project = ({ data }) => (
   <Layout>
+    <h1>{data.sanityProject.title}</h1>
     <h1>{data.sanityProject.title}</h1>
   </Layout>
 );
