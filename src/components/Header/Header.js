8f39a33c0da06f0b8f1888b5ae4f042752128c88
Rob Easthope <hello@robeasthope.com>
2019-04-24 16:51:32 +0100

    Play around with padding

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ import LogoAsset from '../../images/saladin-logo-dark-with-diamond.svg';
 const Header = () => (
   <header>
     <nav>
-      <Nav as="ul" px="2" py="3" alignItems="center" justifyContent="center">
+      <Nav as="ul" px="2" py="4" alignItems="center" justifyContent="center">
         <NavLink to="/portfolio" title="Portfolio" textAlign="right" />
         <Logo url="/" src={LogoAsset} altText="Rob Easthope" />
         <NavLink to="/about" title="About" textAlign="left" />
