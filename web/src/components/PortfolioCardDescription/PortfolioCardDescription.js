1a4d72587b627056b17c47c184433476f733aec1
Rob Easthope <hello@robeasthope.com>
2019-04-26 18:16:42 +0100

    Set provisional font sizes

--- web/src/components/PortfolioCardDescription/PortfolioCardDescription.js
+++ web/src/components/PortfolioCardDescription/PortfolioCardDescription.js
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 import { Text } from 'rebass';
 
 import palette from '../../styles/palette';
+import fontSizing from '../../utils/fontsizing';
 
 const PortfolioCardDescription = styled(Text)`
   display: inline-block;
+  font-size: ${fontSizing(20, 24)};
   color: ${palette.ink};
   text-decoration: none;
+  margin-left: 1em;
 `;
 
 export default PortfolioCardDescription;
--- web/src/components/PortfolioCardTitle/PortfolioCardTitle.js
+++ web/src/components/PortfolioCardTitle/PortfolioCardTitle.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import { Text } from 'rebass';
 
 import palette from '../../styles/palette';
+import fontSizing from '../../utils/fontsizing';
 
 const PortfolioCardTitle = styled(Text)`
   display: inline-block;
+  font-size: ${fontSizing(32, 72)};
   color: ${palette.ink};
   text-decoration: none;
 `;
