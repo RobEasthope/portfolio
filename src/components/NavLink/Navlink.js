298ba1980bfb348209aa52da9cebad4202dd4122
Rob Easthope <hello@robeasthope.com>
2019-04-26 08:52:11 +0100

    Test fontSizing function

--- src/components/NavLink/Navlink.js
+++ src/components/NavLink/Navlink.js
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 import { Text } from 'rebass';
 import palette from '../../styles/palette';
+import fontSizing from '../../utils/fontsizing';
 
 const NavLinkText = styled(Link)`
   color: ${palette.ink};
   font-weight: bold;
   text-decoration: none;
-  font-size: calc(18px + (24 - 18) * ((100vw - 320px) / (1680 - 320)));
+  /* font-size: calc(18px + (24 - 18) * ((100vw - 320px) / (1680 - 320))); */
+  font-size: ${fontSizing(18, 24)};
 `;
 
 const NavLink = ({ to, title }) => (
