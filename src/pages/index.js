8da1630297fd84791923fb479d0c4bff10452730
Rob Easthope <hello@robeasthope.com>
2019-04-26 11:49:31 +0100

    Add error states for index and project template

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,61 +28,81 @@ export const query = graphql`
     }
   }
 `;
+const LandingPage = props => {
+  const { data, errors } = props;
 
-const LandingPage = ({ data }) => (
-  <React.Fragment>
-    <SEO
-      title="Rob Easthope"
-      keywords={data.sanityLandingPage.seoMetaData.keywords}
-    />
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    );
+  }
 
-    <Layout>
-      <Flex>
-        <RelativeBox width={1} px="0">
-          <LandingTagline
-            as="h1"
-            width="100%"
-            px="4"
-            m="0"
-            fontSize={{ sm: 5, md: 6, lg: 7, xlg: 8 }}
-          >
-            {data.sanityLandingPage.tagline}
-          </LandingTagline>
-          <LandingJumbotron
-            fluid={data.sanityLandingPage.landingImage.imageAsset.asset.fluid}
-          />
-        </RelativeBox>
-      </Flex>
-      <Flex flexDirection="row">
-        <Box width={1} p="4" as="section">
-          <Heading
-            as="h2"
-            justifySelf="flex-end"
-            fontSize={{ sm: 5, md: 5, lg: 6, xlg: 7 }}
-            textAlign="right"
-          >
-            Portfolio
-          </Heading>
-          <Heading
-            as="h2"
-            justifySelf="flex-end"
-            fontSize={{ sm: 5, md: 5, lg: 6, xlg: 7 }}
-            textAlign="right"
-          >
-            About
-          </Heading>
-          <Heading
-            as="h2"
-            justifySelf="flex-end"
-            fontSize={{ sm: 5, md: 5, lg: 6, xlg: 7 }}
-            textAlign="right"
-          >
-            Contact
-          </Heading>
-        </Box>
-      </Flex>
-    </Layout>
-  </React.Fragment>
-);
+  const page = data;
+
+  if (!page) {
+    throw new Error('Missing landing page data.');
+  }
+
+  return (
+    <React.Fragment>
+      <SEO
+        title="Rob Easthope"
+        keywords={data.sanityLandingPage.seoMetaData.keywords}
+      />
+
+      <Layout>
+        <Flex>
+          <RelativeBox width={1} px="0">
+            <LandingTagline
+              as="h1"
+              width="100%"
+              px="4"
+              m="0"
+              fontSize={{ sm: 5, md: 6, lg: 7, xlg: 8 }}
+            >
+              {data.sanityLandingPage.tagline}
+            </LandingTagline>
+            <LandingJumbotron
+              fluid={data.sanityLandingPage.landingImage.imageAsset.asset.fluid}
+            />
+          </RelativeBox>
+        </Flex>
+        <Flex flexDirection="row">
+          <Box width={1} p="4" as="section">
+            <Heading
+              as="h2"
+              justifySelf="flex-end"
+              fontSize={{ sm: 5, md: 5, lg: 6, xlg: 7 }}
+              textAlign="right"
+            >
+              Portfolio
+            </Heading>
+            <Heading
+              as="h2"
+              justifySelf="flex-end"
+              fontSize={{ sm: 5, md: 5, lg: 6, xlg: 7 }}
+              textAlign="right"
+            >
+              About
+            </Heading>
+            <Heading
+              as="h2"
+              justifySelf="flex-end"
+              fontSize={{ sm: 5, md: 5, lg: 6, xlg: 7 }}
+              textAlign="right"
+            >
+              Contact
+            </Heading>
+          </Box>
+        </Flex>
+      </Layout>
+    </React.Fragment>
+  );
+};
+// const LandingPage = ({ data }) => (
+
+// );
 
 export default LandingPage;
--- src/templates/Project.js
+++ src/templates/Project.js
@@ -46,39 +46,56 @@ export const query = graphql`
   }
 `;
 
-const Project = ({ data }) => (
-  <Layout>
-    <SEO
-      title={data.sanityProject.shortTitle}
-      description={data.sanityProject.shortDescription}
-      // keywords={data.sanityLandingPage.seoMetaData.keywords}
-    />
-    <ContentContainer>
-      <Flex flexWrap="wrap" mx="-4">
-        <Box width={1 / 1} px="3" pt="7" pb="7" justifyContent="center">
-          {data.sanityProject.projectType && (
-            <Text textAlign="center">{data.sanityProject.projectType}</Text>
-          )}
-          {data.sanityProject.title && (
-            <Text as="h1" textAlign="center">
-              {data.sanityProject.title}
-            </Text>
-          )}
-        </Box>
-        <Box width={{ b: 1, md: 6 / 10 }} px="4">
-          {data.sanityProject.gallery &&
-            data.sanityProject.gallery.map(image => (
-              <Box mb="4" key={image._key}>
-                <Image
-                  fluid={image.imageAsset.asset.fluid}
-                  alt={image.altText}
-                />
-              </Box>
-            ))}
-        </Box>
+const Project = props => {
+  const { data, errors } = props;
 
-        <Box width={{ b: 1, md: 4 / 10 }} px="4">
-          {/* {data.sanityProject.title && (
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    );
+  }
+
+  const page = data;
+
+  if (!page) {
+    throw new Error('Missing project.');
+  }
+
+  return (
+    <Layout>
+      <SEO
+        title={data.sanityProject.shortTitle}
+        description={data.sanityProject.shortDescription}
+        // keywords={data.sanityLandingPage.seoMetaData.keywords}
+      />
+      <ContentContainer>
+        <Flex flexWrap="wrap" mx="-4">
+          <Box width={1 / 1} px="3" pt="7" pb="7" justifyContent="center">
+            {data.sanityProject.projectType && (
+              <Text textAlign="center">{data.sanityProject.projectType}</Text>
+            )}
+            {data.sanityProject.title && (
+              <Text as="h1" textAlign="center">
+                {data.sanityProject.title}
+              </Text>
+            )}
+          </Box>
+          <Box width={{ b: 1, md: 6 / 10 }} px="4">
+            {data.sanityProject.gallery &&
+              data.sanityProject.gallery.map(image => (
+                <Box mb="4" key={image._key}>
+                  <Image
+                    fluid={image.imageAsset.asset.fluid}
+                    alt={image.altText}
+                  />
+                </Box>
+              ))}
+          </Box>
+
+          <Box width={{ b: 1, md: 4 / 10 }} px="4">
+            {/* {data.sanityProject.title && (
             <Text as="h1" mt="0" textAlign="left">
               {data.sanityProject.title}
             </Text>
@@ -92,56 +109,57 @@ const Project = ({ data }) => (
               <Text> / {data.sanityProject.date}</Text>
             )}
           </Flex> */}
-          <Box pb="4">
-            <BlockContent blocks={data.sanityProject._rawBody} />
-          </Box>
-          <Flex mx="-16px" flexWrap="wrap">
-            <Box width={{ b: 1, lg: 1 / 2 }} px="3">
-              {data.sanityProject.client && (
-                <React.Fragment>
-                  <Text>Client</Text>
-                  <Text pb="4">{data.sanityProject.client.name}</Text>
-                </React.Fragment>
-              )}
+            <Box pb="4">
+              <BlockContent blocks={data.sanityProject._rawBody} />
             </Box>
-            <Box width={{ b: 1, lg: 1 / 2 }} px="3">
-              {data.sanityProject.agency && (
-                <React.Fragment>
-                  <Text>Agency</Text>
-                  <Text pb="4">{data.sanityProject.agency.name}</Text>
-                </React.Fragment>
-              )}
-            </Box>
-            <Box width={{ b: 1, lg: 1 / 2 }} px="3">
-              {data.sanityProject.projectUrl &&
-                data.sanityProject.projectUrlTitle && (
+            <Flex mx="-16px" flexWrap="wrap">
+              <Box width={{ b: 1, lg: 1 / 2 }} px="3">
+                {data.sanityProject.client && (
                   <React.Fragment>
-                    <Text>Production site</Text>
+                    <Text>Client</Text>
+                    <Text pb="4">{data.sanityProject.client.name}</Text>
+                  </React.Fragment>
+                )}
+              </Box>
+              <Box width={{ b: 1, lg: 1 / 2 }} px="3">
+                {data.sanityProject.agency && (
+                  <React.Fragment>
+                    <Text>Agency</Text>
+                    <Text pb="4">{data.sanityProject.agency.name}</Text>
+                  </React.Fragment>
+                )}
+              </Box>
+              <Box width={{ b: 1, lg: 1 / 2 }} px="3">
+                {data.sanityProject.projectUrl &&
+                  data.sanityProject.projectUrlTitle && (
+                    <React.Fragment>
+                      <Text>Production site</Text>
+                      <Text pb="3">
+                        <Link to={data.sanityProject.projectUrl}>
+                          {data.sanityProject.projectUrlTitle}
+                        </Link>
+                      </Text>
+                    </React.Fragment>
+                  )}
+              </Box>
+              <Box width={{ b: 1, lg: 1 / 2 }} px="3">
+                {data.sanityProject.repoUrl && (
+                  <React.Fragment>
+                    <Text>Github repo</Text>
                     <Text pb="3">
-                      <Link to={data.sanityProject.projectUrl}>
-                        {data.sanityProject.projectUrlTitle}
+                      <Link to={data.sanityProject.repoUrl}>
+                        {data.sanityProject.repoUrl}
                       </Link>
                     </Text>
                   </React.Fragment>
                 )}
-            </Box>
-            <Box width={{ b: 1, lg: 1 / 2 }} px="3">
-              {data.sanityProject.repoUrl && (
-                <React.Fragment>
-                  <Text>Github repo</Text>
-                  <Text pb="3">
-                    <Link to={data.sanityProject.repoUrl}>
-                      {data.sanityProject.repoUrl}
-                    </Link>
-                  </Text>
-                </React.Fragment>
-              )}
-            </Box>
-          </Flex>
-        </Box>
-      </Flex>
-    </ContentContainer>
-  </Layout>
-);
+              </Box>
+            </Flex>
+          </Box>
+        </Flex>
+      </ContentContainer>
+    </Layout>
+  );
+};
 
 export default Project;
