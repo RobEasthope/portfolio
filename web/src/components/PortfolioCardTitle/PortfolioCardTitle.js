87be395eb17efe00e80034e1ca21ee71004ba9ed
Rob Easthope <hello@robeasthope.com>
2019-04-26 17:41:40 +0100

    Update title styling

--- web/src/components/PortfolioCardTitle/PortfolioCardTitle.js
+++ web/src/components/PortfolioCardTitle/PortfolioCardTitle.js
@@ -4,6 +4,7 @@ import { Text } from 'rebass';
 import palette from '../../styles/palette';
 
 const PortfolioCardTitle = styled(Text)`
+  display: inline-block;
   color: ${palette.ink};
   text-decoration: none;
 `;
