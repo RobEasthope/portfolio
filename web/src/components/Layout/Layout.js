9be844b53207c0963b199e284ad1074ecc8a190a
Rob Easthope <hello@robeasthope.com>
2019-04-26 22:54:41 +0100

    Clean up Layout

--- web/src/components/Layout/Layout.js
+++ web/src/components/Layout/Layout.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Box } from 'rebass';
-
 import App from '../App/App';
-
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import AppWrapper from '../Grid/AppWrapper';
