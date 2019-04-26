0c2e828d08da32671bfea9ef68774aa60383a7bd
Rob Easthope <hello@robeasthope.com>
2019-04-26 22:21:33 +0100

    Move Portfolio index into it's own component

--- web/src/components/PortfolioIndex/PortfolioIndex.js
+++ web/src/components/PortfolioIndex/PortfolioIndex.js
@@ -1,8 +1,58 @@
+import React from 'react';
 import styled from 'styled-components';
 import { Box } from 'rebass';
+import Image from 'gatsby-image';
 
-const PortfolioIndex = styled(Box)`
+import PortfolioCard from '../PortfolioCard/PortfolioCard';
+import PortfolioCardLink from '../PortfolioCardLink/PortfolioCardLink';
+import PortfolioCardTitle from '../PortfolioCardTitle/PortfolioCardTitle';
+import PortfolioCardDescription from '../PortfolioCardDescription/PortfolioCardDescription';
+import PortfolioCardThumbnail from '../PortfolioCardThumbnail/PortfolioCardThumbnail';
+
+const PortfolioIndexWrapper = styled(Box)`
   list-style-type: none;
 `;
+
+const PortfolioIndex = props => {
+  const { portfolioIndex } = props;
+
+  return (
+    <PortfolioIndexWrapper
+      as="ul"
+      width="auto"
+      py="4"
+      px="2"
+      m="0"
+      flexWrap="wrap"
+    >
+      {portfolioIndex.map(project => (
+        <PortfolioCard
+          as="li"
+          width={1}
+          px="3"
+          mb="4"
+          key={project.slug.current}
+        >
+          <PortfolioCardLink to={project.slug.current}>
+            {project.thumbnailImage && (
+              <PortfolioCardThumbnail>
+                <Image
+                  className="projectThumbnail"
+                  fluid={project.thumbnailImage.asset.fluid}
+                />
+              </PortfolioCardThumbnail>
+            )}
+            <div>
+              <PortfolioCardTitle>{project.shortTitle}</PortfolioCardTitle>
+              <PortfolioCardDescription>
+                {project.description}
+              </PortfolioCardDescription>
+            </div>
+          </PortfolioCardLink>
+        </PortfolioCard>
+      ))}
+    </PortfolioIndexWrapper>
+  );
+};
 
 export default PortfolioIndex;
