3638d5984f5127dc097739ec7d4e262acaa78ead
Rob Easthope <hello@robeasthope.com>
2019-04-29 13:11:38 +0100

    Only display thumbnail at larger sizes

--- web/src/components/PortfolioCard/PortfolioCard.js
+++ web/src/components/PortfolioCard/PortfolioCard.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
 import { Box } from 'rebass';
+import { breakpoints } from '../../styles/theme';
 
 const PortfolioCard = styled(Box)`
   position: relative;
 
   .projectThumbnail {
+    display: none;
     visibility: hidden;
   }
 
-  :hover {
+  @media (min-width: ${breakpoints.md}) {
     .projectThumbnail {
-      visibility: visible;
+      display: block;
+    }
+    &:hover {
+      .projectThumbnail {
+        visibility: visible;
+      }
     }
   }
 `;
