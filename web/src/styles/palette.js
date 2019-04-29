b5f15ae84273e724530af248efb41e9aedbf3ede
Rob Easthope <hello@robeasthope.com>
2019-04-29 13:07:04 +0100

    Add carmine shades

--- web/src/styles/palette.js
+++ web/src/styles/palette.js
@@ -3,6 +3,9 @@ const palette = {
   white: '#fff',
   ink: 'rgb(36, 36, 36)',
   imperialRed: 'rgb(237, 41, 57)',
+  carmine: 'rgb(150, 0, 24)',
+  japaneseCarmine: 'rgb(157, 41, 51)',
+',
 };
 
 export default palette;
