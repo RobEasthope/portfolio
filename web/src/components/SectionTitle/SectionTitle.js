f2dc5c8a7997dd1a95145ae07074064bc49cbb78
Rob Easthope <hello@robeasthope.com>
2019-04-29 08:49:17 +0100

    Create SectionTitle.js

---
+++ web/src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Heading } from 'rebass';
+
+const SectionTitle = styled(Heading)`
+  font-size: 12vw;
+  font-weight: normal;
+`;
+
+export default SectionTitle;
