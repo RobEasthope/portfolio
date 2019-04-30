926172698964e9a964db4dedf27085c0e39cd2a6
Rob Easthope <hello@robeasthope.com>
2019-04-30 22:15:17 +0100

    Revert "Header and footer spacing tweak"
    This reverts commit f39c98926a400c1ce99a117b6a69637fd753f110.

--- web/src/components/Footer/Footer.js
+++ web/src/components/Footer/Footer.js
@@ -8,8 +8,7 @@ const Footer = () => (
     as="footer"
     width={1}
     px="4"
-    pt="5"
-    pb="4"
+    py="5"
     flexDirection="row"
     justifyContent={{ b: 'flex-start', md: 'flex-end' }}
   >
