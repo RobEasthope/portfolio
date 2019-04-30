d135387e6c01860cbb50bea97cece4c0a00f8a3b
Rob Easthope <hello@robeasthope.com>
2019-04-30 08:26:51 +0100

    About schema

---
+++ sanity-studio/schemas/about.js
@@ -0,0 +1,28 @@
+export default {
+  name: 'contact',
+  title: 'Contact',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'About section title',
+      type: 'string',
+    },
+    {
+      name: 'body',
+      title: 'About blurb',
+      type: 'blockContent',
+    },
+    {
+      name: 'portrait',
+      title: 'Portrait',
+      type: 'image',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+};
