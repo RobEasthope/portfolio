92b7eeccf644f3785c134637f9987ab8dfec0f6d
Rob Easthope <hello@robeasthope.com>
2019-05-01 12:31:42 +0100

    Remove duplicate code

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -45,18 +45,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: 'gatsby-source-sanity',
       options: {
         projectId: process.env.SANITY_PROJECT_ID,
