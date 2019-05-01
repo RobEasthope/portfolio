3512d0df586bf09daa0b3263d38ff0738e4ef79e
Rob Easthope <hello@robeasthope.com>
2019-05-01 12:40:37 +0100

    Add tracking ID

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -69,8 +69,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: 'UA-XXXXXXXXX-X',
+        trackingId: process.env.GOOGLE_ANALYTICS_CODE,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
