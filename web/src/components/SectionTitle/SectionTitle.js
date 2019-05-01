460cd3e88486c4cb238b45734af90c586cfd4bc7
Rob Easthope <hello@robeasthope.com>
2019-05-01 09:22:21 +0100

    Potential code for title oranamentation

--- web/src/components/SectionTitle/SectionTitle.js
+++ web/src/components/SectionTitle/SectionTitle.js
@@ -6,6 +6,11 @@ const SectionTitle = styled(Heading)`
   font-weight: normal;
   margin-top: 0.5em;
   margin-bottom: 0.25em;
+
+  /* margin-bottom: 0.5em;
+  display: inline-block;
+  border-bottom: 3px solid #333;
+  line-height: 1; */
 `;
 
 export default SectionTitle;
