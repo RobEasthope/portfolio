9f26c6eb89cfe7f9a56bf44a67e8d5100bac12c6
Rob Easthope <hello@robeasthope.com>
2019-05-04 10:53:34 +0100

    Toggle image and video depending on size

--- web/src/components/LandingVideoJumbotron/LandingVideoJumbotron.js
+++ web/src/components/LandingVideoJumbotron/LandingVideoJumbotron.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 import SanityMuxPlayer from 'sanity-mux-player';
 
+import { breakpoints } from '../../styles/theme';
+
 const LandingJumbotron = styled(SanityMuxPlayer)`
   position: relative;
   width: 100%;
   height: 100vh !important;
   overflow: hidden;
+
+  display: none;
+  @media (min-width: ${breakpoints.md}) {
+    display: block;
+  }
 
   video {
     position: absolute;
