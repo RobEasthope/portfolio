e6985ae59e03cc658f808745ee21c6390dce1df2
Rob Easthope <hello@robeasthope.com>
2019-05-04 09:34:55 +0100

    Remove old import

--- web/src/components/LandingJumbotron/LandingJumbotron.js
+++ web/src/components/LandingJumbotron/LandingJumbotron.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import Image from 'gatsby-image';
 
-import breakpoints from '../../styles/theme';
-
 const LandingJumbotron = styled(Image)`
   width: 100%;
   height: 100vh;
