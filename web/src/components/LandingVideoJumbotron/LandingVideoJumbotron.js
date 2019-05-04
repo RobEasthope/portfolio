b179d5b94d4dafe85ac59a7908a87f792346393c
Rob Easthope <hello@robeasthope.com>
2019-05-04 09:34:46 +0100

    Draft out video jumbotron

---
+++ web/src/components/LandingVideoJumbotron/LandingVideoJumbotron.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import SanityMuxPlayer from 'sanity-mux-player';
+
+const LandingJumbotron = styled(SanityMuxPlayer)`
+  width: 100%;
+  height: 100vh;
+`;
+
+export default LandingJumbotron;
