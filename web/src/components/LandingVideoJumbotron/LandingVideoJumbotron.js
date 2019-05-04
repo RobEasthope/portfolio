be4bb0683e548757bd7ed2f867965d5334dd389a
Rob Easthope <hello@robeasthope.com>
2019-05-04 10:48:22 +0100

    Style landing video

--- web/src/components/LandingVideoJumbotron/LandingVideoJumbotron.js
+++ web/src/components/LandingVideoJumbotron/LandingVideoJumbotron.js
@@ -2,8 +2,18 @@ import styled from 'styled-components';
 import SanityMuxPlayer from 'sanity-mux-player';
 
 const LandingJumbotron = styled(SanityMuxPlayer)`
+  position: relative;
   width: 100%;
-  height: 100vh;
+  height: 100vh !important;
+  overflow: hidden;
+
+  video {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    /* width: 100%; */
+    /* height: 100vh; */
+  }
 `;
 
 export default LandingJumbotron;
