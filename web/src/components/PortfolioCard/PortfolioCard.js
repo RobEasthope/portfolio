9ac90afa3762adac1d2e78caafe5bbbeb4b801dc
Rob Easthope <hello@robeasthope.com>
2019-05-05 13:17:34 +0100

    Tweak portfolio card borders

--- web/src/components/PortfolioCard/PortfolioCard.js
+++ web/src/components/PortfolioCard/PortfolioCard.js
@@ -4,11 +4,9 @@ import { breakpoints } from '../../styles/theme';
 import palette from '../../styles/palette';
 
 const PortfolioCard = styled(Box)`
-  /* border-bottom: 1px solid #ececec; */
   border-bottom: 1px solid ${palette.ink};
-  &:first-child {
-    border-top: 1px solid ${palette.ink};
-    /* border-bottom: 0; */
+  &:last-child {
+    border-bottom: 0;
   }
 
   .projectThumbnail {
