3824f39f3c956f046e3de0f4ba586dfb2e7f5146
Rob Easthope <hello@robeasthope.com>
2019-05-06 19:13:57 +0100

    Test code

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -2,6 +2,10 @@ require('dotenv').config({
   path: `.env`,
 });
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Rob Easthope`,
