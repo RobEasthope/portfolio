d2d7484fe5380ef4fbe0247c27b8e61d08c6228e
Rob Easthope <hello@robeasthope.com>
2020-01-14 15:51:29 +0000

    Remove details from CV schema

--- sanity-studio/schemas/cv.js
+++ sanity-studio/schemas/cv.js
@@ -66,50 +66,6 @@ export default {
         ]
       }],
     },
-    {
-      name: 'details',
-      title: 'Contact details',
-      type: 'object',
-      fields: [
-        {
-          name: 'name',
-          title: 'Name',
-          type: 'string',
-        },
-        {
-          name: 'email',
-          title: 'Email address',
-          type: 'email',
-        },
-        {
-          name: 'phoneNumber',
-          title: 'Phone number',
-          type: 'string',
-        },
-        {
-          name: 'twitter',
-          title: 'Twitter profile',
-          type: 'url',
-        },
-        {
-          name: 'linkedin',
-          title: 'LinkedIn profile',
-          type: 'url',
-        },
-        {
-          name: 'portfolioUrl',
-          title: 'Portfolio URL',
-          type: 'url',
-        },
-        {
-          name: 'githubUrl',
-          title: 'Github URL',
-          type: 'url',
-        },
-      ]
-    },
-    
-    
   ],
   preview: {
     prepare() {
