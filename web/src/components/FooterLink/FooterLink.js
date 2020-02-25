71372535b4e0fefcadb6ccc67f0cccd66fd799c3
Rob Easthope <hello@robeasthope.com>
2020-02-25 18:48:46 +0000

    Typography changes

--- web/src/components/FooterLink/FooterLink.js
+++ web/src/components/FooterLink/FooterLink.js
@@ -5,8 +5,9 @@ import { colors } from '../../styles/theme';
 import fontSizing from '../../utils/fontsizing';
 
 const FooterLink = styled(Link)`
-  font-size: ${fontSizing(18, 24)};
+  font-size: ${fontSizing(24, 32)};
   color: ${colors.ink};
+  text-decoration: none;
 `;
 
 export default FooterLink;
