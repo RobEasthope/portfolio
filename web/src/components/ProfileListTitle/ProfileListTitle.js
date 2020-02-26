9c80f6cec44b3b1c5a832f11fb6e946698c7a46a
Rob Easthope <hello@robeasthope.com>
2020-02-26 14:36:10 +0000

    Create ProfileListTitle component

---
+++ web/src/components/ProfileListTitle/ProfileListTitle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Text } from 'rebass';
+
+import fontSizing from '../../utils/fontsizing';
+
+const ProfileListTitle = styled(Text)`
+  font-size: ${fontSizing(20, 24)};
+  margin-bottom: 1em;
+`;
+
+export default ProfileListTitle;
