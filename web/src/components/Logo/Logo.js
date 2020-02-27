c0097d76d3018ad214a59514b84657732ff1c50a
Rob Easthope <hello@robeasthope.com>
2020-02-27 11:44:23 +0000

    Clean up code

--- web/src/components/Logo/Logo.js
+++ web/src/components/Logo/Logo.js
@@ -7,12 +7,12 @@ import { breakpoints, colors } from '../../styles/theme';
 
 import fontSizing from '../../utils/fontsizing';
 
-const Glyph = styled.img`
-  width: 36px;
-  background-color: white;
-  padding: 3px;
-  border-radius: 50%;
-`;
+// const Glyph = styled.img`
+//   width: 36px;
+//   background-color: white;
+//   padding: 3px;
+//   border-radius: 50%;
+// `;
 
 const LogoWrapper = styled(Box)`
   flex: 1;
@@ -27,16 +27,12 @@ const SiteTitleText = styled(Text)`
   color: ${colors.ink};
   text-decoration: none;
   font-size: ${fontSizing(18, 24)};
-  /* margin-top: 0.5em; */
 `;
 
 const Logo = ({ as, url, src, altText }) => (
   <LogoWrapper as={as} justifySelf="flex-start">
     <Link to={url}>
-      {/* <Glyph src={src} alt={altText} /> */}
-      {/* <Box> */}
       <SiteTitleText>Rob Easthope</SiteTitleText>
-      {/* </Box> */}
     </Link>
   </LogoWrapper>
 );
