aaf47d7bc01dc281fecf06614ed8eb82c2426384
Rob Easthope <hello@robeasthope.com>
2020-02-27 09:49:02 +0000

    Restore name to sidebar

--- web/src/components/Logo/Logo.js
+++ web/src/components/Logo/Logo.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Box } from 'rebass';
+import { Box, Text } from 'rebass';
 import { Link } from 'gatsby';
-import { breakpoints } from '../../styles/theme';
+import { breakpoints, colors } from '../../styles/theme';
 
-// import { colors } from '../../styles/theme';
-// import fontSizing from '../../utils/fontsizing';
+import fontSizing from '../../utils/fontsizing';
 
 const Glyph = styled.img`
   width: 36px;
@@ -14,6 +13,7 @@ const Glyph = styled.img`
   padding: 3px;
   border-radius: 50%;
 `;
+
 const LogoWrapper = styled(Box)`
   flex: 1;
   padding: 1em;
@@ -22,22 +22,22 @@ const LogoWrapper = styled(Box)`
   }
 `;
 
-// const SiteTitleText = styled(Link)`
-//   display: inline-block;
-//   color: ${colors.ink};
-//   text-decoration: none;
-//   font-size: ${fontSizing(18, 24)};
-//   margin-top: 0.5em;
-// `;
+const SiteTitleText = styled(Text)`
+  display: inline-block;
+  color: ${colors.ink};
+  text-decoration: none;
+  font-size: ${fontSizing(18, 24)};
+  margin-top: 0.5em;
+`;
 
 const Logo = ({ as, url, src, altText }) => (
   <LogoWrapper as={as} justifySelf="flex-start">
     <Link to={url}>
       <Glyph src={src} alt={altText} />
+      <Box>
+        <SiteTitleText>Rob Easthope</SiteTitleText>
+      </Box>
     </Link>
-    {/* <div>
-      <SiteTitleText>Rob Easthope</SiteTitleText>
-    </div> */}
   </LogoWrapper>
 );
 
