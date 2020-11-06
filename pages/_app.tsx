ba6a77727c323fbe1f2d30442136a8544603925b
Rob Easthope <hello@robeasthope.com>
2020-11-06 16:46:15 +0000

    Refactor _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const App: React.FC =({ Component, pageProps }) => {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
