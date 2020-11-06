edde725b74ec6f9d4ca80c59be1f2accd266330c
Rob Easthope <hello@robeasthope.com>
2020-11-06 17:12:52 +0000

    Add typing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps): React.Node {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
