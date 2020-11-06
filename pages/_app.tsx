c1fa8b3c386f4c917aeda77fec1f0fea5c8d2e07
Rob Easthope <hello@robeasthope.com>
2020-11-06 17:11:00 +0000

    Switch back to standard function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-const App:React.FC =({ Component, pageProps }: AppProps) => {
+function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
