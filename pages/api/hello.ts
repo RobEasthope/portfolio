d63a2351b44a3b61d09b14ac34fd9ae69b9e9595
Rob Easthope <hello@robeasthope.com>
2020-11-06 15:11:55 +0000

    Copy in TS code from examples

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
+export const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
+
+export default handler
