fcfc5bde1c0ad723922c094f30d93f54af73c086
Rob Easthope <hello@robeasthope.com>
2022-01-31 13:01:51 +0000

    Fix syntax

--- packages/utils/sanity-api/sanity-config.ts
+++ packages/utils/sanity-api/sanity-config.ts
@@ -9,10 +9,8 @@ type SanityConfigProps = {
 };
 
 export const sanityConfig: SanityConfigProps = {
-  projectId:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-  dataset:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET!
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   token: process.env.SANITY_API_TOKEN!,
   useCdn: false,
