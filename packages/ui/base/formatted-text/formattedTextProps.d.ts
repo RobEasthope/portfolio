e401bbc3c07b7ef83f24dd68fbef780469d48cbd
Rob Easthope <hello@robeasthope.com>
2022-02-01 16:20:00 +0000

    Create formattedTextProps.d.ts

---
+++ packages/ui/base/formatted-text/formattedTextProps.d.ts
@@ -0,0 +1,12 @@
+import { PageProps } from 'pages/Page/Page';
+import { ReactNode } from 'react';
+
+export type SerializerMarksProps = {
+  mark: { url: string; page: PageProps };
+  children: ReactNode;
+};
+
+export type BlockRendererProps = {
+  node: { style: string };
+  children: ReactNode;
+};
