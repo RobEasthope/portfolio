703f18540be36959a07b2f72d6c324e1ccd11dbd
Rob Easthope <hello@robeasthope.com>
2022-02-01 15:26:13 +0000

    Create Heading.tsx

---
+++ packages/ui/base/typography/Heading/Heading.tsx
@@ -0,0 +1,16 @@
+import { styled } from '@/UI/styles/stitches.config';
+
+export const Heading = styled('div', {
+  lineHeight: 1.75,
+
+  // variants: {
+  //   fontFamily: {
+  //     sansSerif: {
+  //       fontFamily: 'sans-serif',
+  //     },
+  //     serif: {
+  //       fontFamily: 'serif',
+  //     },
+  //   },
+  // },
+});
