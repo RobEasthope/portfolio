ed024f0ec4699435fdcd5cd0b79facf6a3f1853a
Rob Easthope <hello@robeasthope.com>
2022-02-01 14:35:02 +0000

    Change studio icon

--- apps/portfolio-sanity/styles/StudioLogo/StudioLogo.tsx
+++ apps/portfolio-sanity/styles/StudioLogo/StudioLogo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const StudioLogo = () => (
   <div style={{ display: 'flex', alignItems: 'center'}}>
-    <img src="/static/triquetra-white.svg" alt="Portfolio" style={{height: '28px'}} />
+    <img src="/static/saladin-logo-dark-with-bkg.png" alt="Portfolio" style={{height: '28px'}} />
     {/* <span style={{fontSize: '18px', fontWeight: 'bold', marginLeft: '0.5em'}}>Portfolio</span> */}
   </div>
 );
