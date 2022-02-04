f062a41dcc4ae5e0915ffb06666769fd4807bb3a
Rob Easthope <hello@robeasthope.com>
2022-02-04 18:14:12 +0000

    Fix path

--- apps/portfolio-next/pages/api/preview/preview-page.ts
+++ apps/portfolio-next/pages/api/preview/preview-page.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { previewAnyPageByIdQuery } from '@/UI/pages/Page/Page.queries';
+import { previewAnyPageByIdQuery } from '@/UI/pages/Preview/Preview.queries';
 import { previewClient } from '@/UTILS/sanity-api/sanity.server';
 import { PageProps } from '@/UI/pages/Page/Page';
 
