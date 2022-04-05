a7bf718a9d709957611824942427bb4affb779f1
Rob Easthope <hello@robeasthope.com>
2022-04-05 18:06:27 +0100

    Add new props for Header

--- packages/ui/pages/Project/Project.tsx
+++ packages/ui/pages/Project/Project.tsx
@@ -36,7 +36,11 @@ export const Project = ({
 
       <FixedFooterLayout>
         {header && (
-          <Header logo={header?.logo} navigation={header?.navigation} />
+          <Header
+            logo={header?.logo}
+            navigationLeft={header?.navigationLeft}
+            navigationRight={header?.navigationRight}
+          />
         )}
 
         <MainContentLayout as="main">
