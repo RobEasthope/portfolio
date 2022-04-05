c74db469427d57d9381349d3436db3b76f7e3234
Rob Easthope <hello@robeasthope.com>
2022-04-05 15:33:37 +0100

    Basic footer markup and types

--- packages/ui/navigation/Footer/Footer.tsx
+++ packages/ui/navigation/Footer/Footer.tsx
@@ -1,8 +1,34 @@
 import { MaxWidth } from '@/UI/base/layout/MaxWidth/MaxWidth';
 import { PaddedComponent } from '@/UI/base/layout/PaddedComponent/PaddedComponent';
+import { Box } from '@/UI/base/layout/Box/Box';
+import { Flex } from '@/UI/base/layout/Flex/Flex';
+import { ExternalLinkWithTitleSchemaProps } from '@/UI/base/links/ExternalLink/ExternalLink';
+import { InternalLinkWithTitleSchemaProps } from '@/UI/base/links/InternalLink/InternalLink';
+import { SuperLink } from '@/UI/base/links/SuperLink/SuperLink';
 
-export const Footer = () => (
+// Types
+export interface FooterProps extends rawFooterProps {
+  navigation?: [
+    ExternalLinkWithTitleSchemaProps,
+    InternalLinkWithTitleSchemaProps
+  ];
+}
+
+// Markup
+export const Footer = ({ navigation }: Pick<FooterProps, 'navigation'>) => (
   <PaddedComponent as="footer">
-    <MaxWidth width="page">Footer</MaxWidth>
+    <MaxWidth width="page">
+      <Flex>
+        <Box as="nav">
+          {navigation &&
+            navigation?.length > 0 &&
+            navigation.map((nav) => (
+              <li key={nav?._key}>
+                <SuperLink link={nav}>{nav.title}</SuperLink>
+              </li>
+            ))}
+        </Box>
+      </Flex>
+    </MaxWidth>
   </PaddedComponent>
 );
