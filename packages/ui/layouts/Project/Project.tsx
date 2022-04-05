169f8c797fd316f758616bca4142ca53c35876fe
Rob Easthope <hello@robeasthope.com>
2022-04-05 20:04:24 +0100

    Add text elements with some basic alignment

--- packages/ui/layouts/Project/Project.tsx
+++ packages/ui/layouts/Project/Project.tsx
@@ -3,14 +3,22 @@ import { Header } from '@/UI/navigation/Header/Header';
 import { FixedFooterLayout } from '@/UI/base/layout/FixedFooterLayout/FixedFooterLayout';
 import { MainContentLayout } from '@/UI/base/layout/MainContentLayout/MainContentLayout';
 import { NextMetadata } from '@/UI/base/app/Metadata/NextMetadata';
-import { RenderSections } from '@/UI/base/app/RenderSections/RenderSections';
-import { Project as rawProjectProps } from '@/UI/types/sanity-schema';
+import {
+  Organisation,
+  Project as rawProjectProps,
+} from '@/UI/types/sanity-schema';
 import { ExampleSectionProps } from 'sections/ExampleSection/ExampleSection';
 import { AppGlobalsProps } from '@/UI/base/settings/Globals';
 import { Text } from '@/UI/base/typography/Text/Text';
+import { ProjectText } from '@/UI/base/formatted-text/ProjectText/ProjectText';
+import { MaxWidth } from '@/UI/base/layout/MaxWidth/MaxWidth';
+import { Flex } from '@/UI/base/layout/Flex/Flex';
+import { TextAlign } from '@/UI/base/layout/TextAlign/TextAlign';
 
 export interface ProjectProps extends rawProjectProps {
   sections: [ExampleSectionProps];
+  client: Organisation;
+  agency: Organisation;
 }
 
 export const Project = ({
@@ -24,7 +32,17 @@ export const Project = ({
   const { header, footer, metadata } = globals;
 
   // Page props
-  const { title, description, sections } = project;
+  const {
+    title,
+    description,
+    client,
+    agency,
+    projectText,
+    projectUrlTitle,
+    projectUrl,
+    repoUrl,
+    date,
+  } = project;
 
   return (
     <>
@@ -45,9 +63,32 @@ export const Project = ({
         )}
 
         <MainContentLayout as="main">
-          {description && <Text>{description}</Text>}
-          {title && <Text>{title}</Text>}
-          {sections && <RenderSections sections={sections} />}
+          <MaxWidth width="small">
+            <TextAlign align="centre">
+              {description && <Text>{description}</Text>}
+              {title && <Text>{title}</Text>}
+              <Flex gap={3} justify="center">
+                {client && <Text>{client?.name}</Text>}
+                {agency && <Text>{agency?.name}</Text>}
+              </Flex>
+            </TextAlign>
+
+            {projectText && <ProjectText blocks={projectText as unknown} />}
+
+            <TextAlign align="centre">
+              {projectUrl && (
+                <Text>
+                  <a href={projectUrl}>{projectUrlTitle || projectUrl}</a>
+                </Text>
+              )}
+              {repoUrl && (
+                <Text>
+                  <a href={repoUrl}>{repoUrl}</a>
+                </Text>
+              )}
+              {date && <Text>{date}</Text>}
+            </TextAlign>
+          </MaxWidth>
         </MainContentLayout>
 
         <Footer
