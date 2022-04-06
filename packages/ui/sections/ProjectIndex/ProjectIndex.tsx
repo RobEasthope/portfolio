97f62d3c91e205f0c147dff4957706b335b3dea4
Rob Easthope <hello@robeasthope.com>
2022-04-06 19:02:09 +0100

    Add links

--- packages/ui/sections/ProjectIndex/ProjectIndex.tsx
+++ packages/ui/sections/ProjectIndex/ProjectIndex.tsx
@@ -1,4 +1,5 @@
 import { ProjectIndex as rawProjectIndexProps } from '@/UI/types/sanity-schema';
+import Link from 'next/link';
 import { MaxWidth } from '@/UI/base/layout/MaxWidth/MaxWidth';
 import { Box } from '@/UI/base/layout/Box/Box';
 import { Text } from '@/UI/base/typography/Text/Text';
@@ -16,7 +17,13 @@ export const ProjectIndex = ({ projects }: ProjectIndexProps) => (
       {projects?.length > 0 &&
         projects?.map((project) => (
           <li key={project?._id}>
-            {project.title && <Text>{project.title}</Text>}
+            {project?.slug && (
+              <Text>
+                <Link href={project?.slug?.current}>
+                  <a>{project.title}</a>
+                </Link>
+              </Text>
+            )}
           </li>
         ))}
     </MaxWidth>
