147123c18f6b85276cb0609f8e98b47999df664b
Rob Easthope <hello@robeasthope.com>
2022-04-07 13:43:31 +0100

    Add thumbnail image

--- packages/ui/sections/ProjectIndex/ProjectIndex.tsx
+++ packages/ui/sections/ProjectIndex/ProjectIndex.tsx
@@ -1,9 +1,11 @@
 import { ProjectIndex as rawProjectIndexProps } from '@/UI/types/sanity-schema';
 import Link from 'next/link';
 import { MaxWidth } from '@/UI/base/layout/MaxWidth/MaxWidth';
+import { Flex } from '@/UI/base/layout/Flex/Flex';
 import { Box } from '@/UI/base/layout/Box/Box';
 import { Text } from '@/UI/base/typography/Text/Text';
 import { ProjectProps } from '@/UI/layouts/Project/Project';
+import { Picture } from '@/UI/base/media/Picture/Picture';
 import { styled } from '@/UI/styles/stitches.config';
 
 // STYLES
@@ -11,6 +13,20 @@ export const Index = styled(Box, {
   listStyle: 'none',
   marginY: 0,
   paddingX: 0,
+});
+
+export const Card = styled(Flex, {
+  marginY: '$half',
+});
+
+export const Thumbnail = styled(Picture, {
+  borderRadius: '50%',
+  width: '40px',
+  marginRight: '$half',
+
+  '& img': {
+    borderRadius: '50%',
+  },
 });
 
 export const ProjectTitle = styled(Text, {
@@ -32,15 +48,24 @@ export const ProjectIndex = ({ projects }: ProjectIndexProps) => (
       {projects?.length > 0 && (
         <Index as="ul">
           {projects?.map((project) => (
-            <li key={project?._id}>
+            <Card as="li" key={project?._id} align="center">
+              {project?.thumbnailImage && (
+                <Thumbnail
+                  asset={project?.thumbnailImage}
+                  alt={project?.title || ''}
+                  mode="responsive"
+                  maxWidth={200}
+                />
+              )}
+
               {project?.slug && (
                 <ProjectTitle>
                   <Link href={project?.slug?.current}>
-                    <a>{project.title}</a>
+                    <a>{project?.title}</a>
                   </Link>
                 </ProjectTitle>
               )}
-            </li>
+            </Card>
           ))}
         </Index>
       )}
