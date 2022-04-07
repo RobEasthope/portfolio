4f02bba144450639a9d9bb06dfb831d69302ef6a
Rob Easthope <hello@robeasthope.com>
2022-04-07 14:50:02 +0100

    Layout projects in three columns

--- packages/ui/sections/ProjectIndex/ProjectIndex.tsx
+++ packages/ui/sections/ProjectIndex/ProjectIndex.tsx
@@ -2,14 +2,16 @@ import { ProjectIndex as rawProjectIndexProps } from '@/UI/types/sanity-schema';
 import Link from 'next/link';
 import { MaxWidth } from '@/UI/base/layout/MaxWidth/MaxWidth';
 import { Flex } from '@/UI/base/layout/Flex/Flex';
+import { Grid } from '@/UI/base/layout/Grid/Grid';
 import { Box } from '@/UI/base/layout/Box/Box';
 import { Text } from '@/UI/base/typography/Text/Text';
 import { ProjectProps } from '@/UI/layouts/Project/Project';
 import { Picture } from '@/UI/base/media/Picture/Picture';
 import { styled } from '@/UI/styles/stitches.config';
+import { createGroups } from '@/UI/utils/createGroups';
 
 // STYLES
-export const Index = styled(Box, {
+export const Index = styled(Flex, {
   listStyle: 'none',
   marginY: 0,
   paddingX: 0,
@@ -38,40 +40,52 @@ export interface ProjectIndexProps extends rawProjectIndexProps {
 }
 
 // MARKUP
-export const ProjectIndex = ({ projects }: ProjectIndexProps) => (
-  <Box>
-    <MaxWidth width="page">
-      {projects?.length > 0 && (
-        <Index as="ul">
-          {projects?.map((project) => (
-            <Flex
-              as="li"
-              key={project?._id}
-              align="center"
-              css={{ marginY: '$twoThirds' }}
-            >
-              {project?.thumbnailImage && (
-                <Thumbnail
-                  asset={project?.thumbnailImage}
-                  alt={project?.title || ''}
-                  mode="responsive"
-                  maxWidth={48}
-                />
-              )}
+export const ProjectIndex = ({ projects }: ProjectIndexProps) => {
+  const splitProjects: [ProjectProps[]] = createGroups({
+    arr: projects,
+    numGroups: 3,
+  });
 
-              {project?.slug && (
-                <ProjectTitle>
-                  <Link href={project?.slug?.current}>
-                    <a>{project?.title}</a>
-                  </Link>
-                </ProjectTitle>
-              )}
-            </Flex>
-          ))}
-        </Index>
-      )}
-    </MaxWidth>
-  </Box>
-);
+  return (
+    <Box>
+      <MaxWidth width="page">
+        {splitProjects?.length > 0 && (
+          <Index as="ul">
+            {splitProjects?.map((grouping) => (
+              <Box as="ul" css={{ width: 'calc(100% / 3)' }}>
+                {grouping.map((project) => (
+                  <Flex
+                    as="li"
+                    key={project?._id}
+                    align="center"
+                    css={{ marginY: '$twoThirds' }}
+                    direction="row"
+                  >
+                    {project?.thumbnailImage && (
+                      <Thumbnail
+                        asset={project?.thumbnailImage}
+                        alt={project?.title || ''}
+                        mode="responsive"
+                        maxWidth={48}
+                      />
+                    )}
+
+                    {project?.slug && (
+                      <ProjectTitle>
+                        <Link href={project?.slug?.current}>
+                          <a>{project?.title}</a>
+                        </Link>
+                      </ProjectTitle>
+                    )}
+                  </Flex>
+                ))}
+              </Box>
+            ))}
+          </Index>
+        )}
+      </MaxWidth>
+    </Box>
+  );
+};
 
 export default ProjectIndex;
