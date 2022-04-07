7734486ec20dc7af14a55b4adf39aef4cdbd733d
Rob Easthope <hello@robeasthope.com>
2022-04-07 23:09:05 +0100

    Create SPACER_OPTIONS.ts

---
+++ packages/ui/constants/SPACER_OPTIONS.ts
@@ -0,0 +1,11 @@
+export const SPACER_OPTIONS = [
+  { title: 'Small', value: 'quarter' },
+  { title: 'Medium', value: 'third' },
+  { title: 'Half', value: 'half' },
+  { title: 'Two thirds', value: 'twoThirds' },
+  { title: 'Single/x1', value: 'x1' },
+  { title: 'Double/x2', value: 'x2' },
+  { title: 'Triple/x3', value: 'x3' },
+  { title: 'Quadruple/x4', value: 'x4' },
+  { title: 'Quintuple/x5', value: 'x5' },
+];
