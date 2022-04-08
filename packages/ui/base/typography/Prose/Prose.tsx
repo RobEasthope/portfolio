1e2a446826da78d7f6c5d9f93e81dd9e703fff72
Rob Easthope <hello@robeasthope.com>
2022-04-08 13:02:31 +0100

    Add responsive styles to Prose

--- packages/ui/base/typography/Prose/Prose.tsx
+++ packages/ui/base/typography/Prose/Prose.tsx
@@ -1,11 +1,27 @@
 import { SPACING } from '@/UI/constants/SPACING';
 import { styled } from '@/UI/styles/stitches.config';
+import { TYPE_SCALE } from '@/UI/styles/constants/TYPE_SCALE';
+import { MAX_WIDTH } from '@/UI/constants/MAX_WIDTHS';
 
 export const Prose = styled('div', {
   display: 'flex',
   justifyContent: 'center',
   flexWrap: 'wrap',
   marginX: 'auto',
+
+  // Minimum font fontSize
+  fontSize: `${TYPE_SCALE.STANDARD.MIN}px`,
+
+  // Dynamic font fontSize
+  '@pageMinWidth': {
+    fontSize: `calc(${TYPE_SCALE.STANDARD.MIN}px + (${TYPE_SCALE.STANDARD.MAX} - ${TYPE_SCALE.STANDARD.MIN}) * ((100vw - ${MAX_WIDTH.TYPE_LOWER_LIMIT}px) / (${MAX_WIDTH.TYPE_UPPER_LIMIT} - ${MAX_WIDTH.TYPE_LOWER_LIMIT})))`,
+  },
+
+  // Maximum font fontSize
+  '@pageMaxWidth': {
+    fontSize: `${TYPE_SCALE.STANDARD.MAX}px`,
+  },
+  lineHeight: SPACING.VR_BASE,
 
   '> div > *:not(figure):not(section)': {
     position: 'relative',
