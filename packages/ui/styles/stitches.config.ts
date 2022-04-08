69ed58a0383ebdba783f87479f7c94f3b526ac9c
Rob Easthope <hello@robeasthope.com>
2022-04-08 13:14:28 +0100

    Add Text and highlight colors

--- packages/ui/styles/stitches.config.ts
+++ packages/ui/styles/stitches.config.ts
@@ -15,6 +15,9 @@ export const { css, styled, globalCss, getCssText } = createStitches({
       hiContrast: 'hsl(206,10%,5%)',
       loContrast: 'white',
 
+      text: 'rgb(36, 36, 36)',
+      highlight: 'rgb(229, 40, 20)',
+
       gray100: 'hsl(206,22%,99%)',
       gray200: 'hsl(206,12%,97%)',
       gray300: 'hsl(206,11%,92%)',
