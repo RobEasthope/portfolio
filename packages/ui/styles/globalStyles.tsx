83fa55015cb05875a22173098ed316abee55cb62
Rob Easthope <hello@robeasthope.com>
2022-04-08 17:46:12 +0100

    Switch back to use constants

--- packages/ui/styles/globalStyles.tsx
+++ packages/ui/styles/globalStyles.tsx
@@ -9,16 +9,16 @@ export const globalStyles = globalCss({
 
   html: {
     // Minimum font fontSize
-    fontSize: `16px`,
+    fontSize: `${TYPE_SCALE.STANDARD.MIN}px`,
 
     // Dynamic font fontSize
     '@pageMinWidth': {
-      fontSize: `calc(16px + (18 - 16) * ((100vw - ${MAX_WIDTH.TYPE_LOWER_LIMIT}px) / (${MAX_WIDTH.TYPE_UPPER_LIMIT} - ${MAX_WIDTH.TYPE_LOWER_LIMIT})))`,
+      fontSize: `calc(${TYPE_SCALE.STANDARD.MIN}px + (${TYPE_SCALE.STANDARD.MAX} - ${TYPE_SCALE.STANDARD.MIN}) * ((100vw - ${MAX_WIDTH.TYPE_LOWER_LIMIT}px) / (${MAX_WIDTH.TYPE_UPPER_LIMIT} - ${MAX_WIDTH.TYPE_LOWER_LIMIT})))`,
     },
 
     // Maximum font fontSize
     '@pageMaxWidth': {
-      fontSize: `18px`,
+      fontSize: `${TYPE_SCALE.STANDARD.MAX}px`,
     },
   },
 });
