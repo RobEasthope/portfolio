f7395dcc891663180144eea8b61fdbae61ef30a7
Rob Easthope <hello@robeasthope.com>
2022-04-12 14:36:47 +0100

    Delete PagePadding.tsx

--- packages/ui/base/layout/PagePadding/PagePadding.tsx
+++
@@ -1,7 +0,0 @@
-import { styled } from '~/UI/styles/stitches.config';
-import { Box } from '~/UI/base/layout/Box/Box';
-
-export const PagePadding = styled(Box, {
-  marginX: '$x2',
-  marginTop: '$x3',
-});
