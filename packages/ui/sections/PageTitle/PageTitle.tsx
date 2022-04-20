f1370fba6461181049e3fb1f7038cc44d22f059d
Rob Easthope <hello@robeasthope.com>
2022-04-20 02:02:53 +0100

    Add Break to page title section

--- packages/ui/sections/PageTitle/PageTitle.tsx
+++ packages/ui/sections/PageTitle/PageTitle.tsx
@@ -2,6 +2,18 @@ import { MaxWidth } from '~/UI/base/structure/MaxWidth/MaxWidth';
 import { PageTitle as rawPageTitleProps } from '~/UI/types/sanity-schema';
 import { PaddedComponent } from '~/UI/base/structure/PaddedComponent/PaddedComponent';
 import { Text } from '~/UI/base/typography/Text/Text';
+import { TextAlign } from '~/UI/base/structure/TextAlign/TextAlign';
+import { styled } from '~/UI/styles/stitches.config';
+import { Spacer } from '~/UI/sections/Spacer/Spacer';
+
+// STYLES
+export const Break = styled('span', {
+  display: 'block',
+  width: '$x2',
+  height: '2px',
+  marginX: 'auto',
+  backgroundColor: 'RGBA(250, 145, 46, 1)',
+});
 
 // TYPES
 export interface PageTitleProps extends rawPageTitleProps {
@@ -17,7 +29,13 @@ export const PageTitle = ({ pageTitle, customPageTitle }: PageTitleProps) => {
     <PaddedComponent as="section" content="text">
       <MaxWidth width="page">
         {(pageTitle || customPageTitle) && (
-          <Text typeSize="medium">{customPageTitle || pageTitle}</Text>
+          <TextAlign align="centre">
+            <Text typeSize="medium">{customPageTitle || pageTitle}</Text>
+
+            <Spacer height="x1" />
+            <Break />
+            <Spacer height="x2" />
+          </TextAlign>
         )}
       </MaxWidth>
     </PaddedComponent>
