7d57809ed933ef3658c8d26d710a2f623db9a15a
Rob Easthope <hello@robeasthope.com>
2022-04-20 01:17:53 +0100

    Create PageTitle.schema.js

---
+++ packages/ui/sections/PageTitle/PageTitle.schema.js
@@ -0,0 +1,22 @@
+import { BiText } from 'react-icons/bi';
+
+export default {
+  type: 'object',
+  name: 'PageTitle',
+  title: 'PageTitle',
+  icon: BiText,
+  fields: [
+    {
+      name: 'customPageTitle',
+      title: 'Custom pagetitle',
+      type: 'string',
+    },
+  ],
+  preview: {
+    prepare() {
+      return {
+        title: 'customPageTitle' || 'Page Title',
+      };
+    },
+  },
+};
