cc4b49a9e2bb8327a19aa32a3ff4e9a91c02bcc5
Rob Easthope <hello@robeasthope.com>
2023-03-11 16:00:44 +0000

    Fix RenderSections

--- packages/ui-pkg/utils/RenderSections/RenderSections.tsx
+++ packages/ui-pkg/utils/RenderSections/RenderSections.tsx
@@ -43,99 +43,98 @@ export const RenderSections = ({
     return <div>Missing sections</div>;
   }
 
-  return (
-    <>
-      {sections?.map((section) => {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        const { _type } = section;
+  const renderBlocks = () =>
+    sections?.map((section) => {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const { _type } = section;
 
-        switch (_type) {
-          case "ExampleSection":
-            return (
-              <ExampleSection
-                {...(section as ExampleSectionProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "Gallery":
-            return (
-              <Gallery
-                {...(section as GalleryProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "Image":
-            return (
-              <Image
-                {...(section as ImageProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "LandingHero":
-            return (
-              <LandingHero
-                {...(section as LandingHeroProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "MuxVideo":
-            return (
-              <MuxVideo
-                {...(section as MuxVideoProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "PageTitle":
-            return (
-              <PageTitle
-                {...(section as PageTitleProps)}
-                key={`render-sections-${section._key as string}`}
-                pageTitle={pageTitle}
-              />
-            );
-          case "ProjectIndex":
-            return (
-              <ProjectIndex
-                {...(section as ProjectIndexProps)}
-                key={`render-sections-${section._key as string}`}
-                projects={projects}
-              />
-            );
-          case "Spacer":
-            return (
-              <Spacer
-                {...(section as SpacerProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "Text":
-            return (
-              <Text
-                {...(section as TextProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "VimeoVideo":
-            return (
-              <VimeoVideo
-                {...(section as VimeoVideoProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
-          case "YoutubeVideo":
-            return (
-              <YoutubeVideo
-                {...(section as YoutubeVideoProps)}
-                key={`render-sections-${section._key as string}`}
-              />
-            );
+      switch (_type) {
+        case "ExampleSection":
+          return (
+            <ExampleSection
+              {...(section as ExampleSectionProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "Gallery":
+          return (
+            <Gallery
+              {...(section as GalleryProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "Image":
+          return (
+            <Image
+              {...(section as ImageProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "LandingHero":
+          return (
+            <LandingHero
+              {...(section as LandingHeroProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "MuxVideo":
+          return (
+            <MuxVideo
+              {...(section as MuxVideoProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "PageTitle":
+          return (
+            <PageTitle
+              {...(section as PageTitleProps)}
+              key={`render-sections-${section._key as string}`}
+              pageTitle={pageTitle}
+            />
+          );
+        case "ProjectIndex":
+          return (
+            <ProjectIndex
+              {...(section as ProjectIndexProps)}
+              key={`render-sections-${section._key as string}`}
+              projects={projects}
+            />
+          );
+        case "Spacer":
+          return (
+            <Spacer
+              {...(section as SpacerProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "Text":
+          return (
+            <Text
+              {...(section as TextProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "VimeoVideo":
+          return (
+            <VimeoVideo
+              {...(section as VimeoVideoProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
+        case "YoutubeVideo":
+          return (
+            <YoutubeVideo
+              {...(section as YoutubeVideoProps)}
+              key={`render-sections-${section._key as string}`}
+            />
+          );
 
-          default:
-            return (
-              <div key={section?._key as string}>Missing section {section?._type}</div>
-            );
-        }
-      })}
-    </>
-  );
+        default:
+          return (
+            <div key={section?._key as string}>Missing section {section?._type}</div>
+          );
+      }
+    });
+
+  return <>{renderBlocks()}</>;
 };
