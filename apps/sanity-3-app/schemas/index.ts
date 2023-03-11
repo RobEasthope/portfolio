1100256bfd4c629f4f970097d85875163a9b5471
Rob Easthope <hello@robeasthope.com>
2023-03-11 17:02:42 +0000

    Switch to NPM paths

--- apps/sanity-3-app/schemas/index.ts
+++ apps/sanity-3-app/schemas/index.ts
@@ -1,51 +1,51 @@
 //Links
-import internalLink from '../../packages/ui-pkg/base/links/InternalLink/InternalLink.schema.js'
-import internalLinkWithTitle from '../../packages/ui-pkg/base/links/InternalLink/InternalLinkWithTitle.schema.js'
-import externalLink from '../../packages/ui-pkg/base/links/ExternalLink/ExternalLink.schema.js'
-import externalLinkWithTitle from '../../packages/ui-pkg/base/links/ExternalLink/ExternalLinkWithTitle.schema.js'
+import internalLink from 'ui-pkg/base/links/InternalLink/InternalLink.schema.js'
+import internalLinkWithTitle from 'ui-pkg/base/links/InternalLink/InternalLinkWithTitle.schema.js'
+import externalLink from 'ui-pkg/base/links/ExternalLink/ExternalLink.schema.js'
+import externalLinkWithTitle from 'ui-pkg/base/links/ExternalLink/ExternalLinkWithTitle.schema.js'
 
 // Navigation
-import Header from '../../packages/ui-pkg/navigation/Header/Header.schema.js'
-import Footer from '../../packages/ui-pkg/navigation/Footer/Footer.schema.js'
+import Header from 'ui-pkg/navigation/Header/Header.schema.js'
+import Footer from 'ui-pkg/navigation/Footer/Footer.schema.js'
 
 // Media
-import altImage from '../../packages/ui-pkg/base/media/Picture/altImage.schema'
+import altImage from 'ui-pkg/base/media/Picture/altImage.schema'
 
 // Formatted text
-import blockContent from '../../packages/ui-pkg/base/formatted-text/ExampleFormattedText/BlockContent.schema'
-import ProjectText from '../../packages/ui-pkg/base/formatted-text/ProjectText/ProjectText.schema'
-import BasicText from '../../packages/ui-pkg/base/formatted-text/BasicText/BasicText.schema'
-import ExampleText from '../../packages/ui-pkg/base/formatted-text/ExampleFormattedText/ExampleFormattedText.schema'
+import blockContent from 'ui-pkg/base/formatted-text/ExampleFormattedText/BlockContent.schema'
+import ProjectText from 'ui-pkg/base/formatted-text/ProjectText/ProjectText.schema'
+import BasicText from 'ui-pkg/base/formatted-text/BasicText/BasicText.schema'
+import ExampleText from 'ui-pkg/base/formatted-text/ExampleFormattedText/ExampleFormattedText.schema'
 
 // Settings
-import details from '../../packages/ui-pkg/base/settings/Details.schema'
-import Settings from '../../packages/ui-pkg/base/settings/Settings.schema'
-import GlobalMetadata from '../../packages/ui-pkg/base/settings/GlobalMetadata.schema'
+import details from 'ui-pkg/base/settings/Details.schema'
+import Settings from 'ui-pkg/base/settings/Settings.schema'
+import GlobalMetadata from 'ui-pkg/base/settings/GlobalMetadata.schema'
 
 // Data
-import tech from '../../packages/ui-pkg/sections/Tech/schemas/Tech.schema'
-import gig from '../../packages/ui-pkg/sections/Gig/schemas/Gig.schema'
-import project from '../../packages/ui-pkg/pages/Project/Project.schema'
-import organisation from '../../packages/ui-pkg/sections/Organisation/schemas/Organisation.schema'
-import education from '../../packages/ui-pkg/sections/EducationBlock/schemas/Education.schema'
+import tech from 'ui-pkg/sections/Tech/schemas/Tech.schema'
+import gig from 'ui-pkg/sections/Gig/schemas/Gig.schema'
+import project from 'ui-pkg/pages/Project/Project.schema'
+import organisation from 'ui-pkg/sections/Organisation/schemas/Organisation.schema'
+import education from 'ui-pkg/sections/EducationBlock/schemas/Education.schema'
 
 // Content UI
-import ExampleSection from '../../packages/ui-pkg/sections/ExampleSection/ExampleSection.schema'
-import GalleryImage from '../../packages/ui-pkg/sections/Gallery/schemas/GalleryImage.schema'
-import Gallery from '../../packages/ui-pkg/sections/Gallery/Gallery.schema'
-import Image from '../../packages/ui-pkg/sections/Image/Image.schema'
+import ExampleSection from 'ui-pkg/sections/ExampleSection/ExampleSection.schema'
+import GalleryImage from 'ui-pkg/sections/Gallery/schemas/GalleryImage.schema'
+import Gallery from 'ui-pkg/sections/Gallery/Gallery.schema'
+import Image from 'ui-pkg/sections/Image/Image.schema'
 
-import LandingHero from '../../packages/ui-pkg/sections/LandingHero/LandingHero.schema'
-import MuxVideo from '../../packages/ui-pkg/sections/MuxVideo/MuxVideo.schema'
-import PageTitle from '../../packages/ui-pkg/sections/PageTitle/PageTitle.schema'
-import ProjectIndex from '../../packages/ui-pkg/sections/ProjectIndex/ProjectIndex.schema'
-import Spacer from '../../packages/ui-pkg/sections/Spacer/Spacer.schema'
-import Text from '../../packages/ui-pkg/sections/Text/Text.schema'
-import VimeoVideo from '../../packages/ui-pkg/sections/VimeoVideo/VimeoVideo.schema'
-import YoutubeVideo from '../../packages/ui-pkg/sections/YoutubeVideo/YoutubeVideo.schema'
+import LandingHero from 'ui-pkg/sections/LandingHero/LandingHero.schema'
+import MuxVideo from 'ui-pkg/sections/MuxVideo/MuxVideo.schema'
+import PageTitle from 'ui-pkg/sections/PageTitle/PageTitle.schema'
+import ProjectIndex from 'ui-pkg/sections/ProjectIndex/ProjectIndex.schema'
+import Spacer from 'ui-pkg/sections/Spacer/Spacer.schema'
+import Text from 'ui-pkg/sections/Text/Text.schema'
+import VimeoVideo from 'ui-pkg/sections/VimeoVideo/VimeoVideo.schema'
+import YoutubeVideo from 'ui-pkg/sections/YoutubeVideo/YoutubeVideo.schema'
 
 // Pages
-import page from '../../packages/ui-pkg/pages/Page/Page.schema'
+import page from 'ui-pkg/pages/Page/Page.schema'
 
 export const schemaTypes = [
   // Links
