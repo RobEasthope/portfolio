e486b68ef2419d17bfec58f2c9809d9cabedecf4
Rob Easthope <hello@robeasthope.com>
2023-03-11 18:18:20 +0000

    Create ALL_PAGES_TYPES.ts

---
+++ packages/ui-pkg/pages/ALL_PAGES_TYPES.ts
@@ -0,0 +1,4 @@
+import Page from "ui-pkg/pages/Page/Page.schema";
+import Project from "ui-pkg/pages/Project/Project.schema";
+
+export const ALL_PAGES_TYPES = [{ type: Page.name }, { type: Project.name }];
