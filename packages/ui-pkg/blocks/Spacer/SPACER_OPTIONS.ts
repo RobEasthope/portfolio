f5025516ae864629d737bf2e25b2b3aca4c71780
Rob Easthope <hello@robeasthope.com>
2023-03-13 22:15:54 +0000

    Create SPACER_OPTIONS.ts

---
+++ packages/ui-pkg/blocks/Spacer/SPACER_OPTIONS.ts
@@ -0,0 +1,18 @@
+export const SPACER_OPTIONS = [
+  { title: "1", value: "4" },
+  { title: "2", value: "8" },
+  { title: "3", value: "12" },
+  { title: "4", value: "16" },
+  { title: "5", value: "20" },
+  { title: "6", value: "24" },
+  { title: "7", value: "28" },
+  { title: "8", value: "32" },
+  { title: "9", value: "36" },
+  { title: "10", value: "40" },
+  { title: "11", value: "44" },
+  { title: "12", value: "48" },
+  { title: "13", value: "52" },
+  { title: "14", value: "56" },
+  { title: "15", value: "60" },
+  { title: "16", value: "64" },
+];
