f510f957e80ab76345650b6bda0b0ebad93edbe8
Rob Easthope <hello@robeasthope.com>
2023-04-10 21:58:40 +0100

    Update Page markup

--- packages/ui-pkg/pages/Page/Page.tsx
+++ packages/ui-pkg/pages/Page/Page.tsx
@@ -1,13 +1,10 @@
-import { Footer } from "ui-pkg/navigation/Footer/Footer";
-import { Header } from "ui-pkg/navigation/Header/Header";
-import { FixedFooterLayout } from "ui-pkg/base/structure/FixedFooterLayout/FixedFooterLayout";
-import { MainContentLayout } from "ui-pkg/base/structure/MainContentLayout/MainContentLayout";
 import { NextMetadata } from "ui-pkg/base/app/Metadata/NextMetadata";
 import { RenderSections } from "ui-pkg/utils/RenderSections/RenderSections";
 import { Page as rawPageProps } from "ui-pkg/types/sanity-schema";
 import { ExampleSectionProps } from "ui-pkg/blocks/ExampleSection/ExampleSection";
-import { AppGlobalsProps } from "ui-pkg/base/settings/Globals";
+import { AppGlobalsProps } from "ui-pkg/settings/Globals";
 import { ProjectProps } from "ui-pkg/pages/Project/Project";
+import { BasicLayout } from "ui-pkg/layouts/BasicLayout/BasicLayout";
 
 export interface PageProps extends rawPageProps {
   blocks: [ExampleSectionProps];
@@ -30,33 +27,10 @@ export const Page = ({
   const { blocks, projects, slug } = page;
 
   return (
-    <>
-      <NextMetadata
-        title={page?.metadataTitle || page?.title}
-        description={page?.metadataDescription}
-        image={page?.metadataImage}
-        globalMetadata={metadata}
-      />
-
-      <FixedFooterLayout>
-        {header && (
-          <Header
-            logo={header?.logo}
-            navigationLeft={header?.navigationLeft}
-            navigationRight={header?.navigationRight}
-          />
-        )}
-
-        <MainContentLayout as="main">
-          {blocks && (
-            <RenderSections blocks={blocks} projects={projects} pageTitle={page?.title} />
-          )}
-        </MainContentLayout>
-
-        {slug?.current !== homePageSlug && (
-          <Footer copyrightText={footer?.copyrightText} />
-        )}
-      </FixedFooterLayout>
-    </>
+    <BasicLayout header={header} footer={footer}>
+      {blocks && (
+        <RenderSections blocks={blocks} projects={projects} pageTitle={page?.title} />
+      )}
+    </BasicLayout>
   );
 };
