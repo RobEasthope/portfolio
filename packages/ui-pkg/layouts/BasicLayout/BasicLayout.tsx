ad3f94d91b0a4c672fa2a5e6cea72ecf5d43694f
Rob Easthope <hello@robeasthope.com>
2023-04-10 18:44:36 +0100

    Copy in BasicLayout

---
+++ packages/ui-pkg/layouts/BasicLayout/BasicLayout.tsx
@@ -0,0 +1,31 @@
+import { Footer, FooterProps } from "ui-pkg/navigation/Footer/Footer";
+import { Header, HeaderProps } from "ui-pkg/navigation/Header/Header";
+import { Box } from "ui-pkg/base/Box/Box";
+
+export type BasicLayoutProps = {
+  children: React.ReactNode;
+  header: HeaderProps;
+  footer: FooterProps;
+};
+
+export const BasicLayout = ({ children, header, footer }: BasicLayoutProps) => (
+  <Box as="div" className="flex min-h-screen w-full flex-col bg-white">
+    <Header
+      logo={header?.logo}
+      primaryNavigation={header?.primaryNavigation}
+      secondaryNavigation={header?.secondaryNavigation}
+    />
+
+    <Box as="main" className="flex-grow">
+      {children}
+    </Box>
+
+    <Footer
+      footerNavigation={footer?.footerNavigation}
+      copyrightText={footer?.copyrightText}
+      socialMedia={footer?.socialMedia}
+      bhpaLogo={footer?.bhpaLogo}
+      bhpaWebsiteUrl={footer?.bhpaWebsiteUrl}
+    />
+  </Box>
+);
