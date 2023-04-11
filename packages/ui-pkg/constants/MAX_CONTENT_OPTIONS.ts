ec9fd2de3c28068013b050bde062233fb8662285
Rob Easthope <hello@robeasthope.com>
2023-04-11 10:39:07 +0100

    Trim and update config/constants

--- packages/ui-pkg/constants/MAX_CONTENT_OPTIONS.ts
+++
@@ -1,6 +0,0 @@
-export const MAX_CONTENT_OPTIONS = [
-  { title: "Small", value: "small" },
-  { title: "Medium", value: "medium" },
-  { title: "Large", value: "large" },
-  { title: "Full width", value: "full" },
-];
