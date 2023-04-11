7ed9e312d033c21c7e76d98dc32cfee4a4ebddc6
Rob Easthope <hello@robeasthope.com>
2023-04-11 10:16:19 +0100

    Initial work refactoring Project markup

--- packages/ui-pkg/pages/Project/Project.tsx
+++ packages/ui-pkg/pages/Project/Project.tsx
@@ -1,38 +1,15 @@
-import { Footer } from "ui-pkg/navigation/Footer/Footer";
-import { Header } from "ui-pkg/navigation/Header/Header";
-import { FixedFooterLayout } from "ui-pkg/base/structure/FixedFooterLayout/FixedFooterLayout";
-import { MainContentLayout } from "ui-pkg/base/structure/MainContentLayout/MainContentLayout";
-import { NextMetadata } from "ui-pkg/base/app/Metadata/NextMetadata";
 import { Organisation, Project as rawProjectProps } from "ui-pkg/types/sanity-schema";
-import { ExampleSectionProps } from "ui-pkg/blocks/ExampleSection/ExampleSection";
 import { AppGlobalsProps } from "ui-pkg/base/settings/Globals";
-import { Text } from "ui-pkg/base/typography/Text/Text";
-import { ProjectText } from "ui-pkg/base/formatted-text/ProjectText/ProjectText";
-import { MaxWidth } from "ui-pkg/base/structure/MaxWidth/MaxWidth";
-import { Box } from "ui-pkg/base/structure/Box/Box";
-import { Flex } from "ui-pkg/base/structure/Flex/Flex";
-import { TextAlign } from "ui-pkg/base/structure/TextAlign/TextAlign";
-import { Picture } from "ui-pkg/base/media/Picture/Picture";
+import { ProjectText } from "ui-pkg/base/Prose/components/ProjectText/ProjectText";
+import { Box } from "ui-pkg/base/Box/Box";
 
 import { styled } from "ui-pkg/styles/stitches.config";
 import { Spacer } from "ui-pkg/blocks/Spacer/Spacer";
-import { ExternalLink } from "ui-pkg/base/links/ExternalLink/ExternalLink";
+import { ExternalLink } from "ui-pkg/base/ExternalLink/ExternalLink";
 import { BsDot } from "react-icons/bs";
-
-export const ProjectLogo = styled(Picture, {
-  borderRadius: "$circle",
-  xySize: "100px",
-
-  "& img": {
-    borderRadius: "$circle",
-    width: "100%",
-    height: "100%",
-  },
-});
-
-export const Dot = styled(BsDot, {
-  height: "$x1",
-});
+import { BasicLayout } from "ui-pkg/layouts/BasicLayout/BasicLayout";
+import { Type } from "base/Type/Type";
+import { SanityImage } from "base/SanityImage/SanityImage";
 
 export interface ProjectProps extends rawProjectProps {
   blocks: [ExampleSectionProps];
@@ -41,128 +18,72 @@ export interface ProjectProps extends rawProjectProps {
 }
 
 export const Project = ({
-  project,
+  page,
   globals,
 }: {
   project: ProjectProps;
   globals: AppGlobalsProps;
-}) => {
-  // Globals props
-  const { header, footer, metadata } = globals;
+}) => (
+  <BasicLayout header={globals?.header} footer={globals?.footer}>
+    <Box as="div" className="py-2">
+      <Box as="div" className="text-center mb-1">
+        <SanityImage
+          asset={page?.thumbnailImage}
+          alt={page?.title || ""}
+          mode="responsive"
+          maxWidth={200}
+          aspectRatio={1}
+          className="mb-1"
+        />
 
-  // Page props
-  const {
-    title,
-    description,
-    clientOrg,
-    agencyOrg,
-    projectText,
-    projectUrlTitle,
-    projectUrl,
-    repoUrl,
-    date,
-    thumbnailImage,
-  } = project;
+        <Type as="h1" className="text-2xl mb-1">
+          {page?.title}
+        </Type>
 
-  return (
-    <>
-      <NextMetadata
-        title={project?.title}
-        description={project?.description}
-        image={project?.thumbnailImage}
-        globalMetadata={metadata}
-      />
+        <Type as="p" className="mb-1">
+          {page?.description}
+        </Type>
 
-      <FixedFooterLayout>
-        {header && (
-          <Header
-            logo={header?.logo}
-            navigationLeft={header?.navigationLeft}
-            navigationRight={header?.navigationRight}
-          />
-        )}
+        <Box as="section" className="flex gap-1 justify-center">
+          {page?.clientOrg && (
+            <Type as="p">
+              <Box as="span">Client: </Box>
+              <ExternalLink href={page?.clientOrg?.url || null}>
+                {page?.clientOrg?.name}
+              </ExternalLink>
+            </Type>
+          )}
+          {page?.agencyOrg && (
+            <Type as="p">
+              <Box as="span">Agency: </Box>
+              <ExternalLink href={page?.agencyOrg?.url || null}>
+                {page?.agencyOrg?.name}
+              </ExternalLink>
+            </Type>
+          )}
+        </Box>
+        <Spacer height="1" />
 
-        <MainContentLayout as="main">
-          <Spacer height="x2" />
+        <BsDot className="h-1" />
+      </Box>
 
-          <MaxWidth width="text">
-            <TextAlign align="centre" css={{ marginBottom: "$x1" }}>
-              {thumbnailImage && (
-                <Flex justify="center" css={{ marginBottom: "$x1" }}>
-                  <ProjectLogo
-                    asset={thumbnailImage}
-                    alt={title || ""}
-                    mode="responsive"
-                    maxWidth={200}
-                    aspectRatio={1}
-                  />
-                </Flex>
-              )}
+      {page?.projectText && <ProjectText blocks={page?.projectText as unknown} />}
 
-              {title && (
-                <Text typeSize="large" css={{ marginBottom: "$x1" }}>
-                  {title}
-                </Text>
-              )}
+      <Box as="div" className="mt-1 text-center">
+        <BsDot className="h-1" />
 
-              {description && <Text css={{ marginBottom: "$x1" }}>{description}</Text>}
-
-              {(clientOrg || agencyOrg) && (
-                <Flex justify="center" gap="x1">
-                  {clientOrg && (
-                    <Text>
-                      <Box as="span" css={{ fontStyle: "italic" }}>
-                        Client:{" "}
-                      </Box>
-                      <ExternalLink href={clientOrg?.url || null}>
-                        {clientOrg?.name}
-                      </ExternalLink>
-                    </Text>
-                  )}
-                  {agencyOrg && (
-                    <Text>
-                      <Box as="span" css={{ fontStyle: "italic" }}>
-                        Agency:{" "}
-                      </Box>
-                      <ExternalLink href={agencyOrg?.url || null}>
-                        {agencyOrg?.name}
-                      </ExternalLink>
-                    </Text>
-                  )}
-                </Flex>
-              )}
-              <Spacer height="x1" />
-
-              <Dot />
-            </TextAlign>
-
-            {projectText && <ProjectText blocks={projectText as unknown} />}
-
-            {(projectUrl || repoUrl || date) && (
-              <TextAlign align="centre" css={{ marginTop: "$x1" }}>
-                <Dot />
-
-                {projectUrl && (
-                  <Text css={{ marginTop: "$x1" }}>
-                    <ExternalLink href={projectUrl}>
-                      {projectUrlTitle || projectUrl}
-                    </ExternalLink>
-                  </Text>
-                )}
-                {repoUrl && (
-                  <Text css={{ marginTop: "$x1" }}>
-                    <ExternalLink href={repoUrl}>{repoUrl}</ExternalLink>
-                  </Text>
-                )}
-                {date && <Text css={{ marginTop: "$x1" }}>{date}</Text>}
-              </TextAlign>
-            )}
-            <Spacer height="x2" />
-          </MaxWidth>
-        </MainContentLayout>
-
-        <Footer copyrightText={footer?.copyrightText} />
-      </FixedFooterLayout>
-    </>
-  );
-};
+        <Type as="p" className="mt-1">
+          <ExternalLink href={page?.projectUrl || null}>
+            {page?.projectUrlTitle || page?.projectUrl}
+          </ExternalLink>
+        </Type>
+        <Type as="p" className="mt-1">
+          <ExternalLink href={page?.repoUrl || null}>{page?.repoUrl}</ExternalLink>
+        </Type>
+        <Type as="p" className="mt-1">
+          {page?.date}
+        </Type>
+      </Box>
+    </Box>
+  </BasicLayout>
+);
