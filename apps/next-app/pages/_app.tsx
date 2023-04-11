6eff57009c8dde6b8eb9a254bfe2c386e4681bf3
Rob Easthope <hello@robeasthope.com>
2023-04-11 16:30:23 +0100

    Remove sanitize.css

--- apps/next-app/pages/_app.tsx
+++ apps/next-app/pages/_app.tsx
@@ -2,14 +2,6 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ReactNode } from "react";
 
-// Sanitize CSS
-import "sanitize.css";
-import "sanitize.css/typography.css";
-import "sanitize.css/assets.css";
-import "sanitize.css/forms.css";
-import "sanitize.css/system-ui.css";
-import "sanitize.css/reduce-motion.css";
-
 // Tailwind CSS
 import "styles/tailwind-globals.css";
 
