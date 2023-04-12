837e4cfa2ecc06656531a2cfcc41fec1be16ffd4
Rob Easthope <hello@robeasthope.com>
2023-04-12 15:11:10 +0100

    Remove old metadata

--- app/root.tsx
+++ app/root.tsx
@@ -21,13 +21,6 @@ export const meta: MetaFunction<typeof loader> = ({ data: { host } }) => ({
   charset: 'utf-8',
   title: 'Remix on Vercel Edge Functions',
   description: 'HTML, dynamically rendered in a city near you',
-  'twitter:card': 'summary_large_image',
-  'twitter:site': '@vercel',
-  'twitter:creator': '@vercel',
-  'twitter:title': 'Remix on Vercel Edge Functions',
-  'twitter:description': 'HTML, dynamically rendered in a city near you',
-  'twitter:image': `https://${host}/og-card.png`,
-  'twitter:image:alt': 'The Vercel and Remix logos',
   viewport: 'width=device-width,initial-scale=1',
 });
 
