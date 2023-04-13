5543714a67337be83f4c0ef2f1a837ce27ae9130
Rob Easthope <hello@robeasthope.com>
2023-04-13 11:28:44 +0100

    Ignore TS error

--- app/utils/sanity-js-api/getClient.ts
+++ app/utils/sanity-js-api/getClient.ts
@@ -4,5 +4,6 @@ import { sanityPreviewAPI } from '~/utils/sanity-js-api/sanityPreviewAPI';
 export const getClient = (props: { preview: boolean }) => {
   const { preview } = props;
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return preview ? sanityPreviewAPI : sanityAPI;
 };
