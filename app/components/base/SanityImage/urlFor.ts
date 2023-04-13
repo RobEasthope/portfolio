4789ca1774617ba68c29962b8ac1ac3247e885cc
Rob Easthope <hello@robeasthope.com>
2023-04-13 19:47:50 +0100

    Update fallbacks

--- app/components/base/SanityImage/urlFor.ts
+++ app/components/base/SanityImage/urlFor.ts
@@ -2,8 +2,8 @@ import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET! || 'production',
+  projectId: process.env.SANITY_PROJECT_ID || '',
+  dataset: process.env.SANITY_DATASET || '',
 });
 
 export function urlFor(source: SanityImageSource) {
