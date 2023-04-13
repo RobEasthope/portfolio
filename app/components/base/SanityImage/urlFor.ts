bd2598ac10bfb278631a300b50971b692298d906
Rob Easthope <hello@robeasthope.com>
2023-04-13 21:37:26 +0100

    Refactor urlFor

--- app/components/base/SanityImage/urlFor.ts
+++ app/components/base/SanityImage/urlFor.ts
@@ -1,11 +1,26 @@
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import imageUrlBuilder from '@sanity/image-url';
 
-const builder = imageUrlBuilder({
-  projectId: process.env.SANITY_PROJECT_ID || '',
-  dataset: process.env.SANITY_PROJECT_DATASET || '',
-});
+type ProjectDetails = {
+  projectId: string;
+  dataset: string;
+  apiVersion: string;
+};
 
-export function urlFor(source: SanityImageSource) {
-  return builder.image(source);
-}
+export const projectDetails = (): ProjectDetails => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const {
+    SANITY_PUBLIC_PROJECT_ID,
+    SANITY_PUBLIC_DATASET,
+    SANITY_PUBLIC_API_VERSION,
+  } = typeof document === 'undefined' ? process.env : window.ENV;
+
+  return {
+    projectId: String(SANITY_PUBLIC_PROJECT_ID),
+    dataset: String(SANITY_PUBLIC_DATASET),
+    apiVersion: String(SANITY_PUBLIC_API_VERSION),
+  };
+};
+
+export const urlFor = (source: SanityImageSource) =>
+  imageUrlBuilder(projectDetails()).image(source);
