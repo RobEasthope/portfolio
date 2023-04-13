6d1723efbd47826fc95ac16dd080f72931bb4178
Rob Easthope <hello@robeasthope.com>
2023-04-13 13:29:50 +0100

    Update env var naming

--- legacy-code/packages/apis-pkg/sanity/config.ts
+++ legacy-code/packages/apis-pkg/sanity/config.ts
@@ -2,13 +2,13 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const sanityConfig = {
   dataset:
-    process.env.NEXT_PUBLIC_SANITY_DATASET! ||
+    process.env.SANITY_DATASET! ||
     process.env.STORYBOOK_SANITY_DATASET ||
     "production",
   projectId:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID! ||
+    process.env.SANITY_PROJECT_ID! ||
     process.env.STORYBOOK_SANITY_PROJECT_ID!,
   useCdn: process.env.NODE_ENV !== "production",
-  token: process.env.SANITY_API_TOKEN! || process.env.NEXT_PUBLIC_SANITY_API_TOKEN!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  token: process.env.SANITY_API_TOKEN! || process.env.SANITY_API_TOKEN!,
+  apiVersion: process.env.SANITY_API_VERSION!,
 };
--- legacy-code/packages/apis-pkg/sanity/sanityPreviewAPI.ts
+++ legacy-code/packages/apis-pkg/sanity/sanityPreviewAPI.ts
@@ -3,13 +3,13 @@ import createClient from "@sanity/client";
 
 export const sanityPreviewAPI = createClient({
   dataset:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET! ||
+    process.env.SANITY_PROJECT_DATASET! ||
     process.env.STORYBOOK_SANITY_PROJECT_DATASET ||
     "production",
   projectId:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID! ||
+    process.env.SANITY_PROJECT_ID! ||
     process.env.STORYBOOK_SANITY_PROJECT_ID!,
   useCdn: false,
-  token: process.env.SANITY_API_TOKEN! || process.env.NEXT_PUBLIC_SANITY_API_TOKEN!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  token: process.env.SANITY_API_TOKEN! || process.env.SANITY_API_TOKEN!,
+  apiVersion: process.env.SANITY_API_VERSION!,
 });
