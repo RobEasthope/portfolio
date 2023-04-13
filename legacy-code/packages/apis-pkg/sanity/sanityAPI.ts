397d607a5924bb35b7398dfaa56326bb1fcf515c
Rob Easthope <hello@robeasthope.com>
2023-04-13 19:55:44 +0100

    Remove legacy APIs pkg

--- legacy-code/packages/apis-pkg/sanity/sanityAPI.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const sanityAPI = createClient({
-  dataset: process.env.SANITY_PROJECT_DATASET || '',
-  projectId: process.env.SANITY_PROJECT_ID || '',
-  useCdn: true,
-  token: process.env.SANITY_API_TOKEN || '',
-  apiVersion: process.env.SANITY_API_VERSION || '',
-});
--- legacy-code/packages/apis-pkg/sanity/sanityPreviewAPI.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const sanityPreviewAPI = createClient({
-  dataset: process.env.SANITY_PROJECT_DATASET || '',
-  projectId: process.env.SANITY_PROJECT_ID || '',
-  useCdn: false,
-  token: process.env.SANITY_API_TOKEN || '',
-  apiVersion: process.env.SANITY_API_VERSION || '',
-});
