a1d8baef341da63dd55d67c60172bd28ebe3a05d
Rob Easthope <hello@robeasthope.com>
2023-04-13 19:34:24 +0100

    Move urlFor to SanityImage

--- legacy-code/packages/apis-pkg/sanity/urlFor.ts
+++
@@ -1,9 +0,0 @@
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import imageUrlBuilder from "@sanity/image-url";
-import { sanityConfig } from "./config";
-
-const builder = imageUrlBuilder(sanityConfig);
-
-export function urlFor(source: SanityImageSource) {
-  return builder.image(source);
-}
