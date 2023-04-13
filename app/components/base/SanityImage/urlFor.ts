05ea8a10514333263357ce12cdeda5c06799209b
Rob Easthope <hello@robeasthope.com>
2023-04-13 20:15:06 +0100

    Fix env var

--- app/components/base/SanityImage/urlFor.ts
+++ app/components/base/SanityImage/urlFor.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder({
   projectId: process.env.SANITY_PROJECT_ID || '',
-  dataset: process.env.SANITY_DATASET || '',
+  dataset: process.env.SANITY_PROJECT_DATASET || '',
 });
 
 export function urlFor(source: SanityImageSource) {
