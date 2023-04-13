823b32697f8720e61abdab54d6b8cf8bd7496790
Rob Easthope <hello@robeasthope.com>
2023-04-13 13:34:24 +0100

    Remove legacy props

--- app/components/base/InternalLink/InternalLink.tsx
+++ app/components/base/InternalLink/InternalLink.tsx
@@ -2,27 +2,21 @@ import Link from 'next/link';
 import type { SanityReference } from 'sanity-codegen';
 import { HOME_PAGE_SLUG } from '~/components/pages/Page/constants/HOME_PAGE_SLUG';
 import type { PageProps } from '~/components/pages/Page/Page';
-import type { CourseProps } from '~/components/pages/Course/Course';
-import type { BlogPostProps } from '~/components/pages/BlogPost/BlogPost';
 
 // Schema props
 export type InternalLinkWithTitleSchemaProps = {
   _type: 'InternalLinkWithTitle';
   _key: string;
-  internalUID: SanityReference<
-    PageProps['page'] | CourseProps['page'] | BlogPostProps['page']
-  >;
-  to?: PageProps['page'] | CourseProps['page'] | BlogPostProps['page'];
+  internalUID: SanityReference<PageProps['page']>;
+  to?: PageProps['page'];
   title: string;
 };
 
 export type InternalLinkSchemaProps = {
   _type: 'InternalLinkSansTitle';
   _key: string;
-  internalUID: SanityReference<
-    PageProps['page'] | CourseProps['page'] | BlogPostProps['page']
-  >;
-  to?: PageProps['page'] | CourseProps['page'] | BlogPostProps['page'];
+  internalUID: SanityReference<PageProps['page']>;
+  to?: PageProps['page'];
 };
 
 // Component props
