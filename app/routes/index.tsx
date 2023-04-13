225049c1546b1a59e6d0c0303126fd2a1d577e61
Rob Easthope <hello@robeasthope.com>
2023-04-13 14:27:51 +0100

    Experiment with adding header to index route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,28 @@
 import type { LoaderArgs } from '@vercel/remix';
 import { useLoaderData } from '@remix-run/react';
 import { Header } from '~/components/navigation/Header/Header';
+import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
+import { HEADER_QUERY } from '~/components/navigation/Header/Header.query';
 
 export const config = { runtime: 'edge' };
 
-export async function loader({ request }: LoaderArgs) {
+export const loader = async () => {
+  const header = await sanityAPI.fetch(HEADER_QUERY).then((payload) => payload);
+
+  console.log('header', header);
+
   return {
-    date: new Date().toISOString(),
+    logo: header?.logo,
+    primaryNavigation: header?.primaryNavigation,
+    secondaryNavigation: header?.secondaryNavigation,
   };
-}
+};
 
 export default function Index() {
   const { date } = useLoaderData<typeof loader>();
   return (
     <>
       <Header />
-
       <main>
         <h1 className="text-red-400">foo</h1>
       </main>
