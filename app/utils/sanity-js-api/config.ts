4fba89bfc6c7b0b7a8e9f2ae4e7754dc588c4d28
Rob Easthope <hello@robeasthope.com>
2023-04-13 15:56:50 +0100

    Remove Storybook env vars

--- app/utils/sanity-js-api/config.ts
+++ app/utils/sanity-js-api/config.ts
@@ -1,12 +1,8 @@
 // Disable no-non-null-assertion as it's irrelevant for this bit
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const sanityConfig = {
-  dataset:
-    process.env.SANITY_DATASET! ||
-    process.env.STORYBOOK_SANITY_DATASET ||
-    'production',
-  projectId:
-    process.env.SANITY_PROJECT_ID! || process.env.STORYBOOK_SANITY_PROJECT_ID!,
+  dataset: process.env.SANITY_DATASET! || 'production',
+  projectId: process.env.SANITY_PROJECT_ID!,
   useCdn: process.env.NODE_ENV !== 'production',
   token: process.env.SANITY_API_TOKEN! || process.env.SANITY_API_TOKEN!,
   apiVersion: process.env.SANITY_API_VERSION!,
