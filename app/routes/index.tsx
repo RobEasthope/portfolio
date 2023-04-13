9a388ecd66b4b4527eda3bb4a735242d59733055
Rob Easthope <hello@robeasthope.com>
2023-04-13 14:56:11 +0100

    Fix TS props

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,14 @@
 import type { LoaderArgs } from '@vercel/remix';
 import { useLoaderData } from '@remix-run/react';
+import type { HeaderProps } from '~/components/navigation/Header/Header';
 import { Header } from '~/components/navigation/Header/Header';
 import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
 import { HEADER_QUERY } from '~/components/navigation/Header/Header.query';
 
 export const config = { runtime: 'edge' };
 
-export async function loader({}: LoaderArgs) {
-  const header = await sanityAPI.fetch(HEADER_QUERY).then((payload) => payload);
-
-  console.log('header', header);
+export async function loader() {
+  const header: HeaderProps = await sanityAPI.fetch(HEADER_QUERY);
 
   return {
     header,
