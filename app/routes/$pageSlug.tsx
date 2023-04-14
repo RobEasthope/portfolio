a97c185e872f6988ce51fc79b99bb08419cc9a3f
Rob Easthope <hello@robeasthope.com>
2023-04-14 14:16:05 +0100

    Clean up page route code

--- app/routes/$pageSlug.tsx
+++ app/routes/$pageSlug.tsx
@@ -2,9 +2,7 @@ import { json } from '@vercel/remix';
 import type { LoaderArgs } from '@vercel/remix';
 import { useLoaderData } from '@remix-run/react';
 import { cacheHeader } from 'pretty-cache-header';
-import type { HeaderProps } from '~/components/navigation/Header/Header';
 import { sanityAPI } from '~/sanity/sanity-js-api/sanityAPI';
-import { HEADER_QUERY } from '~/components/navigation/Header/Header.query';
 import type { PageProps } from '~/components/pages/Page/Page';
 import { Page } from '~/components/pages/Page/Page';
 import type { SanityPageByIdQueryProps } from '~/types/SanityPageByIdQueryProps';
@@ -12,7 +10,6 @@ import {
   PAGE_BY_ID_QUERY,
   PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
 } from '~/components/pages/Page/Page.queries';
-import { HOME_PAGE_SLUG } from '~/components/pages/Page/constants/HOME_PAGE_SLUG';
 import type { Error404Props } from '~/components/pages/Error404/Error404';
 import type { MetadataSettingsProps } from '~/components/settings/MetadataSettingsProps';
 
