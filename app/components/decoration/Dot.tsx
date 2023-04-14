0aa068f105742f23fe54202072ef617bd8cf7ea6
Rob Easthope <hello@robeasthope.com>
2023-04-14 17:55:05 +0100

    Pass in className prop

--- app/components/decoration/Dot.tsx
+++ app/components/decoration/Dot.tsx
@@ -1,3 +1,9 @@
-export const Dot = () => (
-  <hr className="mx-auto h-0.25 w-0.25 bg-black rounded-full border-none" />
+export type DotProps = {
+  className?: string;
+};
+
+export const Dot = ({ className }: DotProps) => (
+  <hr
+    className={`mx-auto h-0.25 w-0.25 bg-black rounded-full border-none ${className}`}
+  />
 );
