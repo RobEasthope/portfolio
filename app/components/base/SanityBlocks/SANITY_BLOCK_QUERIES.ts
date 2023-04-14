124d4beba1d08d7aa402607d46eefce7484b46ac
Rob Easthope <hello@robeasthope.com>
2023-04-14 12:34:05 +0100

    Add Project index to query index

--- app/components/base/SanityBlocks/SANITY_BLOCK_QUERIES.ts
+++ app/components/base/SanityBlocks/SANITY_BLOCK_QUERIES.ts
@@ -3,6 +3,7 @@ import { IMAGE_QUERY } from '~/components/blocks/Image/Image.query';
 import { LANDING_HERO_QUERY } from '~/components/blocks/LandingHero/LandingHero.query';
 import { MUX_VIDEO_QUERY } from '~/components/blocks/MuxVideo/MuxVideo.query';
 import { PAGE_TITLE_QUERY } from '~/components/blocks/PageTitle/PageTitle.query';
+import { PROJECT_INDEX_QUERY } from '~/components/blocks/ProjectIndex/ProjectIndex.query';
 import { SPACER_QUERY } from '~/components/blocks/Spacer/Spacer.query';
 import { TEXT_QUERY } from '~/components/blocks/Text/Text.query';
 import { VIMEO_VIDEO_QUERY } from '~/components/blocks/VimeoVideo/VimeoVideo.query';
@@ -18,6 +19,7 @@ export const SANITY_BLOCK_QUERIES = ({
   LandingHero: LANDING_HERO_QUERY,
   MuxVideo: MUX_VIDEO_QUERY,
   PageTitle: PAGE_TITLE_QUERY,
+  ProjectIndex: PROJECT_INDEX_QUERY,
   Spacer: SPACER_QUERY,
   Text: TEXT_QUERY,
   VimeoVideo: VIMEO_VIDEO_QUERY,
