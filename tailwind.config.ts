210570989fde5b769c36ec9de8e0c70ac96658dc
Rob Easthope <hello@robeasthope.com>
2023-04-14 18:31:41 +0100

    Set correct font name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,7 +42,7 @@ export default {
       fontFamily: {
         sans: ['aktiv-grotesk', ...defaultTheme.fontFamily.sans],
         plantin: ['plantin', ...defaultTheme.fontFamily.serif],
-        sabon: ['sabon', ...defaultTheme.fontFamily.serif],
+        sabon: ['linotype-sabon', ...defaultTheme.fontFamily.serif],
       },
       fontSize: {
         xs: fluidTypeSize({
