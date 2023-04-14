8249210988ca44523a1c3b2407d512af60573fac
Rob Easthope <hello@robeasthope.com>
2023-04-14 14:14:35 +0100

    Remove error boundary code

--- app/routes/$pageSlug.tsx
+++ app/routes/$pageSlug.tsx
@@ -1,10 +1,6 @@
 import { json } from '@vercel/remix';
 import type { LoaderArgs } from '@vercel/remix';
-import {
-  useLoaderData,
-  useRouteError,
-  isRouteErrorResponse,
-} from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { cacheHeader } from 'pretty-cache-header';
 import type { HeaderProps } from '~/components/navigation/Header/Header';
 import { sanityAPI } from '~/sanity/sanity-js-api/sanityAPI';
@@ -57,36 +53,6 @@ export function headers() {
       staleIfError: '7days',
     }),
   };
-}
-
-export function ErrorBoundary() {
-  const error = useRouteError();
-
-  // when true, this is what used to go to `CatchBoundary`
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div>
-        <h1>Oops</h1>
-        <p>Status: {error.status}</p>
-        <p>{error.data.message}</p>
-      </div>
-    );
-  }
-
-  // Don't forget to typecheck with your own logic.
-  // Any value can be thrown, not just errors!
-  let errorMessage = 'Unknown error';
-  if (isDefinitelyAnError(error)) {
-    errorMessage = error.message;
-  }
-
-  return (
-    <div>
-      <h1>Uh oh ...</h1>
-      <p>Something went wrong.</p>
-      <pre>{errorMessage}</pre>
-    </div>
-  );
 }
 
 export default function Index() {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -57,36 +57,6 @@ export function headers() {
   };
 }
 
-export function ErrorBoundary() {
-  const error = useRouteError();
-
-  // when true, this is what used to go to `CatchBoundary`
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div>
-        <h1>Oops</h1>
-        <p>Status: {error.status}</p>
-        <p>{error.data.message}</p>
-      </div>
-    );
-  }
-
-  // Don't forget to typecheck with your own logic.
-  // Any value can be thrown, not just errors!
-  let errorMessage = 'Unknown error';
-  if (isDefinitelyAnError(error)) {
-    errorMessage = error.message;
-  }
-
-  return (
-    <div>
-      <h1>Uh oh ...</h1>
-      <p>Something went wrong.</p>
-      <pre>{errorMessage}</pre>
-    </div>
-  );
-}
-
 export default function Index() {
   const { page, header, footer } = useLoaderData<typeof loader>();
 
