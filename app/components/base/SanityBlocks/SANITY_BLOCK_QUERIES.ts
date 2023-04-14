c9fd37b9f69506488267730f8ec59927c0f078dd
Rob Easthope <hello@robeasthope.com>
2023-04-14 12:28:09 +0100

    Add PAGE_TITLE_QUERY to query index

--- app/components/base/SanityBlocks/SANITY_BLOCK_QUERIES.ts
+++ app/components/base/SanityBlocks/SANITY_BLOCK_QUERIES.ts
@@ -2,6 +2,7 @@ import { GALLERY_QUERY } from '~/components/blocks/Gallery/Gallery.query';
 import { IMAGE_QUERY } from '~/components/blocks/Image/Image.query';
 import { LANDING_HERO_QUERY } from '~/components/blocks/LandingHero/LandingHero.query';
 import { MUX_VIDEO_QUERY } from '~/components/blocks/MuxVideo/MuxVideo.query';
+import { PAGE_TITLE_QUERY } from '~/components/blocks/PageTitle/PageTitle.query';
 import { SPACER_QUERY } from '~/components/blocks/Spacer/Spacer.query';
 import { TEXT_QUERY } from '~/components/blocks/Text/Text.query';
 import { VIMEO_VIDEO_QUERY } from '~/components/blocks/VimeoVideo/VimeoVideo.query';
@@ -16,6 +17,7 @@ export const SANITY_BLOCK_QUERIES = ({
   Image: IMAGE_QUERY,
   LandingHero: LANDING_HERO_QUERY,
   MuxVideo: MUX_VIDEO_QUERY,
+  PageTitle: PAGE_TITLE_QUERY,
   Spacer: SPACER_QUERY,
   Text: TEXT_QUERY,
   VimeoVideo: VIMEO_VIDEO_QUERY,
