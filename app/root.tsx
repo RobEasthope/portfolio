26f4fee8cdecf12e681e7a3b7ff35686f2604038
Rob Easthope <hello@robeasthope.com>
2023-04-14 13:04:21 +0100

    Set serif font as the default

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="font-serif">
         <Outlet />
         <ScrollRestoration />
 
