9cf4094241aaa76bc719853a6f4b3f27f5b91b42
Rob Easthope <hello@robeasthope.com>
2023-04-14 14:03:48 +0100

    Clean up imports

--- app/components/navigation/Header/Header.tsx
+++ app/components/navigation/Header/Header.tsx
@@ -1,24 +1,14 @@
-import type { LoaderArgs } from '@vercel/remix';
 import { Box } from '~/components/base/Box/Box';
-import { Type } from '~/components/base/Type/Type';
 import { SmallNavigation } from '~/components/navigation/SmallNavigation/SmallNavigation';
 import type { ExternalLinkWithTitleSchemaProps } from '~/components/base/ExternalLink/ExternalLink';
 import type { InternalLinkWithTitleSchemaProps } from '~/components/base/InternalLink/InternalLink';
-import { InternalLink } from '~/components/base/InternalLink/InternalLink';
-import { SuperLink } from '~/components/base/SuperLink/SuperLink';
 import Headroom from 'react-headroom';
 import type {
   SanityImageAsset,
   SanityImageCrop,
   SanityReference,
 } from 'sanity-codegen';
-import { HOME_PAGE_SLUG } from '~/components/pages/Page/constants/HOME_PAGE_SLUG';
 import type { EmailLinkWithTitleSchemaProps } from '~/components/base/EmailLink/EmailLink';
-import { SanityImage } from '~/components/base/SanityImage/SanityImage';
-import { METADATA_HARD_CODED_FALLBACKS } from '~/constants/METADATA_HARD_CODED_FALLBACKS';
-import { HEADER_QUERY } from '~/components/navigation/Header/Header.query';
-import { sanityAPI } from '~/sanity/sanity-js-api/sanityAPI';
-import { useLoaderData } from '@remix-run/react';
 import { NavListing } from '~/components/navigation/Header/components/NavListing/NavListing';
 import { HeaderLogo } from '~/components/navigation/Header/components/HeaderLogo/HeaderLogo';
 
