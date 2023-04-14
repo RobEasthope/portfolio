f245ead7fb245f7286dfca2e3ce207e6277379c9
Rob Easthope <hello@robeasthope.com>
2023-04-14 14:40:36 +0100

    lint:fix

--- app/components/blocks/PageTitle/PageTitle.tsx
+++ app/components/blocks/PageTitle/PageTitle.tsx
@@ -1,6 +1,6 @@
-import type { PageTitle as rawPageTitleProps } from '~/components/types/sanity-schema';
 import { Box } from '~/components/base/Box/Box';
 import { Type } from '~/components/base/Type/Type';
+import type { PageTitle as rawPageTitleProps } from '~/components/types/sanity-schema';
 
 // TYPES
 export interface PageTitleProps extends rawPageTitleProps {
