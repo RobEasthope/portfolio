213d65f756055735dcda319ddcc2581be26bea68
Rob Easthope <hello@robeasthope.com>
2023-04-14 18:12:20 +0100

    Make Dot full width for better DX

--- app/components/decoration/Dot.tsx
+++ app/components/decoration/Dot.tsx
@@ -4,7 +4,7 @@ export type DotProps = {
 
 export const Dot = ({ className }: DotProps) => (
   <hr
-    className={`flex justify-center items-center h-1 mx-auto after:content-[''] after:h-0.25 after:w-0.25 after:bg-black after:rounded-full border-none ${
+    className={`flex w-full justify-center items-center h-1 mx-auto after:content-[''] after:h-0.25 after:w-0.25 after:bg-black after:rounded-full border-none ${
       className || ''
     }`}
   />
