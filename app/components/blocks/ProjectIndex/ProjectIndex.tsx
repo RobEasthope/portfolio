b002dc55a48cf5e286bced021cb51ccb98850b8f
Rob Easthope <hello@robeasthope.com>
2023-04-14 15:29:43 +0100

    Refactor ProjectIndexCard markup

--- app/components/blocks/ProjectIndex/ProjectIndex.tsx
+++ app/components/blocks/ProjectIndex/ProjectIndex.tsx
@@ -1,7 +1,5 @@
 import { Box } from '~/components/base/Box/Box';
-import { InternalLink } from '~/components/base/InternalLink/InternalLink';
-import { SanityImage } from '~/components/base/SanityImage/SanityImage';
-import { Type } from '~/components/base/Type/Type';
+import { ProjectIndexCard } from '~/components/blocks/ProjectIndex/components/ProjectIndexCard';
 
 import type { ProjectProps } from '~/components/pages/Project/Project';
 import type { ProjectIndex as rawProjectIndexProps } from '~/components/types/sanity-schema';
@@ -20,30 +18,7 @@ export const ProjectIndex = ({ projects }: ProjectIndexProps) => (
         className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
       >
         {projects?.map((project, i) => (
-          <Box
-            as="li"
-            key={`${project?._id}-${i}`}
-            className="flex flex-row sm:flex-col items-center gap-0.5"
-          >
-            <InternalLink href={project?.slug?.current}>
-              <a>
-                <SanityImage
-                  asset={project?.thumbnailImage}
-                  alt={project?.title || ''}
-                  mode="responsive"
-                  maxWidth={48}
-                  aspectRatio={1}
-                  className="rounded-full w-[48px] h-[48px]"
-                />
-              </a>
-            </InternalLink>
-
-            <Type as="p">
-              <InternalLink href={project?.slug?.current}>
-                {project?.title}
-              </InternalLink>
-            </Type>
-          </Box>
+          <ProjectIndexCard key={`${project?._id}-${i}`} project={project} />
         ))}
       </Box>
     )}
