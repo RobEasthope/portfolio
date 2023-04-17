e56c5e349390708e30ae547b4e653f28d061647f
Rob Easthope <hello@robeasthope.com>
2023-04-17 16:27:18 +0100

    Pass in utf-8 and viewport config at root level

--- app/root.tsx
+++ app/root.tsx
@@ -79,6 +79,9 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+
         {/* Favicons */}
         <link
           rel="apple-touch-icon"
