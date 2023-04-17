ff118ef80e336bd0b3cbea75e5fe094e3795c05c
Rob Easthope <hello@robeasthope.com>
2023-04-17 15:04:23 +0100

    Fix MetadataFallbacks imports

--- app/components/pages/Error404/Error404.queries.ts
+++ app/components/pages/Error404/Error404.queries.ts
@@ -3,7 +3,7 @@ import groq from 'groq';
 import { FOOTER_QUERY } from '~/components/navigation/Footer/Footer.query';
 import { HEADER_QUERY } from '~/components/navigation/Header/Header.query';
 
-import { METADATA_SETTINGS_QUERY } from '~/components/settings/MetadataFallbacks.query';
+import { METADATA_FALLBACKS_QUERY } from '~/components/settings/MetadataFallbacks/MetadataFallbacks.query';
 
 export const ERROR_404_QUERY = groq`{
   "page": *[_type == "Error404" && !(_id in path("drafts.**"))][0]{
@@ -22,5 +22,5 @@ export const ERROR_404_QUERY = groq`{
   },
   "header": ${HEADER_QUERY},
   "footer": ${FOOTER_QUERY},
-  "fallbacks": ${METADATA_SETTINGS_QUERY},
+  "fallbacks": ${METADATA_FALLBACKS_QUERY},
 }`;
