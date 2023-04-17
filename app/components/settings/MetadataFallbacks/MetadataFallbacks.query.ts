f17639b6a576f793a97634a58c01a63cfbefe089
Rob Easthope <hello@robeasthope.com>
2023-04-17 15:42:38 +0100

    Update MetadataFallbacks.query.ts

--- app/components/settings/MetadataFallbacks/MetadataFallbacks.query.ts
+++ app/components/settings/MetadataFallbacks/MetadataFallbacks.query.ts
@@ -4,8 +4,8 @@ export const METADATA_FALLBACKS_QUERY = groq`
   *[_type== 'MetadataFallbacks' && !(_id in path("drafts.**"))][0]{
     sitename,
     canonicalUrl,
-    titleFallback,
-    descriptionFallback,
-    thumbnailFallback
+    title,
+    description,
+    thumbnail,
   }
 `;
