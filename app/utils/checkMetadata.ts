507c79ff2e6f318c54f2d67a33a77668d1c40424
Rob Easthope <hello@robeasthope.com>
2023-04-17 18:46:58 +0100

    Create checkMetadata.ts

---
+++ app/utils/checkMetadata.ts
@@ -0,0 +1,33 @@
+import { urlFor } from '~/components/base/SanityImage/urlFor';
+
+export function checkMetadata({ title, description, image }) {
+  const data = [];
+
+  if (title) {
+    data.push(
+      { title },
+      {
+        property: 'og:title',
+        content: title,
+      },
+    );
+  }
+
+  if (description) {
+    data.push({
+      property: 'description',
+      content: description,
+    });
+  }
+
+  if (image) {
+    data.push({
+      property: 'og:image',
+      content: urlFor(image).width(1200).height(630).url(),
+    });
+  }
+
+  console.log(data);
+
+  return data.length > 0 ? data : null;
+}
