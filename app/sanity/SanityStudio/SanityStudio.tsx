da6ae9a35df8e3c47035d48544797279d4b065d1
Rob Easthope <hello@robeasthope.com>
2023-04-19 15:05:09 +0100

    Add back to app link

--- app/sanity/SanityStudio/SanityStudio.tsx
+++ app/sanity/SanityStudio/SanityStudio.tsx
@@ -3,8 +3,29 @@ import { Studio } from 'sanity';
 
 import config from '~/sanity/sanity.config';
 
+import { Box } from '~/components/base/Box/Box';
+import { InternalLink } from '~/components/base/InternalLink/InternalLink';
+
+import { HOME_PAGE_SLUG } from '~/components/pages/Page/constants/HOME_PAGE_SLUG';
+
 export function SanityStudio() {
-  return <ClientOnly>{() => <Studio config={config} />}</ClientOnly>;
+  return (
+    <ClientOnly>
+      {() => (
+        <>
+          <Studio config={config} />
+          <Box as="nav" className="flex justify-center">
+            <InternalLink
+              href={HOME_PAGE_SLUG}
+              className="inline-block text-white bg-gray-900 font-bold font-sans text-center px-1 py-0.5"
+            >
+              Back to app
+            </InternalLink>
+          </Box>
+        </>
+      )}
+    </ClientOnly>
+  );
 }
 
 export default SanityStudio;
