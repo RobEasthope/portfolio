579a50dddea8fec8bbcaadab464841ab42ea98ea
Rob Easthope <hello@robeasthope.com>
2023-04-19 09:03:40 +0100

    Delete config.ts

--- app/sanity/sanity-js-api/config.ts
+++
@@ -1,7 +0,0 @@
-export const sanityConfig = {
-  projectId: process.env.SANITY_PUBLIC_PROJECT_ID || '',
-  dataset: process.env.SANITY_PUBLIC_DATASET || '',
-  apiVersion: process.env.SANITY_PUBLIC_API_VERSION || '',
-  useCdn: process.env.NODE_ENV !== 'production',
-  token: process.env.SANITY_API_TOKEN || '',
-};
