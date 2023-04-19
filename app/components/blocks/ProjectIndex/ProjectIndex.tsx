09a2da38c4fd88b56aac883dc98a479e4ea9990f
Rob Easthope <hello@robeasthope.com>
2023-04-19 15:16:28 +0100

    Add null check to ProjectIndex

--- app/components/blocks/ProjectIndex/ProjectIndex.tsx
+++ app/components/blocks/ProjectIndex/ProjectIndex.tsx
@@ -1,10 +1,10 @@
+import type { ProjectIndex as rawProjectIndexProps } from '~/components/types/sanity-schema';
 
 import { Box } from '~/components/base/Box/Box';
 
 import { ProjectIndexCard } from '~/components/blocks/ProjectIndex/components/ProjectIndexCard';
 
 import type { ProjectProps } from '~/components/pages/Project/Project';
-import type { ProjectIndex as rawProjectIndexProps } from '~/components/types/sanity-schema';
 
 // TYPES
 export interface ProjectIndexProps extends rawProjectIndexProps {
@@ -12,19 +12,25 @@ export interface ProjectIndexProps extends rawProjectIndexProps {
 }
 
 // MARKUP
-export const ProjectIndex = ({ projects }: ProjectIndexProps) => (
-  <Box as="section" className="px-1">
-    {projects?.length > 0 && (
-      <Box
-        as="ul"
-        className="grid grid-cols-1 gap-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7"
-      >
-        {projects?.map((project, i) => (
-          <ProjectIndexCard key={`${project?._id}-${i}`} project={project} />
-        ))}
-      </Box>
-    )}
-  </Box>
-);
+export const ProjectIndex = ({ projects }: ProjectIndexProps) => {
+  if (!projects) {
+    return null;
+  }
+
+  return (
+    <Box as="section" className="px-1">
+      {projects?.length > 0 && (
+        <Box
+          as="ul"
+          className="grid grid-cols-1 gap-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7"
+        >
+          {projects?.map((project, i) => (
+            <ProjectIndexCard key={`${project?._id}-${i}`} project={project} />
+          ))}
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 export default ProjectIndex;
