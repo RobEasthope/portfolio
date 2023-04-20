9fdf36e965b1a56d6ecd423c02a1e426a77c8de7
Rob Easthope <hello@robeasthope.com>
2023-04-20 13:45:30 +0100

    Draft out TechList

---
+++ app/components/blocks/Tech/TechList.tsx
@@ -0,0 +1,23 @@
+import { Box } from '~/components/base/Box/Box';
+import { ExternalLink } from '~/components/base/ExternalLink/ExternalLink';
+import { Type } from '~/components/base/Type/Type';
+
+export type TechProps = {
+  _type: 'tech';
+  name: string;
+  url: string;
+};
+
+export type TechListProps = {
+  technologies: TechProps[];
+};
+
+export const TechList = ({ technologies }: TechListProps) => (
+  <Box as="ul">
+    {technologies.map((tech) => (
+      <Type as="li" key={tech?._id}>
+        <ExternalLink href={tech?.url}>{tech?.name}</ExternalLink>
+      </Type>
+    ))}
+  </Box>
+);
