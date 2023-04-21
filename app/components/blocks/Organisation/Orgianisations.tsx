92d3adf0ca31e325fbf1b26d9d7ade262a0e2c1a
Rob Easthope <hello@robeasthope.com>
2023-04-21 10:56:06 +0100

    Tweak max-width and spacing

--- app/components/blocks/Organisation/Orgianisations.tsx
+++ app/components/blocks/Organisation/Orgianisations.tsx
@@ -9,7 +9,7 @@ export type OrganisationsProps = {
 
 export const Organisations = ({ organisations }: OrganisationsProps) => (
   <Box as="div" className="px-1">
-    <Box as="ul" className="grid grid-cols-4 max-w-7xl mx-auto">
+    <Box as="ul" className="grid grid-cols-4 max-w-6xl mx-auto gap-0.5 gap-x-1">
       {organisations?.map((org) => (
         <Box key={org?._id} as="li">
           <ExternalLink href={org?.url || ''}>{org.name}</ExternalLink>
