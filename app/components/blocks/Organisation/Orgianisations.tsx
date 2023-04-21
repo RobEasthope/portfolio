7a63dd55be9a31c7bb48b5a2c8a361d98c96f4ed
Rob Easthope <hello@robeasthope.com>
2023-04-21 11:03:36 +0100

    Add null check

--- app/components/blocks/Organisation/Orgianisations.tsx
+++ app/components/blocks/Organisation/Orgianisations.tsx
@@ -7,19 +7,23 @@ export type OrganisationsProps = {
   organisations: Organisation[];
 };
 
-export const Organisations = ({ organisations }: OrganisationsProps) => (
-  <Box as="section" className="px-1">
-    <Box as="div" className="max-w-6xl mx-auto">
-      <Box
-        as="ul"
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0.5 gap-x-1"
-      >
-        {organisations?.map((org) => (
-          <Box key={org?._id} as="li">
-            <ExternalLink href={org?.url || ''}>{org.name}</ExternalLink>
-          </Box>
-        ))}
+export const Organisations = ({ organisations }: OrganisationsProps) => {
+  if (!organisations) return null;
+
+  return (
+    <Box as="section" className="px-1">
+      <Box as="div" className="max-w-6xl mx-auto">
+        <Box
+          as="ul"
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0.5 gap-x-1"
+        >
+          {organisations?.map((org) => (
+            <Box key={org?._id} as="li">
+              <ExternalLink href={org?.url || ''}>{org.name}</ExternalLink>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
-  </Box>
-);
+  );
+};
