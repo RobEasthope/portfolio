eed7650329891396106ead51179d459354cddf93
Rob Easthope <hello@robeasthope.com>
2023-04-21 10:22:28 +0100

    Add Organisations to SANITY_BLOCK_QUERIES

--- app/components/base/SanityBlocks/SANITY_BLOCK_QUERIES.ts
+++ app/components/base/SanityBlocks/SANITY_BLOCK_QUERIES.ts
@@ -2,6 +2,7 @@ import { GALLERY_QUERY } from '~/components/blocks/Gallery/Gallery.query';
 import { IMAGE_QUERY } from '~/components/blocks/Image/Image.query';
 import { LANDING_HERO_QUERY } from '~/components/blocks/LandingHero/LandingHero.query';
 import { MUX_VIDEO_QUERY } from '~/components/blocks/MuxVideo/MuxVideo.query';
+import { ORGANISATIONS_QUERY } from '~/components/blocks/Organisation/Organisations.query';
 import { PAGE_TITLE_QUERY } from '~/components/blocks/PageTitle/PageTitle.query';
 import { PROJECT_INDEX_QUERY } from '~/components/blocks/ProjectIndex/ProjectIndex.query';
 import { SPACER_QUERY } from '~/components/blocks/Spacer/Spacer.query';
@@ -14,6 +15,7 @@ export const SANITY_BLOCK_QUERIES = () => ({
   Image: IMAGE_QUERY,
   LandingHero: LANDING_HERO_QUERY,
   MuxVideo: MUX_VIDEO_QUERY,
+  Organisations: ORGANISATIONS_QUERY,
   PageTitle: PAGE_TITLE_QUERY,
   ProjectIndex: PROJECT_INDEX_QUERY,
   Spacer: SPACER_QUERY,
