1bc2b860b4aa2022a1a897eee7df914f511b24fc
Rob Easthope <hello@robeasthope.com>
2023-04-22 22:49:45 +0100

    Add APP_SETTINGS_QUERY to full text query

--- app/components/base/Prose/components/FullText/FullText.query.ts
+++ app/components/base/Prose/components/FullText/FullText.query.ts
@@ -1,11 +1,14 @@
 import groq from 'groq';
 
+import { APP_SETTINGS_QUERY } from '~/components/settings/AppSettings/AppSettings.queries';
+
 export const FULL_TEXT_QUERY_BODY = groq`
   ...,
   markDefs[]{
     ...,
     _type == "InternalLink" => {
-      "page": @.internalUID->
+      "page": @.internalUID->,
+      "appSettings": ${APP_SETTINGS_QUERY}
     }
   }
 `;
