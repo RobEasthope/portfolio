7ad7b80fe7805ad717ced3764e66c50d2c31d7eb
Rob Easthope <hello@robeasthope.com>
2023-04-22 22:46:15 +0100

    Resolve internal links correctly

--- app/components/blocks/Text/Text.query.ts
+++ app/components/blocks/Text/Text.query.ts
@@ -3,5 +3,13 @@ import groq from 'groq';
 export const TEXT_QUERY = groq`{
   _type,
   _key,
-  text
+  text[]{
+    ...,
+    markDefs[]{
+      ...,
+      _type == "InternalLink" => {
+        "page": @.internalUID->,
+      }
+    },
+  },
 }`;
