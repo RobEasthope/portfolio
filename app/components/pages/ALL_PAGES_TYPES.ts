d448843f2285cb7eb88cac0b46bb1d110f7fd3a5
Rob Easthope <hello@robeasthope.com>
2023-04-23 22:00:43 +0100

    Refactor ALL_PAGES_TYPES

--- app/components/pages/ALL_PAGES_TYPES.ts
+++ app/components/pages/ALL_PAGES_TYPES.ts
@@ -1,4 +1,1 @@
-import Page from '~/components/pages/Page/Page.schema';
-import Project from '~/components/pages/Project/Project.schema';
-
-export const ALL_PAGES_TYPES = [{ type: Page.name }, { type: Project.name }];
+export const ALL_PAGES_TYPES = [{ type: 'Page' }, { type: 'project' }];
