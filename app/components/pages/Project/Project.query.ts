72d6509433f22112d31517e123a2eea7a0572988
Rob Easthope <hello@robeasthope.com>
2023-04-23 20:26:07 +0100

    Remove legacy testimonial code from query

--- app/components/pages/Project/Project.query.ts
+++ app/components/pages/Project/Project.query.ts
@@ -14,7 +14,6 @@ const PROJECT_QUERY_BODY = groq`
   projectText[]{
     ...,
     "muxVideo": rawMuxVideo.asset->,
-    "testimonial": rawTestimonial->,
     markDefs[]{
       ...,
       _type == "InternalLink" => {
