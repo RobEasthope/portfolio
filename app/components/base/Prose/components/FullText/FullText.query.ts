7729fdfc8e86e06ccd8af96cbb3d10fe1f9330d0
Rob Easthope <hello@robeasthope.com>
2023-04-23 19:58:17 +0100

    Clean up import

--- app/components/base/Prose/components/FullText/FullText.query.ts
+++ app/components/base/Prose/components/FullText/FullText.query.ts
@@ -2,8 +2,6 @@ import groq from 'groq';
 
 import { INTERNAL_LINK_QUERY } from '~/components/base/InternalLink/InternalLink.query';
 
-import { APP_SETTINGS_QUERY } from '~/components/settings/AppSettings/AppSettings.queries';
-
 export const FULL_TEXT_QUERY_BODY = groq`
   ...,
   markDefs[]{
