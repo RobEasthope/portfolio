85dbbf1c16cde846b21e3b33642b709a44e754e1
Rob Easthope <hello@robeasthope.com>
2023-04-23 20:54:13 +0100

    Integrate BASIC_TEXT_QUERY

--- app/components/blocks/Organisation/Organisations.query.ts
+++ app/components/blocks/Organisation/Organisations.query.ts
@@ -1,9 +1,14 @@
 import groq from 'groq';
 
+import { BASIC_TEXT_QUERY } from '~/components/base/Prose/components/BasicText/BasicText.query';
+
 export const ORGANISATIONS_QUERY = groq`{
   _key,
   _type,
   heading,
+  description[]{
+    ${BASIC_TEXT_QUERY}
+  },
   "organisations": *[_type == "organisation" && !(_id in path("drafts.**")) && hiddenOrg == false]{
     _id,
     name,
