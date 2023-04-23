e9dd1902b08f0ed877bbc9da839950d7346aa5ec
Rob Easthope <hello@robeasthope.com>
2023-04-23 20:52:48 +0100

    Add heading to Org query

--- app/components/blocks/Organisation/Organisations.query.ts
+++ app/components/blocks/Organisation/Organisations.query.ts
@@ -3,6 +3,7 @@ import groq from 'groq';
 export const ORGANISATIONS_QUERY = groq`{
   _key,
   _type,
+  heading,
   "organisations": *[_type == "organisation" && !(_id in path("drafts.**")) && hiddenOrg == false]{
     _id,
     name,
