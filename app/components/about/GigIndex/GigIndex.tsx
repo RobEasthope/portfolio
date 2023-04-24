7486e0e67561e28ff0d2840d9ad5723604b35e35
Rob Easthope <hello@robeasthope.com>
2023-04-24 18:16:27 +0100

    Draft out Gig index markup

--- app/components/about/GigIndex/GigIndex.tsx
+++ app/components/about/GigIndex/GigIndex.tsx
@@ -1,16 +1,13 @@
-import type { ProjectProps } from '~/components/work/Project/Project';
-import { ProjectIndexCard } from '~/components/work/ProjectIndex/components/ProjectIndexCard';
-
 import { Box } from '~/components/base/Box/Box';
 
 // TYPES
-export type ProjectIndexProps = {
-  projects: ProjectProps['page'][];
+export type GigIndexProps = {
+  projects: GigProps[];
 };
 
 // MARKUP
-export const ProjectIndex = ({ projects }: ProjectIndexProps) => {
-  if (!projects) {
+export const GigIndex = ({ projects }: GigIndexProps) => {
+  if (!allGigs?.length) {
     return null;
   }
 
@@ -21,10 +18,15 @@ export const ProjectIndex = ({ projects }: ProjectIndexProps) => {
           as="ul"
           className="grid grid-cols-1 gap-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7"
         >
-          {projects?.map((project, i) => (
-            <ProjectIndexCard
-              key={`${project?._id || ''}-${i}`}
-              project={project}
+          {allGigs?.map((gig, i) => (
+            <Gig
+              key={`${gig?._id || ''}-${i}`}
+              employer={gig.employer}
+              location={gig.location}
+              jobTitle={gig.jobTitle}
+              description={gig.description}
+              startDate={gig.startDate}
+              endDate={gig.endDate}
             />
           ))}
         </Box>
