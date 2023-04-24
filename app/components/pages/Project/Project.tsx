00e87263900695c6417cd01c88477e26231d67b9
Rob Easthope <hello@robeasthope.com>
2023-04-24 13:40:33 +0100

    Fix ProjectProps

--- app/components/pages/Project/Project.tsx
+++ app/components/pages/Project/Project.tsx
@@ -19,6 +19,7 @@ import { Dot } from '~/components/blocks/Dot/Dot';
 
 import { BasicLayout } from '~/components/layouts/BasicLayout/BasicLayout';
 
+import type { TechProps } from '~/components/pages/Project/components/TechList/TechList';
 import { TechList } from '~/components/pages/Project/components/TechList/TechList';
 
 export type ProjectProps = {
@@ -51,10 +52,7 @@ export type ProjectProps = {
     projectUrlTitle?: string;
     repoUrl?: string;
     date?: string;
-    technologies?: {
-      name?: string;
-      url?: string;
-    }[];
+    technologies?: TechProps[];
     displayProject?: boolean;
   };
   header: HeaderProps;
