744a6612cdfad4cbc5f49373a7d7eb09b04bdeba
Rob Easthope <hello@robeasthope.com>
2023-04-24 14:39:34 +0100

    Draft out CV markup

--- app/components/pages/CV/CV.tsx
+++ app/components/pages/CV/CV.tsx
@@ -1,40 +1,17 @@
-import type {
-  SanityImageAsset,
-  SanityImageCrop,
-  SanityImageHotspot,
-  SanityReference,
-} from 'sanity-codegen';
-
 import { SanityBlocks } from '~/components/base/SanityBlocks/SanityBlocks';
 
-import type { FooterProps } from '~/components/navigation/Footer/Footer';
-import type { HeaderProps } from '~/components/navigation/Header/Header';
-
-import { BasicLayout } from '~/components/layouts/BasicLayout/BasicLayout';
-
 // TYPES
-export type PageProps = {
+export type CVProps = {
   page: {
     _id: string;
     _type: 'Page';
     title: string;
     slug: { _type: 'slug'; current: string };
     sections: any[];
-    metadataDescription: string;
-    metadataImage: {
-      _type: 'image';
-      asset: SanityReference<SanityImageAsset>;
-      crop?: SanityImageCrop;
-      hotspot?: SanityImageHotspot;
-    };
   };
-  header: HeaderProps;
-  footer: FooterProps;
 };
 
 // MARKUP
-export const Page = ({ page, header, footer }: PageProps) => (
-  <BasicLayout header={header} footer={footer} slug={page?.slug?.current}>
-    <SanityBlocks blocks={page?.sections} pageID={page?._id} />
-  </BasicLayout>
+export const CV = ({ page }: CVProps) => (
+  <SanityBlocks blocks={page?.sections} pageID={page?._id} />
 );
