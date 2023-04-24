17cb52ab3b6bc2d0c7469abf6e7880bee864e3ba
Rob Easthope <hello@robeasthope.com>
2023-04-24 18:29:20 +0100

    Update GigIndex props

--- app/components/about/GigIndex/GigIndex.tsx
+++ app/components/about/GigIndex/GigIndex.tsx
@@ -1,19 +1,21 @@
+import type { GigProps } from '~/components/about/GigIndex/components/Gig/Gig';
+
 import { Box } from '~/components/base/Box/Box';
 
 // TYPES
 export type GigIndexProps = {
-  projects: GigProps[];
+  allGigs: GigProps[];
 };
 
 // MARKUP
-export const GigIndex = ({ projects }: GigIndexProps) => {
+export const GigIndex = ({ allGigs }: GigIndexProps) => {
   if (!allGigs?.length) {
     return null;
   }
 
   return (
     <Box as="section" blockSpacing>
-      {projects?.length > 0 && (
+      {allGigs?.length > 0 && (
         <Box
           as="ul"
           className="grid grid-cols-1 gap-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7"
