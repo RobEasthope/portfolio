7598a1a7013bd1180e2b4a5c637cf30b29c3ae08
Rob Easthope <hello@robeasthope.com>
2023-04-24 15:17:24 +0100

    Remove fallback queries from page level routes and queries

--- app/components/pages/Error404/Error404.query.ts
+++ app/components/pages/Error404/Error404.query.ts
@@ -3,8 +3,6 @@ import groq from 'groq';
 import { FOOTER_QUERY } from '~/components/navigation/Footer/Footer.query';
 import { HEADER_QUERY } from '~/components/navigation/Header/Header.query';
 
-import { METADATA_FALLBACKS_QUERY } from '~/components/settings/MetadataFallbacks/MetadataFallbacks.query';
-
 export const ERROR_404_QUERY = groq`{
   "page": *[_type == "Error404" && !(_id in path("drafts.**"))][0]{
     _type,
@@ -22,5 +20,4 @@ export const ERROR_404_QUERY = groq`{
   },
   "header": ${HEADER_QUERY},
   "footer": ${FOOTER_QUERY},
-  "fallbacks": ${METADATA_FALLBACKS_QUERY},
 }`;
