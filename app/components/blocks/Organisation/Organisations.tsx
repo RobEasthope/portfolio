62273381ed1ca1a6c89ed2107e58e11da5341f0c
Rob Easthope <hello@robeasthope.com>
2023-04-24 11:52:06 +0100

    Remove last remaining code based on sanity schema

--- app/components/blocks/Organisation/Organisations.tsx
+++ app/components/blocks/Organisation/Organisations.tsx
@@ -1,10 +1,29 @@
-import type { Organisation } from '~/types/sanity-schema';
+import type {
+  SanityImageAsset,
+  SanityImageCrop,
+  SanityImageHotspot,
+  SanityReference,
+} from 'sanity-codegen';
 
 import { Box } from '~/components/base/Box/Box';
 import { ExternalLink } from '~/components/base/ExternalLink/ExternalLink';
 
+export type OrganisationProps = {
+  _type: 'organisation';
+  _id?: string;
+  name?: string;
+  url?: string;
+  logo?: {
+    _type: 'image';
+    asset: SanityReference<SanityImageAsset>;
+    crop?: SanityImageCrop;
+    hotspot?: SanityImageHotspot;
+  };
+  hiddenOrg?: boolean;
+};
+
 export type OrganisationsProps = {
-  organisations: Organisation[];
+  organisations: OrganisationProps[];
 };
 
 export const Organisations = ({ organisations }: OrganisationsProps) => {
