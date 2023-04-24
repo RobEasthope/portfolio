9fec2ef2950808b87a40efbdaaf83c26a123a529
Rob Easthope <hello@robeasthope.com>
2023-04-24 18:08:43 +0100

    Add heading to query

--- app/components/about/GigIndex/GigIndex.query.ts
+++ app/components/about/GigIndex/GigIndex.query.ts
@@ -3,6 +3,7 @@ import groq from 'groq';
 export const GIG_INDEX_QUERY = groq`{
   _key,
   _type,
+  heading,
   "gigs": *[_type == "gig" && !(_id in path("drafts.**")) && defined(startDate)]{
     employer,
     jobTitle,
