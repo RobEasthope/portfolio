f14a826d52f30d36c0372ea8148bed487d9df77c
Rob Easthope <hello@robeasthope.com>
2023-04-24 16:10:15 +0100

    Move LineBreak

--- app/components/blocks/PageTitle/PageTitle.tsx
+++ app/components/blocks/PageTitle/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { LineBreak } from '~/components/decoration/LineBreak';
+import { LineBreak } from '~/components/decoration/LineBreak/LineBreak';
 
 import { Box } from '~/components/base/Box/Box';
 import { Type } from '~/components/base/Type/Type';
