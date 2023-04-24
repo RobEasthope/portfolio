fee9eb1887fa909eedad27e6a16747275f00b3ca
Rob Easthope <hello@robeasthope.com>
2023-04-24 11:49:10 +0100

    Add fallback

--- app/components/blocks/ProjectIndex/ProjectIndex.tsx
+++ app/components/blocks/ProjectIndex/ProjectIndex.tsx
@@ -23,7 +23,10 @@ export const ProjectIndex = ({ projects }: ProjectIndexProps) => {
           className="grid grid-cols-1 gap-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7"
         >
           {projects?.map((project, i) => (
-            <ProjectIndexCard key={`${project?._id}-${i}`} project={project} />
+            <ProjectIndexCard
+              key={`${project?._id || ''}-${i}`}
+              project={project}
+            />
           ))}
         </Box>
       )}
