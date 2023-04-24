a87c528dee07cb26d6cd92e2f9765a5a7946bd29
Rob Easthope <hello@robeasthope.com>
2023-04-24 10:40:45 +0100

    Clean up project imports

--- app/routes/work.$project.tsx
+++ app/routes/work.$project.tsx
@@ -1,28 +1,15 @@
-import {
-  isRouteErrorResponse,
-  useLoaderData,
-  useRouteError,
-} from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { json } from '@vercel/remix';
 import type { LoaderArgs } from '@vercel/remix';
-import { cacheHeader } from 'pretty-cache-header';
 import { blockPreview } from 'sanity-pills';
 import { checkMetadata } from '~/utils/checkMetadata';
 import { mergeMeta } from '~/utils/mergeMeta';
 
-import type { SanityPageByIdQueryProps } from '~/types/SanityPageByIdQueryProps';
-
 import { sanityAPI } from '~/sanity/sanity-js-api/sanityAPI';
-
-import { urlFor } from '~/components/base/SanityImage/urlFor';
 
 import type { MetadataFallbacksProps } from '~/components/settings/MetadataFallbacks/MetadataFallbacks';
 
 import type { Error404Props } from '~/components/pages/Error404/Error404';
-import {
-  PAGE_BY_ID_QUERY,
-  PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
-} from '~/components/pages/Page/Page.query';
 import type { ProjectProps } from '~/components/pages/Project/Project';
 import { Project } from '~/components/pages/Project/Project';
 import { PROJECT_BY_SLUG_QUERY } from '~/components/pages/Project/Project.query';
