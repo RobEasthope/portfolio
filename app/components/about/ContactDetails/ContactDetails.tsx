35c386343d64df24b4fc5ba8d43773b295ee3365
Rob Easthope <hello@robeasthope.com>
2023-04-25 20:39:01 +0100

    Add logo markup

--- app/components/about/ContactDetails/ContactDetails.tsx
+++ app/components/about/ContactDetails/ContactDetails.tsx
@@ -1,10 +1,16 @@
+import { METADATA_HARD_CODED_FALLBACKS } from '~/constants/METADATA_HARD_CODED_FALLBACKS';
+
 import { Box } from '~/components/base/Box/Box';
 import { EmailLink } from '~/components/base/EmailLink/EmailLink';
 import { ExternalLink } from '~/components/base/ExternalLink/ExternalLink';
+import { SanityImage } from '~/components/base/SanityImage/SanityImage';
 import { Type } from '~/components/base/Type/Type';
+
+import type { HeaderProps } from '~/components/navigation/Header/Header';
 
 export type ContactDetailsProps = {
   _id: 'ContactDetails';
+  logo: HeaderProps['logo'];
   displayLogo: boolean;
   details: {
     name: string;
@@ -18,11 +24,19 @@ export type ContactDetailsProps = {
 };
 
 export const ContactDetails = ({
+  logo,
   displayLogo,
   details,
 }: ContactDetailsProps) => (
   <Box as="section" blockSpacing>
     <Box as="div" className="max-w-prose mx-auto">
+      <SanityImage
+        asset={logo}
+        alt={METADATA_HARD_CODED_FALLBACKS.TITLE}
+        mode="contain"
+        maxWidth={200}
+        className="h-1.25 w-1.25"
+      />
       <Type as="h1">{details?.name}</Type>
       <Box as="div" className="flex gap-0.5">
         <EmailLink email={details?.email}>{details?.email}</EmailLink>
