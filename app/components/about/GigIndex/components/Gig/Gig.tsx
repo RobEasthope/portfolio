910520d6cd9ca11ffbb0dc0cafb35d4493f8aa81
Rob Easthope <hello@robeasthope.com>
2023-04-25 18:45:07 +0100

    Refactor date code into GigDate

--- app/components/about/GigIndex/components/Gig/Gig.tsx
+++ app/components/about/GigIndex/components/Gig/Gig.tsx
@@ -4,6 +4,7 @@ import type { BasicTextProps } from '~/components/base/Prose/components/BasicTex
 import { BasicTextComponents } from '~/components/base/Prose/components/BasicText/BasicText';
 import { Type } from '~/components/base/Type/Type';
 
+import { GigDate } from '~/components/about/GigIndex/components/Gig/GigDate';
 import type { OrganisationProps } from '~/components/about/Organisation/Organisations';
 
 export type GigProps = {
@@ -21,50 +22,18 @@ export const Gig = ({
   description,
   startDate,
   endDate = 'present',
-}: GigProps) => {
-  const parsedStartDate = new Date(startDate);
-  const parsedEndDate = new Date(endDate);
-
-  const startDateFull = parsedStartDate.toLocaleString('default', {
-    month: 'short',
-    year: 'numeric',
-  });
-  const startMonth = parsedStartDate.toLocaleString('default', {
-    month: 'short',
-  });
-  const startYear = parsedStartDate.getFullYear();
-
-  const endDateFull = parsedEndDate.toLocaleString('default', {
-    month: 'short',
-    year: 'numeric',
-  });
-  const endMonth = parsedEndDate.toLocaleString('default', {
-    month: 'short',
-  });
-  const endYear = parsedEndDate.getFullYear();
-
-  return (
-    <Box as="article">
-      <Box as="section" className="flex flex-row">
-        <Type as="h4">{client?.name}</Type>
-        <Type as="span" className="mx-0.25">
-          /
-        </Type>
-        <Type as="h4">{jobTitle}</Type>
-      </Box>
-      <Box as="div" className="flex flex-wrap">
-        <Type as="p">
-          {startMonth} {startYear !== endYear && startYear}
-        </Type>
-        <Type as="span" className="mx-0.25">
-          &ndash;
-        </Type>
-        <Type as="p">
-          {startDateFull !== endDateFull && endMonth} {endYear}
-        </Type>
-      </Box>
-
-      <Prose as="div" content={description} components={BasicTextComponents} />
+}: GigProps) => (
+  <Box as="article">
+    <Box as="section" className="flex flex-row">
+      <Type as="h4">{client?.name}</Type>
+      <Type as="span" className="mx-0.25">
+        /
+      </Type>
+      <Type as="h4">{jobTitle}</Type>
     </Box>
-  );
-};
+
+    <GigDate startDate={startDate} endDate={endDate} />
+
+    <Prose as="div" content={description} components={BasicTextComponents} />
+  </Box>
+);
---
+++ app/components/about/GigIndex/components/Gig/GigDate.tsx
@@ -0,0 +1,49 @@
+import { Box } from '~/components/base/Box/Box';
+import { Type } from '~/components/base/Type/Type';
+
+import type { GigProps } from '~/components/about/GigIndex/components/Gig/Gig';
+
+export type GigDateProps = {
+  startDate: GigProps['startDate'];
+  endDate: GigProps['endDate'] | Date;
+};
+
+export const GigDate = ({ startDate, endDate = new Date() }: GigDateProps) => {
+  const parsedStartDate = new Date(startDate);
+  const parsedEndDate = new Date(endDate);
+
+  const startDateFull = parsedStartDate.toLocaleString('default', {
+    month: 'short',
+    year: 'numeric',
+  });
+  const startMonth = parsedStartDate.toLocaleString('default', {
+    month: 'short',
+  });
+  const startYear = parsedStartDate.getFullYear();
+
+  const endDateFull = parsedEndDate.toLocaleString('default', {
+    month: 'short',
+    year: 'numeric',
+  });
+  const endMonth = parsedEndDate.toLocaleString('default', {
+    month: 'short',
+  });
+  const endYear = parsedEndDate.getFullYear();
+
+  return (
+    <Box as="div" className="flex flex-wrap">
+      <Type as="p">
+        {startMonth} {startYear !== endYear && startYear}
+      </Type>
+      {startDateFull !== endDateFull ? (
+        <Type as="span">&nbsp;&ndash;&nbsp;</Type>
+      ) : (
+        <>&nbsp;</>
+      )}
+
+      <Type as="p">
+        {startDateFull !== endDateFull && endMonth} {endYear}
+      </Type>
+    </Box>
+  );
+};
