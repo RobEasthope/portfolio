2d7ebffddccfbe8c07023b27c3a3f9a1ba8bcef8
Rob Easthope <hello@robeasthope.com>
2023-04-25 20:51:27 +0100

    Add props

--- app/components/decoration/Dot/Dot.tsx
+++ app/components/decoration/Dot/Dot.tsx
@@ -1,8 +1,10 @@
 export type DotProps = {
+  spacingTop?: number;
+  spacingBottom?: number;
   className?: string;
 };
 
-export const Dot = ({ className }: DotProps) => (
+export const Dot = ({ spacingTop, spacingBottom, className }: DotProps) => (
   <hr
     className={`flex w-full justify-center items-center h-1 mx-auto after:content-[''] after:h-0.25 after:w-0.25 after:bg-black after:rounded-full border-none ${
       className || ''
