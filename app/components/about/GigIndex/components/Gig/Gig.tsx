a933b621b19e959e6f67b54f09337bc7ce4f8bd2
Rob Easthope <hello@robeasthope.com>
2023-04-25 18:06:42 +0100

    Basic date formatting

--- app/components/about/GigIndex/components/Gig/Gig.tsx
+++ app/components/about/GigIndex/components/Gig/Gig.tsx
@@ -20,24 +20,43 @@ export const Gig = ({
   jobTitle,
   description,
   startDate,
-  endDate,
-}: GigProps) => (
-  <Box as="article">
-    <Box as="section" className="flex flex-row">
-      <Type as="h4">{client?.name}</Type>
-      <Type as="span" className="mx-0.25">
-        /
-      </Type>
-      <Type as="h4">{jobTitle}</Type>
-    </Box>
-    <Box as="div" className="flex flex-wrap">
-      <Type as="p">{startDate}</Type>
-      <Type as="span" className="mx-0.25">
-        &ndash;
-      </Type>
-      <Type as="p">{endDate}</Type>
-    </Box>
+  endDate = 'present',
+}: GigProps) => {
+  const parsedStartDate = new Date(startDate);
+  const parsedEndDate = new Date(endDate);
 
-    <Prose as="div" content={description} components={BasicTextComponents} />
-  </Box>
-);
+  const startMonth = parsedStartDate.toLocaleString('default', {
+    month: 'short',
+  });
+  const startYear = parsedStartDate.getFullYear();
+
+  const endMonth = parsedEndDate.toLocaleString('default', {
+    month: 'short',
+  });
+  const endYear = parsedEndDate.getFullYear();
+
+  return (
+    <Box as="article">
+      <Box as="section" className="flex flex-row">
+        <Type as="h4">{client?.name}</Type>
+        <Type as="span" className="mx-0.25">
+          /
+        </Type>
+        <Type as="h4">{jobTitle}</Type>
+      </Box>
+      <Box as="div" className="flex flex-wrap">
+        <Type as="p">
+          {startMonth} {startYear !== endYear ? startYear : ''}
+        </Type>
+        <Type as="span" className="mx-0.25">
+          &ndash;
+        </Type>
+        <Type as="p">
+          {endMonth} {endYear}
+        </Type>
+      </Box>
+
+      <Prose as="div" content={description} components={BasicTextComponents} />
+    </Box>
+  );
+};
