b25c9659178efc05097311e1062c5bfd01083fac
Rob Easthope <hello@robeasthope.com>
2023-04-25 20:38:55 +0100

    Grab logo from Header

--- app/components/about/ContactDetails/ContactDetails.query.ts
+++ app/components/about/ContactDetails/ContactDetails.query.ts
@@ -3,6 +3,7 @@ import groq from 'groq';
 export const CONTACT_DETAILS_QUERY = groq`{
   _type,
   _id,
+  "logo": *[_type== 'Header' && !(_id in path("drafts.**"))][0].logo,
   displayLogo,
   "details": *[_type== 'Details' && !(_id in path("drafts.**"))][0]{
     name,
