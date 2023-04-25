664c287b6fd2e26fa9d6e7a896eb4a8931da05ed
Rob Easthope <hello@robeasthope.com>
2023-04-25 20:57:33 +0100

    Integrate spacing values

--- app/components/decoration/Dot/Dot.tsx
+++ app/components/decoration/Dot/Dot.tsx
@@ -6,11 +6,17 @@ export type DotProps = {
   className?: string;
 };
 
-export const Dot = ({ spacingTop, spacingBottom, className }: DotProps) => (
+export const Dot = ({
+  spacingTop = '0',
+  spacingBottom = '0',
+  className,
+}: DotProps) => (
   <hr
     className={classNames(
       `flex w-full justify-center items-center h-1 mx-auto after:content-[''] after:h-0.25 after:w-0.25 after:bg-black after:rounded-full border-none`,
       className,
+      `mt-${spacingTop}`,
+      `mb-${spacingBottom}`,
     )}
   />
 );
