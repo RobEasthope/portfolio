e3a818511c35f10b72cb7a8f2f6079c1abc3f716
Rob Easthope <hello@robeasthope.com>
2023-04-25 15:14:44 +0100

    Move sanity studio footer to layout

--- app/components/sanity/SanityStudio/SanityStudio.tsx
+++ app/components/sanity/SanityStudio/SanityStudio.tsx
@@ -2,34 +2,8 @@ import { ClientOnly } from 'remix-utils';
 import { Studio } from 'sanity';
 import config from 'sanity.config';
 
-import type { AppSettingsProps } from '~/components/settings/AppSettings/AppSettings';
-
-import { Box } from '~/components/base/Box/Box';
-import { InternalLink } from '~/components/base/InternalLink/InternalLink';
-
-export type SanityStudioProps = {
-  homePageSlug: AppSettingsProps['homePageSlug'];
-};
-
-export function SanityStudio({ homePageSlug }: SanityStudioProps) {
-  return (
-    <ClientOnly>
-      {() => (
-        <>
-          <Studio config={config} />
-          <Box as="nav" className="flex justify-center">
-            <InternalLink
-              href={homePageSlug}
-              className="inline-block text-white bg-gray-900 font-bold font-sans text-center px-1 py-0.5"
-              homePageSlug={homePageSlug}
-            >
-              Back to app
-            </InternalLink>
-          </Box>
-        </>
-      )}
-    </ClientOnly>
-  );
+export function SanityStudio() {
+  return <ClientOnly>{() => <Studio config={config} />}</ClientOnly>;
 }
 
 export default SanityStudio;
--- app/routes/_studioLayout.sanity-studio.$.tsx
+++ app/routes/_studioLayout.sanity-studio.$.tsx
@@ -1,27 +1,10 @@
 /* eslint-disable camelcase */
 import type { LinksFunction, V2_MetaFunction } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
 import { Suspense, lazy } from 'react';
-
-import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
 
 import styles from '~/components/sanity/SanityStudio/SanityStudio.css';
 
-import type { AppSettingsProps } from '~/components/settings/AppSettings/AppSettings';
-import { APP_SETTINGS_QUERY } from '~/components/settings/AppSettings/AppSettings.query';
-
-export async function loader() {
-  const appSettings: AppSettingsProps = await sanityAPI.fetch(
-    APP_SETTINGS_QUERY,
-  );
-
-  return json({
-    appSettings: appSettings || null,
-  });
-}
-
-export const meta: V2_MetaFunction<typeof loader> = () => [
+export const meta: V2_MetaFunction = () => [
   { title: 'Sanity Studio' },
   {
     property: 'robots',
@@ -44,11 +27,9 @@ const Fallback = (
 );
 
 export default function StudioPage() {
-  const { appSettings } = useLoaderData<typeof loader>();
-
   return (
     <Suspense fallback={Fallback}>
-      <SanityStudio homePageSlug={appSettings?.homePageSlug} />
+      <SanityStudio />
     </Suspense>
   );
 }
