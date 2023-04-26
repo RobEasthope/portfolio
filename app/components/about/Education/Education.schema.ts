9b782d0cb6e5475beafd5db188b5f50f46145738
Rob Easthope <hello@robeasthope.com>
2023-04-26 10:02:26 +0100

    Update Education.schema.ts

--- app/components/about/Education/Education.schema.ts
+++ app/components/about/Education/Education.schema.ts
@@ -1,58 +1,26 @@
+import { RiPhoneLine } from 'react-icons/ri';
 import { defineField, defineType } from 'sanity';
 
-import type { BasicSanityListingProps } from '~/types/BasicSanityListing';
-
 export default defineType({
-  name: 'education',
-  title: 'Education',
-  type: 'document',
+  type: 'object',
+  name: 'Education index',
+  title: 'Education index',
+  icon: RiPhoneLine,
   fields: [
     defineField({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
+      name: 'display',
+      title: 'Display block',
+      type: 'boolean',
       validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: 'url',
-      title: 'URL',
-      type: 'url',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: 'qualification',
-      title: 'Qualification',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: 'when',
-      title: 'When',
-      type: 'string',
-      validation: (Rule) => Rule.required(),
-    }),
-    defineField({
-      name: 'logo',
-      title: 'Logo',
-      type: 'image',
-      validation: (Rule) => Rule.required(),
-      options: {
-        hotspot: true,
-      },
     }),
   ],
-
+  initialValue: {
+    display: true,
+  },
   preview: {
-    select: {
-      title: 'name',
-      media: 'logo',
-    },
-    prepare(selection: BasicSanityListingProps) {
-      const { title, media } = selection;
+    prepare() {
       return {
-        title: title || 'Education',
-        subtitle: title ? 'Education' : '',
-        media,
+        title: 'Education index',
       };
     },
   },
