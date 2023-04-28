66abc6c1b8756de91bdcd3337e380c2091e1b832
Rob Easthope <hello@robeasthope.com>
2023-04-28 10:23:00 +0100

    Remove link back to main site

--- app/routes/_studioLayout.tsx
+++ app/routes/_studioLayout.tsx
@@ -1,41 +1,5 @@
-import { json } from '@remix-run/node';
-import { Outlet, useLoaderData } from '@remix-run/react';
+import { Outlet } from '@remix-run/react';
 
-import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
-
-import type { AppSettingsProps } from '~/components/settings/AppSettings/AppSettings';
-import { APP_SETTINGS_QUERY } from '~/components/settings/AppSettings/AppSettings.query';
-
-import { Box } from '~/components/base/Box/Box';
-import { InternalLink } from '~/components/base/InternalLink/InternalLink';
-
-export async function loader() {
-  const appSettings: AppSettingsProps = await sanityAPI.fetch(
-    APP_SETTINGS_QUERY,
-  );
-
-  return json({
-    appSettings: appSettings || null,
-  });
-}
-
-export const StudioLayout = () => {
-  const { appSettings } = useLoaderData<typeof loader>();
-
-  return (
-    <>
-      <Outlet />
-      <Box as="footer" className="flex items-center justify-center">
-        <InternalLink
-          href={appSettings?.homePageSlug}
-          className="inline-block text-white bg-gray-900 font-bold font-sans text-center px-1 py-0.5"
-          homePageSlug={appSettings?.homePageSlug}
-        >
-          Back to app
-        </InternalLink>
-      </Box>
-    </>
-  );
-};
+export const StudioLayout = () => <Outlet />;
 
 export default StudioLayout;
