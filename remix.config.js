f11f0b289b6afe39f0328fc26041cd7f8f7365de
Rob Easthope <hello@robeasthope.com>
2023-05-03 10:32:14 +0100

    Add v2_normalizeFormMethod remix flag and set to true

--- remix.config.js
+++ remix.config.js
@@ -9,6 +9,7 @@ module.exports = {
     unstable_tailwind: true,
     v2_errorBoundary: true,
     v2_meta: true,
+    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
   serverDependenciesToBundle: ["classname-variants"],
