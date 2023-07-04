10590d0a40d1d38633d33178f815b3ebf9360997
Rob Easthope <hello@robeasthope.com>
2023-07-04 17:44:35 +0100

    Update remix tailwind config to stable flag

--- remix.config.js
+++ remix.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   future: {
-    unstable_tailwind: true,
+    tailwind: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
