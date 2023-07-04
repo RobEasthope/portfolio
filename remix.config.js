ac8627e996d6089bdb8c066ad0e7c1baac8f3305
Rob Easthope <hello@robeasthope.com>
2023-07-04 17:47:15 +0100

    Opt into v2 headers format

--- remix.config.js
+++ remix.config.js
@@ -9,6 +9,7 @@ module.exports = {
     tailwind: true,
     serverModuleFormat: 'esm',
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
