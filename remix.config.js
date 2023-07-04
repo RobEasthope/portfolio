25390a3a3272eeced5e88572b0a5cca89f3bf565
Rob Easthope <hello@robeasthope.com>
2023-07-04 17:45:26 +0100

    Opt into esm server module format

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,7 @@ module.exports = {
   // publicPath: "/build/",
   future: {
     tailwind: true,
+    serverModuleFormat: 'esm',
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
