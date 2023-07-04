1e4383f30dd38b9e9399418e76e4e51005d8f94d
Rob Easthope <hello@robeasthope.com>
2023-07-04 17:50:18 +0100

    Remove comment out code

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
   serverDependenciesToBundle: ["classname-variants"],
   serverModuleFormat: "esm",
   tailwind: true,
