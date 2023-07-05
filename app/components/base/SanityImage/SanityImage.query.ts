a8b55aedf06ae191869f15a61f6e3741030cb10f
Rob Easthope <hello@robeasthope.com>
2023-07-05 10:16:30 +0100

    Tweak query to be cleaner

--- app/components/base/SanityImage/SanityImage.query.ts
+++ app/components/base/SanityImage/SanityImage.query.ts
@@ -4,7 +4,12 @@ export const SANITY_IMAGE_QUERY = groq`
   {
     "id": asset._ref,
     "preview": asset->metadata.lqip,
-    "dimensions": asset->metadata.dimensions,
+    "dimensions": {
+      "width": asset->metadata.dimensions.width,
+      "height": asset->metadata.dimensions.height,
+      "originalAspectRatio": asset->metadata.dimensions.aspectRatio,
+
+    },
     hotspot { x, y },
     crop {
       top,
