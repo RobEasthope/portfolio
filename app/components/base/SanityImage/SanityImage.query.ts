073283d63fa70737f9098ceb7f450b2227338b2b
Rob Easthope <hello@robeasthope.com>
2023-07-05 10:14:35 +0100

    Add dimensions to query

--- app/components/base/SanityImage/SanityImage.query.ts
+++ app/components/base/SanityImage/SanityImage.query.ts
@@ -4,6 +4,7 @@ export const SANITY_IMAGE_QUERY = groq`
   {
     "id": asset._ref,
     "preview": asset->metadata.lqip,
+    "dimensions": asset->metadata.dimensions,
     hotspot { x, y },
     crop {
       top,
