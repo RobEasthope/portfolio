d69c20131b7d20c9298c64a7be59af35b4b60b27
Rob Easthope <hello@robeasthope.com>
2023-07-08 19:40:35 +0100

    Get routing page to work

--- app/routes/_nav.$page.tsx
+++ app/routes/_nav.$page.tsx
@@ -31,7 +31,11 @@ type PageBySlugProps = PageProps & {
 };
 
 export async function loader({ params }: LoaderArgs) {
-  const appSettings: AppSettingsProps = await sanityAPI({}).fetch(
+  const token = process.env.SANITY_API_TOKEN;
+  const preview =
+    process.env.SANITY_API_PREVIEW_DRAFTS === 'true' ? { token } : undefined;
+
+  const appSettings: AppSettingsProps = await sanityAPI({ preview }).fetch(
     APP_SETTINGS_QUERY,
   );
 
@@ -42,14 +46,14 @@ export async function loader({ params }: LoaderArgs) {
     });
   }
 
-  const primer: SanityPageByIdQueryProps = await sanityAPI({}).fetch(
+  const primer: SanityPageByIdQueryProps = await sanityAPI({ preview }).fetch(
     PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
     {
       slug: params?.page,
     },
   );
 
-  const payload: PageBySlugProps = await sanityAPI({}).fetch(
+  const payload: PageBySlugProps = await sanityAPI({ preview }).fetch(
     PAGE_BY_ID_QUERY({
       id: primer?.id,
       componentTypes: primer?.componentTypes,
