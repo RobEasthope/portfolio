e0ef4953bef83b7553e371d19f15df7acace2eb4
Rob Easthope <hello@robeasthope.com>
2023-07-08 12:45:52 +0100

    Upgrade sanityAPI util

--- app/utils/sanity-js-api/sanityAPI.ts
+++ app/utils/sanity-js-api/sanityAPI.ts
@@ -1,10 +1,28 @@
 import { createClient } from '@sanity/client';
+import type { SanityClient } from '@sanity/client';
 
-export const sanityAPI = createClient({
-  projectId: process.env.SANITY_PUBLIC_PROJECT_ID || '',
-  dataset: process.env.SANITY_PUBLIC_DATASET || '',
-  apiVersion: process.env.SANITY_PUBLIC_API_VERSION || '',
-  useCdn: true,
-  perspective: 'published',
-  token: process.env.SANITY_API_TOKEN || '',
-});
+export function sanityAPI({
+  preview,
+}: {
+  preview?: { token: string };
+}): SanityClient {
+  const client = createClient({
+    projectId: process.env.SANITY_PUBLIC_PROJECT_ID || '',
+    dataset: process.env.SANITY_PUBLIC_DATASET || '',
+    apiVersion: process.env.SANITY_PUBLIC_API_VERSION || '',
+    useCdn: true,
+    perspective: 'published',
+  });
+  if (preview) {
+    if (!preview.token) {
+      throw new Error('You must provide a token to preview drafts');
+    }
+    return client.withConfig({
+      token: process.env.SANITY_API_TOKEN || '',
+      useCdn: false,
+      ignoreBrowserTokenWarning: true,
+      perspective: 'previewDrafts',
+    });
+  }
+  return client;
+}
