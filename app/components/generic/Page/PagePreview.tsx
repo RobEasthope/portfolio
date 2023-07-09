53c428fb5ae205270f8d290836d5771907d18382
Rob Easthope <hello@robeasthope.com>
2023-07-09 14:49:37 +0100

    Update tyings

--- app/components/generic/Page/PagePreview.tsx
+++ app/components/generic/Page/PagePreview.tsx
@@ -1,10 +1,10 @@
 import { useParams } from '@remix-run/react';
-import type { SanityDocument } from '@sanity/client';
 import { useLiveQuery } from '@sanity/preview-kit';
+import type { PageProps } from '~/components/generic/Page/Page';
 import { Page } from '~/components/generic/Page/Page';
 import { PAGE_BY_ID_QUERY } from '~/components/generic/Page/Page.query';
 
-export function PagePreview({ page }: { page: SanityDocument }) {
+export function PagePreview({ page }: { page: PageProps['page'] }) {
   const params = useParams();
   const [data] = useLiveQuery(
     page,
