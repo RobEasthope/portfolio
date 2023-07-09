023bb2981198accbbffe24cff94b2ac12a4920d4
Rob Easthope <hello@robeasthope.com>
2023-07-09 16:36:06 +0100

    Query for sanity data

--- app/components/generic/Page/PagePreview.tsx
+++ app/components/generic/Page/PagePreview.tsx
@@ -2,9 +2,15 @@ import { useParams } from '@remix-run/react';
 import { useLiveQuery } from '@sanity/preview-kit';
 import type { PageProps } from '~/components/generic/Page/Page';
 import { Page } from '~/components/generic/Page/Page';
-import { PAGE_BY_SLUG_QUERY } from '~/components/generic/Page/Page.query';
+import type { PageBySlugQueryProps } from '~/components/generic/Page/Page.query';
+import {
+  PAGE_BY_SLUG_QUERY,
+  PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
+} from '~/components/generic/Page/Page.query';
 
-export function PagePreview({
+import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
+
+export async function PagePreview({
   page,
   slug,
 }: {
@@ -12,6 +18,14 @@ export function PagePreview({
   slug: string;
 }) {
   const params = useParams();
+
+  const primer: PageBySlugQueryProps = await sanityAPI({ preview }).fetch(
+    PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
+    {
+      slug,
+    },
+  );
+
   const [data] = useLiveQuery(
     page,
     PAGE_BY_SLUG_QUERY({
