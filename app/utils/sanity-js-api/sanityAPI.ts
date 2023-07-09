d6f01f9498e79cfcdbda9b8e53f5892de9cdc57a
Rob Easthope <hello@robeasthope.com>
2023-07-09 18:53:38 +0100

    Refactor SanityAPI client something more simple

--- app/utils/sanity-js-api/sanityAPI.ts
+++ app/utils/sanity-js-api/sanityAPI.ts
@@ -1,28 +1,26 @@
 import { createClient } from '@sanity/client';
 import type { SanityClient } from '@sanity/client';
 
-export function sanityAPI({
-  preview,
-}: {
-  preview?: { token: string };
-}): SanityClient {
-  const client = createClient({
+export function sanityAPI({ preview }: { preview?: boolean }): SanityClient {
+  // Published mode
+  if (!preview) {
+    return createClient({
+      projectId: process.env.SANITY_PUBLIC_PROJECT_ID || '',
+      dataset: process.env.SANITY_PUBLIC_DATASET || '',
+      apiVersion: process.env.SANITY_PUBLIC_API_VERSION || '',
+      useCdn: true,
+      perspective: 'published',
+    });
+  }
+
+  // Preview mode
+  return createClient({
     projectId: process.env.SANITY_PUBLIC_PROJECT_ID || '',
     dataset: process.env.SANITY_PUBLIC_DATASET || '',
     apiVersion: process.env.SANITY_PUBLIC_API_VERSION || '',
-    useCdn: true,
-    perspective: 'published',
+    token: process.env.SANITY_API_TOKEN || '',
+    useCdn: false,
+    ignoreBrowserTokenWarning: true,
+    perspective: 'previewDrafts',
   });
-  if (preview) {
-    if (!preview.token) {
-      throw new Error('You must provide a token to preview drafts');
-    }
-    return client.withConfig({
-      token: process.env.SANITY_API_TOKEN || '',
-      useCdn: false,
-      ignoreBrowserTokenWarning: true,
-      perspective: 'previewDrafts',
-    });
-  }
-  return client;
 }
