6b9cff7bd184126bc49e73e193e7d7257e898fe1
Rob Easthope <hello@robeasthope.com>
2023-07-09 18:59:50 +0100

    Clean up PagePreview code

--- app/components/generic/Page/PagePreview.tsx
+++ app/components/generic/Page/PagePreview.tsx
@@ -2,30 +2,16 @@ import { useParams } from '@remix-run/react';
 import { useLiveQuery } from '@sanity/preview-kit';
 import type { PageProps } from '~/components/generic/Page/Page';
 import { Page } from '~/components/generic/Page/Page';
-import type { PageBySlugQueryProps } from '~/components/generic/Page/Page.query';
-import {
-  PAGE_BY_SLUG_QUERY,
-  PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
-} from '~/components/generic/Page/Page.query';
-
-import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
+import { PAGE_BY_SLUG_QUERY } from '~/components/generic/Page/Page.query';
 
 export function PagePreview({
   page,
   slug,
-  preview,
 }: {
   page: PageProps['page'];
   slug: string;
 }) {
   const params = useParams();
-
-  // const primer: PageBySlugQueryProps = await sanityAPI({ preview }).fetch(
-  //   PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
-  //   {
-  //     slug,
-  //   },
-  // );
 
   const [data] = useLiveQuery(
     page,
