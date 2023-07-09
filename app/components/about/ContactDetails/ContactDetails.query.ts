7a281e4087fa9f56f5bd77b93873d03a4c3770d0
Rob Easthope <hello@robeasthope.com>
2023-07-09 20:50:25 +0100

    Fix CONTACT_DETAILS_QUERY

--- app/components/about/ContactDetails/ContactDetails.query.ts
+++ app/components/about/ContactDetails/ContactDetails.query.ts
@@ -1,9 +1,10 @@
 import groq from 'groq';
+import { SANITY_IMAGE_QUERY } from '~/components/_base/SanityImage/SanityImage.query';
 
 export const CONTACT_DETAILS_QUERY = groq`{
   _type,
   _id,
-  "logo": *[_type== 'Header' ][0].logo,
+  "logo": *[_type== 'Header' ][0]{"asset": logo{${SANITY_IMAGE_QUERY}}},
   displayLogo,
   "details": *[_type== 'Details' ][0]{
     name,
