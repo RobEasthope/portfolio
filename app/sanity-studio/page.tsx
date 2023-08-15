df65f66f97f73d7362fb2b3492e0eabe7e17edf5
Rob Easthope <hello@robeasthope.com>
2023-08-15 08:30:00 +0100

    Create test route

---
+++ app/sanity-studio/page.tsx
@@ -0,0 +1,7 @@
+export default function SanityStudio() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Sanity studio</h1>
+    </main>
+  );
+}
