d9fe25e3362dc4f49e5179c73011c54d8b6f6276
Rob Easthope <hello@robeasthope.com>
2023-08-15 12:12:44 +0100

    Successfully pull down home page data

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,59 @@
+import { Error404Props } from '@/components/generic/Error404/Error404';
+import { PageProps } from '@/components/generic/Page/Page';
+import {
+  PAGE_BY_SLUG_QUERY,
+  PAGE_COMPONENT_TYPES_BY_SLUG_QUERY,
+  PageBySlugQueryProps,
+} from '@/components/generic/Page/Page.query';
 import { AppSettingsProps } from '@/components/settings/AppSettings/AppSettings';
 import { APP_SETTINGS_QUERY } from '@/components/settings/AppSettings/AppSettings.query';
 import { sanityAPI } from '@/utils/sanity-js-api/sanityAPI';
 
+type PageBySlugProps = PageProps & {
+  error404: Error404Props['page'];
+};
+
 export async function getData() {
-  const data: AppSettingsProps = await sanityAPI({ preview: false }).fetch(
-    APP_SETTINGS_QUERY,
+  const appSettings: AppSettingsProps = await sanityAPI({
+    preview: false,
+  }).fetch(APP_SETTINGS_QUERY);
+
+  const primer: PageBySlugQueryProps = await sanityAPI({
+    preview: false,
+  }).fetch(
+    PAGE_COMPONENT_TYPES_BY_SLUG_QUERY({
+      slug: appSettings?.homePageSlug,
+    }),
   );
 
-  if (!data) {
+  const payload: PageBySlugProps = await sanityAPI({ preview: false }).fetch(
+    PAGE_BY_SLUG_QUERY({
+      slug: appSettings?.homePageSlug,
+      componentTypes: primer?.componentTypes,
+    }),
+  );
+
+  if (!payload) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data');
   }
 
-  return data;
+  return {
+    appSettings: appSettings,
+    primer: primer,
+    payload: payload,
+  };
 }
 
 export default async function Home() {
   const data = await getData();
 
+  console.log(data);
+
   return (
     <main>
       <h1>./root</h1>
-      <h2>Home page slug: {data.homePageSlug}</h2>
+      <h2>Home page slug: {data?.appSettings?.homePageSlug}</h2>
     </main>
   );
 }
