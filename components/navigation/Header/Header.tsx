3b4423c7351af47a66a473092fd865cb1241a89a
Rob Easthope <hello@robeasthope.com>
2023-08-17 13:42:47 +0100

    Comment out headroom component

--- components/navigation/Header/Header.tsx
+++ components/navigation/Header/Header.tsx
@@ -50,26 +50,26 @@ export const Header = ({
   }
 
   return (
-    <Headroom className="print:hidden">
-      <Box as="header" className="bg-white px-1 md:px-2 py-0.5 sm:py-1">
-        <Box
-          as="nav"
-          className="mx-auto flex w-full flex-row-reverse flex-wrap items-center justify-between leading-4 sm:flex-row"
-        >
-          <NavListing links={primaryNavigation} />
+    // <Headroom className="print:hidden">
+    <Box as="header" className="bg-white px-1 md:px-2 py-0.5 sm:py-1">
+      <Box
+        as="nav"
+        className="mx-auto flex w-full flex-row-reverse flex-wrap items-center justify-between leading-4 sm:flex-row"
+      >
+        <NavListing links={primaryNavigation} />
 
-          <HeaderLogo logo={logo} homePageSlug={appSettings?.homePageSlug} />
+        <HeaderLogo logo={logo} homePageSlug={appSettings?.homePageSlug} />
 
-          <NavListing links={secondaryNavigation} />
+        <NavListing links={secondaryNavigation} />
 
-          <SmallNavigation
-            logo={logo}
-            primaryNavigation={primaryNavigation}
-            secondaryNavigation={secondaryNavigation}
-            appSettings={appSettings}
-          />
-        </Box>
+        <SmallNavigation
+          logo={logo}
+          primaryNavigation={primaryNavigation}
+          secondaryNavigation={secondaryNavigation}
+          appSettings={appSettings}
+        />
       </Box>
-    </Headroom>
+    </Box>
+    // </Headroom>
   );
 };
