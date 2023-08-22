2d45e00cd8cc549a6bf3cbdc93c64790116ebe09
Rob Easthope <hello@robeasthope.com>
2023-08-22 10:49:32 +0100

    Add notes field

--- components/about/GigIndex/components/Gig/Gig.schema.ts
+++ components/about/GigIndex/components/Gig/Gig.schema.ts
@@ -43,6 +43,11 @@ export default defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      name: 'notes',
+      title: 'Notes',
+      type: 'BasicText',
+    }),
+    defineField({
       name: 'startDate',
       title: 'Start date',
       type: 'date',
